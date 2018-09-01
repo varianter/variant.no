f8c2a63b1aa9bb02aaf2924862b1494de663e739
Krakels <marius.krakeli@gmail.com>
2018-09-01 17:17:13 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Variant Homepage
+
+Current homepage of Variant.
+
+## Building and running locally
+
+`dist.css` is a generated file and shouldn't be edited manually. You will not see style changes by changing `style.css` unless you run this command from the terminal:
+
+```
+npm run css
+```
+
+Other than that, just change the `index.html` or `main.js`.
---
+++ assets/main.js
@@ -0,0 +1,126 @@
+const refNode = document.getElementById('blogReadMore');
+const insertNode = (nodes = []) =>
+  nodes.forEach(node =>
+    refNode.insertAdjacentHTML('beforebegin', node)
+  );
+
+const compiledTemplate = template.bind(
+  null,
+  document.querySelector('#blogTemplate').innerHTML
+);
+const values =
+  Object.values ||
+  function values(object) {
+    if (!object) return [];
+    return Object.keys(object).map(key => object[key]);
+  };
+
+if ('fetch' in window) {
+  // If not modern browser, just don't show posts.
+
+  fetch('https://variant-blogpost-service-ayxrpnqazg.now.sh/')
+    .then(i => i.json())
+    .then(function(data) {
+      if (!data.success) {
+        return logError(data.error);
+      }
+      showPosts(data.payload.references.Post);
+    })
+    .catch(logError);
+}
+
+function toTemplateObject(post) {
+  return {
+    IMAGE: './assets/illustrations/no-image.svg',
+    TITLE: post.title,
+    URL: `https://medium.com/@mikaelbrevik/${post.uniqueSlug}`,
+    DESCRIPTION: post.content.subtitle
+  };
+}
+
+function showPosts(posts = []) {
+  insertNode(
+    values(posts)
+      .map(toTemplateObject)
+      .map(compiledTemplate)
+  );
+}
+
+function template(text, obj) {
+  return text.replace(
+    /\{\{([A-Z]+)\}\}/g,
+    (_, key) => obj[key] || ''
+  );
+}
+
+// Just print error, doesn't matter if they don't show as we already link to
+// all posts in pure HTML.
+function logError(err) {
+  console.error(err);
+}
+
+// Menu animation
+
+let prevAngle = 8;
+
+const el = document.querySelector('.hero__inner');
+const elLink = el.querySelector('.hero__menu');
+const hero = document.querySelector('.hero');
+const thumbsUp = document.querySelector('.thumbsUp');
+
+const calcOffset = () =>
+  hero.offsetHeight - el.offsetHeight + elLink.offsetHeight + 10;
+let elOffset = calcOffset();
+throttle('resize', function() {
+  elOffset = calcOffset();
+});
+
+function calculateAnimationPoints(scrollOffset) {
+  const rect = el.getBoundingClientRect();
+  const top = el.offsetTop - rect.height;
+  const x = window.innerWidth;
+  const y = rect.height - scrollOffset + top;
+  const angle = Math.max(
+    0,
+    Math.min(8, (Math.atan(y / x) * 180) / Math.PI)
+  );
+
+  el.classList.toggle(
+    'hero__inner--offTop',
+    scrollOffset > thumbsUp.offsetTop + thumbsUp.offsetHeight
+  );
+
+  if (prevAngle !== angle) {
+    el.style.setProperty('--rotation', `-${angle}deg`);
+    el.style.setProperty('--opacity', Math.max(0.7, 1 - angle / 10));
+  }
+  const isBelowHeader = scrollOffset >= elOffset + 5;
+  hero.classList.toggle('hero--fixed', isBelowHeader);
+  el.classList.toggle('hero__inner--fixed', isBelowHeader);
+  prevAngle = angle;
+}
+
+throttle(
+  'scroll',
+  function(_, prevScroll) {
+    calculateAnimationPoints(prevScroll);
+  },
+  () => window.scrollY
+);
+
+function throttle(eventName, fn, valueSelector) {
+  let val = 0;
+  let tick = false;
+  window.addEventListener(eventName, function(e) {
+    if (valueSelector) {
+      val = valueSelector(e);
+    }
+    if (!tick) {
+      window.requestAnimationFrame(function() {
+        fn(e, val);
+        tick = false;
+      });
+      tick = true;
+    }
+  });
+}
