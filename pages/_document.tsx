e625ff5b2684e81456c0dd92e7d285dbbd5976dd
Mikael Brevik <mikaelbre@gmail.com>
2020-08-23 19:36:40 +0200

    Adds matomo stats. Fixes #86

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,29 @@ export default class Doc extends Document {
         <body>
           <Main />
           <NextScript />
+
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                  var _paq = _paq || [];
+                  _paq.push(["disableCookies"]);
+                  _paq.push(["trackPageView"]);
+                  _paq.push(["enableLinkTracking"]);
+                  (function() {
+                    var u = "https://variant.innocraft.cloud/";
+                    _paq.push(["setTrackerUrl", u + "piwik.php"]);
+                    _paq.push(["setSiteId", "1"]);
+                    var d = document,
+                      g = d.createElement("script"),
+                      s = d.getElementsByTagName("script")[0];
+                    g.type = "text/javascript";
+                    g.async = true;
+                    g.defer = true;
+                    g.src = u + "piwik.js";
+                    s.parentNode.insertBefore(g, s);
+                  })();`,
+            }}
+          />
         </body>
       </Html>
     );
