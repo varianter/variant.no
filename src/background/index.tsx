34a4da21fc0aa7b493de4f11101dfdc07bd37563
Mikael Brevik <mikaelbre@gmail.com>
2020-08-23 22:41:42 +0200

    Deactivates animations for background

--- src/background/index.tsx
+++ src/background/index.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
 import Blob from './blobs';
 import style from './background.module.css';
-import { useReducedMotionEffect } from 'src/utils/hooks';
 
 export default function AnimatingBackground() {
-  const preventAuto = useReducedMotionEffect();
   return (
     <div className={style.backgroundContainer}>
       <Blob
         variation="blob-1"
-        autoplay={!preventAuto}
+        autoplay={false}
         className={style.background__first}
       />
       <Blob
         variation="blob-2"
-        autoplay={!preventAuto}
+        autoplay={false}
         className={style.background__second}
       />
       <Blob
         variation="blob-3"
-        autoplay={!preventAuto}
+        autoplay={false}
         className={style.background__third}
       />
     </div>
