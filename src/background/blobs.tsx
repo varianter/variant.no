d91813ab81c66c5a855809f18ba1104ec4db491d
Mikael Brevik <mikaelbre@gmail.com>
2020-09-03 13:04:44 +0200

    Removes lottie and blob animation from background

--- src/background/blobs.tsx
+++ src/background/blobs.tsx
@@ -1,14 +1,9 @@
 import React from 'react';
-import Lottie, { Options } from 'react-lottie';
-import * as blob1 from './blob-1.json';
-import * as blob2 from './blob-2.json';
-import * as blob3 from './blob-3.json';
 
 export type BlobVariations = 'blob-1' | 'blob-2' | 'blob-3';
 
 export type BlobProps = {
   className?: string;
-  autoplay?: boolean;
   variation?: BlobVariations;
 };
 
@@ -18,41 +13,21 @@ declare module 'react-lottie' {
   }
 }
 
-export default function Blob({
-  className,
-  autoplay = true,
-  variation = 'blob-1',
-}: BlobProps) {
+export default function Blob({ className, variation = 'blob-1' }: BlobProps) {
   return (
     <div className={className}>
-      <Lottie
-        tabIndex={-1}
-        options={createDefaultOptions(typeToImport(variation), autoplay)}
-      />
+      <img src={typeToImport(variation)} role="none" alt="" />
     </div>
   );
 }
 
-const createDefaultOptions = (
-  animationData: unknown,
-  autoplay: boolean,
-): Options => ({
-  loop: false,
-  autoplay,
-  animationData,
-
-  rendererSettings: {
-    preserveAspectRatio: 'xMidYMid slice',
-  },
-});
-
 function typeToImport(variation: BlobVariations) {
   switch (variation) {
     case 'blob-1':
-      return blob1;
+      return require('./blob1.svg');
     case 'blob-2':
-      return blob2;
+      return require('./blob2.svg');
     case 'blob-3':
-      return blob3;
+      return require('./blob3.svg');
   }
 }
