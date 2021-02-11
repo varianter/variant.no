fa22863ada5e1ceb307875d9120d2d2c4b01d253
oddsve <odd.morten@sveas.org>
2021-02-11 11:24:36 +0100

    Justrer litt på formatering av tallene i tabellen

--- src/oslo/index.tsx
+++ src/oslo/index.tsx
@@ -35,7 +35,7 @@ export default function VariantTur() {
               mennesker sammen utfordrer hverandre og bygger på hverandre. Dette
               er et selskap som våger å vokse fort. Vi har en modell
               for bærekraftig vekst, og et realistisk mål om å bygge et fagmiljø
-              med 50 dyktige varianter på 3 år.
+              med 40 dyktige varianter på 3 år.
             </p>
 
             <p>
@@ -62,38 +62,41 @@ export default function VariantTur() {
                     <td className={style.number}>40 varianter</td>
                   </tr>
                   <tr>
-                    <td>Lønn daglig leder</td>
-                    <td className={style.number}>1 100 000 kr</td>
-                    <td className={style.number}>1 300 000 kr</td>
-                    <td className={style.number}>1 500 000 kr</td>
+                    <td>Lønn daglig leder (kr)</td>
+                    <td className={style.number}>1 100 000</td>
+                    <td className={style.number}>1 300 000</td>
+                    <td className={style.number}>1 500 000</td>
                   </tr>
                   <tr>
-                    <td>Omsetning (kr)</td>
-                    <td className={style.number}>3 200 000 kr</td>
-                    <td className={style.number}>25 000 000 kr</td>
-                    <td className={style.number}>60 000 000 kr</td>
+                    <td>Omsetning (i 1000 kr)</td>
+                    <td className={style.number}>3 200</td>
+                    <td className={style.number}>25 000</td>
+                    <td className={style.number}>60 000</td>
                   </tr>
                   <tr>
-                    <td>Resultat før skatt</td>
-                    <td className={style.number}>191 000 kr</td>
-                    <td className={style.number}>3 800 000 kr</td>
-                    <td className={style.number}>7 200 000 kr</td>
+                    <td>Resultat før skatt (i 1000 kr)</td>
+                    <td className={style.number}>191</td>
+                    <td className={style.number}>3 800</td>
+                    <td className={style.number}>7 200</td>
                   </tr>
                   <tr>
-                    <td>Totalt utbytte for selskapet</td>
-                    <td className={style.number}>100 000 kr</td>
-                    <td className={style.number}>2 600 000 kr</td>
-                    <td className={style.number}>5 100 000 kr</td>
+                    <td>Totalt utbytte for selskapet (i 1000 kr)</td>
+                    <td className={style.number}>100</td>
+                    <td className={style.number}>2 600</td>
+                    <td className={style.number}>5 100</td>
                   </tr>
                   <tr>
-                    <td>Selskapets totale verdiutvikling</td>
-                    <td className={style.number}>8 000 000 kr</td>
-                    <td className={style.number}>30 000 000 kr</td>
-                    <td className={style.number}>75 000 000 kr</td>
+                    <td>Selskapets totale verdiutvikling (i 1000 kr)</td>
+                    <td className={style.number}>8 000</td>
+                    <td className={style.number}>30 000</td>
+                    <td className={style.number}>75 000</td>
                   </tr>
                 </tbody>
               </table>
             </div>
+            <p>
+              Vi er ambisiøse og søker deg som også har lyst til å oppnå noe. 
+            </p>
           </div>
         </div>
       </div>
