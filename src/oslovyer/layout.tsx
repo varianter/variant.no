436707addf4ec045a46134a8de4cbcea4fb86bde
Mikael Brevik <mikaelbre@gmail.com>
2021-02-12 13:50:01 +0100

    feat: adds oslovyer subpage

---
+++ src/oslovyer/layout.tsx
@@ -0,0 +1,33 @@
+import Head from 'next/head';
+import React, { PropsWithChildren } from 'react';
+
+import './app.module.css';
+
+const favicon = require('@variant/profile/lib/logo/favicon.png');
+
+type LayoutProps = PropsWithChildren<{
+  title?: string;
+}>;
+
+export default function Layout({ children, title = 'Variant' }: LayoutProps) {
+  return (
+    <main>
+      <Head>
+        <title>{title}</title>
+        <link rel="icon" href={favicon} />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@variant_as" />
+        <meta property="og:title" content={title} />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.variant.no/oslovyer" />
+        <meta
+          property="og:image"
+          content="https://www.variant.no/og-header-min.png"
+        />
+      </Head>
+
+      {children}
+    </main>
+  );
+}
