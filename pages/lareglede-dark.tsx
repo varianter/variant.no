7a6da8cf56a5bd473a2116f51c4fc95607b9477f
Mikael Brevik <mikaelbre@gmail.com>
2021-03-15 16:13:46 +0100

    adds dark logo

---
+++ pages/lareglede-dark.tsx
@@ -0,0 +1,11 @@
+export default function Logo() {
+  return (
+    <div
+      style={{
+        background: 'url(/lareglede-dark.jpg) center center no-repeat',
+        backgroundSize: 'cover',
+        height: '100vh',
+      }}
+    ></div>
+  );
+}
