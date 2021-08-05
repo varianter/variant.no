e5430398d9c36a54260c7d2548811f8f98d970e3
Tore Stensaker Tefre <tst@variant.no>
2021-08-05 10:06:18 +0200

    add white logo with link to top of page

--- src/summersplash/index.tsx
+++ src/summersplash/index.tsx
@@ -1,8 +1,8 @@
 // Based on existing Jobs page
 
 import React from 'react';
-import Layout from 'src/layout';
 import Head from 'next/head';
+import Link from 'next/link';
 
 import style from './index.module.css';
 import { NextPage, InferGetStaticPropsType } from 'next';
@@ -11,47 +11,61 @@ import { ButtonNextLink } from 'src/components/button';
 import { BaseBlob } from '@variant/components/lib/blob';
 import { colors } from '@variant/profile';
 
-const SummerSplash: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = () => {
+const SummerSplash: NextPage<
+  InferGetStaticPropsType<typeof getStaticProps>
+> = () => {
   return (
     <>
-        <Head>
-          <meta
-            property="og:url"
-            content={'https://www.variant.no/jobs/'}
-            key="og:url"
-          />
-          <meta
-            property="og:title"
-            content="Bli en variant! Se på ledige stillinger i Trondheim og Oslo."
-            key="og:title"
-          />
-          <meta
-            property="og:description"
-            content="Vi trenger mennesker som engasjerer seg for design og utvikling. Vi ønsker å samle mennesker som bryr seg og vil skape løsninger som tjener samfunnet vi lever i."
-            key="og:description"
-          />
-          <meta
-            name="description"
-            content="Vi trenger mennesker som engasjerer seg for design og utvikling. Vi ønsker å samle mennesker som bryr seg og vil skape løsninger som tjener samfunnet vi lever i."
-            key="description"
-          />
-          <meta
-            property="og:image"
-            content="https://www.variant.no/images/jobs_meta_image.png"
-            key="og:image"
-          />
-          <title>Variant - Din neste sommerjobb?</title>
-        </Head>
-        <section className={style.omVariant}>
-          <h2 className={style.omVariant__title}>Sommerjobb 2022</h2>
+      <Head>
+        <meta
+          property="og:url"
+          content={'https://www.variant.no/jobs/'}
+          key="og:url"
+        />
+        <meta
+          property="og:title"
+          content="Bli en variant! Se på ledige stillinger i Trondheim og Oslo."
+          key="og:title"
+        />
+        <meta
+          property="og:description"
+          content="Vi trenger mennesker som engasjerer seg for design og utvikling. Vi ønsker å samle mennesker som bryr seg og vil skape løsninger som tjener samfunnet vi lever i."
+          key="og:description"
+        />
+        <meta
+          name="description"
+          content="Vi trenger mennesker som engasjerer seg for design og utvikling. Vi ønsker å samle mennesker som bryr seg og vil skape løsninger som tjener samfunnet vi lever i."
+          key="description"
+        />
+        <meta
+          property="og:image"
+          content="https://www.variant.no/images/jobs_meta_image.png"
+          key="og:image"
+        />
+        <title>Variant - Din neste sommerjobb?</title>
+      </Head>
+      <section className={style.splashScrollContainer}>
+        <section className={style.splashSection}>
+          <h1 className={style.header__logo}>
+            <Link href="/">
+              <a>
+                <img
+                  src={require('./images/variant-white.svg')}
+                  alt="Variant"
+                />
+                <p>Gå til ordinær hjemmeside</p>
+              </a>
+            </Link>
+          </h1>
+          <h1 className={style.omVariant__title}>Sommerjobb 2022</h1>
           <p>Søk innen 12. oktober</p>
           <article className={style.omVariant__wrapper}>
-            <div className={style.omVariant__blob}>
+            <div className={style.omVaria0nt__blob}>
               <BaseBlob
                 seed="Variant"
                 width={350}
                 height={350}
-                color={colors.colorPairs.secondary3.default.bg}
+                color={colors.colorPairs.secondary2.default.bg}
                 imageProps={{ src: require('src/jobs/images/undraw_lynx.png') }}
                 alt="Dame som holder smarttelefon som viser kode"
               />
@@ -66,7 +80,34 @@ const SummerSplash: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = (
               </p>
             </div>
           </article>
+          <BaseBlob
+            seed="Variant"
+            width={1000}
+            height={1000}
+            color={'#E61A6B'}
+          />
         </section>
+
+        <section className={style.splashSection}>
+          <h2>Seksjon</h2>
+        </section>
+
+        <section className={style.splashSection}>
+          <h2>Seksjon</h2>
+        </section>
+
+        <section className={style.splashSection}>
+          <h2>Seksjon</h2>
+        </section>
+
+        <section className={style.splashSection}>
+          <h2>Seksjon</h2>
+        </section>
+
+        <section className={style.splashSection}>
+          <h2>Seksjon</h2>
+        </section>
+      </section>
     </>
   );
 };
