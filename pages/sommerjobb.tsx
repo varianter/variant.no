775de9c81c3b629d02dc64903cb1db2427adae7f
Tore Stensaker Tefre <tst@variant.no>
2021-08-24 09:43:22 +0200

    remove unused getStaticSideProps

--- pages/sommerjobb.tsx
+++ pages/sommerjobb.tsx
@@ -1,13 +1,3 @@
 // Based on existing Jobs page
 
-import { getFileListingData } from 'src/jobs/utils/getListings';
-
 export { default } from 'src/summersplash';
-
-export const getStaticProps = async () => {
-  const listings = await getFileListingData();
-  return {
-    props: { listings },
-    revalidate: 60 * 60,
-  };
-};
