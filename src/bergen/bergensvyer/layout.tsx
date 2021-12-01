a0a64ba9fae041e51403556fcc433e5b2387e2e0
Christian Brevik <christian.brevik@gmail.com>
2021-12-01 10:31:45 +0100

    Bergen open graph (#177)

--- src/bergen/bergensvyer/layout.tsx
+++ src/bergen/bergensvyer/layout.tsx
@@ -16,9 +16,23 @@ export default function Layout({ children, title = 'Variant' }: LayoutProps) {
 
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@variant_as" />
-        <meta property="og:title" content={title} />
+        <meta
+          property="og:title"
+          content="Hallaien! Vil du utvikle Bergen?"
+          key="og:title"
+        />
+        <meta
+          property="og:description"
+          content="Her har vi skrevet litt om våre tanker om å bygge Variant i Bergen. Nå er vi spente på hva du tenker."
+          key="og:description"
+        />
+        <meta
+          name="description"
+          content="Her har vi skrevet litt om våre tanker om å bygge Variant i Bergen. Nå er vi spente på hva du tenker."
+          key="description"
+        />
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://www.variant.no/oslovyer" />
+        <meta property="og:url" content="https://www.variant.no/bergen/vyer" />
         <meta
           property="og:image"
           content="https://www.variant.no/og-header-min.png"
