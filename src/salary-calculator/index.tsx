1be0424f93b3df6a078ef0568f9c6183fa7874cd
Mikael Brevik <mikaelbre@gmail.com>
2021-12-07 22:50:32 +0100

    fix: adds image preview of calculator

--- src/salary-calculator/index.tsx
+++ src/salary-calculator/index.tsx
@@ -27,6 +27,11 @@ const SalaryCalculator: NextPage<Props> = ({ year, degree, addition }) => {
           name="description"
           content="Variants åpne lønnskalkulator. Her kan du se lønnsnivået vi har og se hvilken lønn du kan få hos oss basert på din utdanning og utdanningsår."
         />
+        <meta
+          property="og:image"
+          content="https://www.variant.no/images/pay.jpg"
+          key="og:image"
+        />
       </Head>
 
       <div className={style.wrapper}>
