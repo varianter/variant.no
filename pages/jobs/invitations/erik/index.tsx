eed709408c8062c9351d38d55c2a08eb8aae4f79
Mikael Brevik <mikaelbre@gmail.com>
2022-01-14 08:43:15 +0100

    fix: adds text on Kristin and Marius and profile images

--- pages/jobs/invitations/erik/index.tsx
+++ pages/jobs/invitations/erik/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Layout from 'src/layout';
+import { MainInfo } from '../celie';
 import style from '../invitations.module.css';
 
 export default function Invitation() {
@@ -37,94 +38,8 @@ export default function Invitation() {
               </li>
             </ul>
           </div>
-          <div>
-            <h3 className="fancy">Styrkesamtale</h3>
-            <img
-              className={style.right_image}
-              src="/images/linn.png"
-              alt="Linn Sandvik"
-            />
-            <p>
-              Som du sikkert er klar over baserer vi vår medarbeiderutvikling på
-              positiv psykologi og legger styrkebasert utvikling til grunn for
-              hvordan vi ønsker bygge varianter. Første ledd i en slik utvikling
-              er å kartlegge våre styrker og våre verdier. Dette gjør vi for
-              alle mennesker i selskapet gjennom en styrkesamtale.
-            </p>
 
-            <p className={style.prep}>
-              Til samtalen ønsker vi at du tenker igjennom: I hvilke situasjoner
-              er du mest motivert? Hva gjør at du blir frustrert, irritert eller
-              sint? Hvordan liker du å løse utfordringer?
-            </p>
-            <p>
-              Linn kommer til å gjennomføre denne samtalen, og som et resultat
-              får du ditt eget styrkekart.
-            </p>
-          </div>
-
-          <div>
-            <h3 className="fancy">Samarbeidscase - Rekrutteringsstrategi</h3>
-            <p>
-              En forutsetning for å lykkes i Bergen er at vi lykkes med
-              rekrutteringen. Vi har derfor lyst til at å kjøre et
-              samarbeidscase på hvordan vi kan utvikle en god
-              rekrutteringsstrategi for Variant Bergen.
-            </p>
-            <img
-              className={style.right_image}
-              src="/images/mikael.png"
-              alt="Mikael Brevik"
-            />
-            <p className={style.prep}>
-              Til caset ønsker vi at du forbereder en innledning der du
-              forteller litt om dine tanker rundt Bergens rekruttering på kort
-              og lengre sikt. Format er helt opp til deg. Lengden bør ikke være
-              mer enn 10 minutter, og kort er bedre enn langt.
-            </p>
-            <img
-              className={style.right_image}
-              src="/images/anders.png"
-              alt="Anders Hammervold"
-            />
-            <p>
-              Etter din innledning vil du, Anders, Mikael og Odd Morten spinne
-              videre på dine tanker. Vi kommer til å utfordre eller male videre,
-              og ønsker at du gjør det samme. Altså slik vi faktisk kommer til å
-              jobbe når vi samarbeider om å bygge et selskap i framtiden. Idéer
-              som du bringer til torgs, skal vi ikke stjele.
-            </p>
-            <img
-              className={style.right_image}
-              src="/images/odd-morten.png"
-              alt="Odd Morten Sveås"
-            />
-            <p>
-              Vi tror samarbeidsevne og kompabilitet med resten av
-              konsernledelsen er en nøkkel til å lykkes. Hovedpoenget med denne
-              sesjonen er derfor å se på hvordan vi samarbeider. Hvordan vi
-              klarer å utnytte hverandres potensial. Hvordan ditt samspill i
-              gruppen vil være. Sluttproduktet, hvis vi kommer så langt, kommer
-              vi ikke til å evaluere.
-            </p>
-          </div>
-
-          <div>
-            <h3 className="fancy">Utlegg</h3>
-            <p>
-              Vi setter stor pris på at du er villig til å bruke en dag på oss
-              her i Trondheim. Vi dekker selvsagt kostnader til selve reisen. Vi
-              ber deg derfor å legge ut for turen, og så sende oss kopi av
-              kvitteringer sammen med bankkontonummer på mail til{' '}
-              <a
-                href="mailto:faktura.bergen@variant.no?subject=Utlegg i forbindelse med intervju&body=Vedlagt kvitteringer for min reise til Trondheim i forbindelse med intervju.%0D%0A
-                %0D%0A
-                Utbetales til kontonr:"
-              >
-                faktura.bergen@variant.no
-              </a>
-            </p>
-          </div>
+          <MainInfo />
         </section>
       </div>
     </Layout>
