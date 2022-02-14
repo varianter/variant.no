a400790ddc502c731748fcc216c23bbac0fbe876
Mikael Brevik <mikaelbre@gmail.com>
2022-02-14 19:50:25 +0100

    feat: adds filtering to jobs listing (#198)
    * feat: adds filtering to jobs listing
    
    * refactor: auto support more offices by dynamically checking
    
    * fix: avoid scrolling to top when navigating

--- pages/jobs/index.tsx
+++ pages/jobs/index.tsx
@@ -1,11 +1,20 @@
-import { getFileListingData } from 'src/jobs/utils/getListings';
+import { GetStaticProps } from 'next';
+import { getFileListingData, Listing } from 'src/jobs/utils/getListings';
+import {
+  Office,
+  stringToDepartment as stringToOffice,
+} from 'src/office-selector';
 
 export { default } from 'src/jobs';
 
-export const getStaticProps = async () => {
-  const listings = await getFileListingData();
+export const getStaticProps: GetStaticProps<{
+  listings: Listing[];
+  office?: Office;
+}> = async (context) => {
+  const office = stringToOffice(String(context.params?.office));
+  const listings = await getFileListingData(office);
   return {
-    props: { listings },
+    props: office ? { listings, office } : { listings },
     revalidate: 60 * 60,
   };
 };
