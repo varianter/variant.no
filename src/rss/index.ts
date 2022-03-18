9671cbc1b084b16c3a33e592cf7a49ae1efdf6d8
Mikael Brevik <mikaelbre@gmail.com>
2022-03-18 18:22:21 +0100

    refactor: export types directly

--- src/rss/index.ts
+++ src/rss/index.ts
@@ -1,3 +1,1 @@
-import { BlogItem, YoutubeVideoItem, PodcastItem} from './service';
-
-export type { BlogItem, YoutubeVideoItem, PodcastItem};
+export type { BlogItem, YoutubeVideoItem, PodcastItem } from './service';
