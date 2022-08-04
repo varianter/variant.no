f679078cdf0ffca84ec080bf782326e27710d575
TrymVei <tve@variant.no>
2022-08-04 10:03:24 +0200

    fix(Routing): added page sommersplash to pages route is /sommersplash

---
+++ pages/sommersplash.tsx
@@ -0,0 +1,1 @@
+export { default } from 'src/sommersplash2022'
---
+++ src/sommersplash2022/index.tsx
@@ -0,0 +1,12 @@
+import Content from './Content';
+import react from 'react';
+
+const SommerSplash2022 = () => {
+  return (
+    <>
+      <Content />
+    </>
+  );
+};
+
+export default SommerSplash2022;
