91a344519466c24e34c7c9f4b2c7a00ed3e726c7
Inge Hegge <ingehegg@hotmail.com>
2022-08-05 08:07:28 +0200

    feat: preparing for routing pages

--- src/summersplash2022/index.tsx
+++ src/summersplash2022/index.tsx
@@ -1,11 +1,85 @@
-import Content from './Content';
+// Based on the page from 2022
 
-const Summersplash2022 = () => {
-  return (
-    <>
-      <Content />
-    </>
-  );
+// NOTE: There are comments in the file that needs to be deleted. They are 
+// here only for explanation understandment for the writers. 
+
+// Library
+import React, {useEffect, useState} from 'react';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { NextPage, InferGetStaticPropsType} from 'next';
+import DefaultErrorPage from 'next/error';
+
+
+// Local
+import { getStaticProps } from 'pages';
+// import {JobOrInternship} from './utils/utils'
+import Layout from 'src/layout';
+import Content from './Content'
+import Link from 'next/link';
+
+
+const Summersplash2022: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = () => {
+
+
+
+	// When a file is added the(a) 'pages' directory it's automatically available 
+	// as a route.
+	const router = useRouter();
+
+	const queryedPage = router.query.dynmaicPath;
+
+	// New useState called 'mode'. 
+	// useState gives us two things: a variable to hold the state 'mode' and a
+	// function to change the state with 'setMode'
+	// A useState is a hook that helps us manage states in function based components
+	// A hook is something that lets us use states and other React features without writing a class
+	const [mode, setMode] = useState(
+		router.query.dynmaicPath === 'sommerjobb' ? 'internship' : 'job',
+	);
+
+	// useEffects are where we put the 'side effecs', and that means: "when 
+	// a components variables or state changes based on some outside thing"
+	// useEffect takes two agruments, the last one is an array to decide when
+	// the hook should run. If it should only run once, send in an empty array,
+	// here it uses the '[router.query.dynmaicPath]' (see it's in an array)
+	// ? Cause setMode don't need parameters the parentheses are empty, and the 
+	// fatarrow just means that we don't need to use the function keyword.
+	useEffect(() => {
+		setMode(router.query.dynmaicPath === 'sommerjobb' ? 'internship' : 'job');
+	}, [router.query.dynmaicPath]);
+
+	if (queryedPage === undefined) {
+		return null;
+	}
+	if (queryedPage != 'sommerjobb' && queryedPage != 'nyutdannet') {
+		return <DefaultErrorPage statusCode = {404}/>;
+	}
+
+
+	return (
+		<Layout>
+
+			<Head>
+				<meta
+					property='og:image'
+					content={'https://www.variant.no/jobs/'}
+					key='og:image'
+					/>
+			</Head>
+
+			{mode === 'intership'}
+
+			<section>
+				<Link href="/sommerjobb">
+				</Link>
+			</section>
+				<>
+					<Content selected={mode=== 'job' ? 'job' : 'internship'}/>
+				</>
+		</Layout>
+
+	);
 };
 
 export default Summersplash2022;
