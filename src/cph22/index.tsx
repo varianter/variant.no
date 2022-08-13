028fdfc45ff150e709cfd358b3e29416a0509e71
Adrian Santana Berg <adrian_berg96@hotmail.com>
2022-08-14 01:03:10 +0200

    feat: add lørdagstips

--- src/cph22/index.tsx
+++ src/cph22/index.tsx
@@ -1,22 +1,39 @@
 import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { useEffect, useMemo, useState } from 'react';
+import Tips from './tips';
 import style from './varianttur.module.css';
 
 export default function Cph22() {
-  const currentDate = new Date();
-  const formattedCurrentDate = currentDate
-    .getDate()
-    .toString()
-    .concat(
-      (currentDate.getMonth() + 1).toString(),
-      currentDate.getFullYear().toString(),
-    );
+  const router = useRouter();
+  const [showTips, setShowTips] = useState(false);
+  const [touchStart, setTouchStart] = useState(0);
+  const [touchEnd, setTouchEnd] = useState(0);
 
-  let currentProgramDay = 'fredag';
-  if (formattedCurrentDate === '392022') currentProgramDay = 'lordag';
-  else if (formattedCurrentDate === '492022') currentProgramDay = 'sondag';
+  useEffect(() => {
+    if (router.asPath.endsWith('#tips')) {
+      setShowTips(true);
+    }
+  }, [router]);
+
+  useEffect(() => {
+    if (showTips) {
+      router.replace('/cph22#tips');
+    } else {
+      router.replace('/cph22');
+    }
+  }, [showTips]);
+
+  const handleSwipe = () => {
+    if (touchEnd < touchStart - 100) {
+      setShowTips(true);
+    }
+  };
+
+  const currentProgramDay = useCurrentProgramDay();
 
   return (
-    <div className={style.container}>
+    <div className={style.container} data-showing-tips={showTips}>
       <Head>
         <title>Varianttur</title>
       </Head>
@@ -122,7 +139,17 @@ export default function Cph22() {
         </ul>
       </section>
 
-      <section className={style.agenda} id="lordag">
+      <section
+        className={style.agenda}
+        id="lordag"
+        onTouchStart={(e) => {
+          setTouchStart(e.touches[0].clientX);
+        }}
+        onTouchEnd={(e) => {
+          setTouchEnd(e.changedTouches[0].clientX);
+          handleSwipe();
+        }}
+      >
         <h3 className="fancy">Lørdag</h3>
         <ul>
           <li>
@@ -151,6 +178,50 @@ export default function Cph22() {
             </p>
           </li>
         </ul>
+
+        <button
+          className={style.tipsArrow}
+          onClick={() => setShowTips(!showTips)}
+        >
+          <p>Lørdagstips!</p>
+          <svg
+            width="81"
+            height="89"
+            viewBox="0 0 81 89"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g clipPath="url(#clip0_502_275)">
+              <path
+                d="M66.8387 48.5265C66.4181 52.8937 66.0022 57.2619 65.5709 61.6272C65.5279 62.0614 65.6318 62.2524 66.037 62.2787C66.4292 62.3044 66.827 62.3455 67.2102 62.4464C67.6989 62.5749 67.8916 62.4191 67.9956 61.8651C68.5878 58.7141 69.1469 55.5509 69.8569 52.4366C70.6879 48.7904 71.6622 45.1903 72.5736 41.57C72.6248 41.3668 72.6675 41.1607 72.7335 40.8708C72.4711 40.8473 72.2656 40.8126 72.0618 40.8134C65.7879 40.8426 59.5228 40.349 53.2615 39.6118C52.2985 39.4985 52.3925 39.389 52.2273 40.4673C51.9812 42.0773 51.9804 42.0644 53.4526 42.3824C57.1774 43.1871 60.9 44.0039 64.6237 44.8174C64.7225 44.839 64.8185 44.8819 64.9762 44.9356C64.868 45.0596 64.8084 45.1492 64.7312 45.2132C61.4572 47.9243 58.0875 50.4667 54.3324 52.3525C46.4753 56.2987 38.2387 57.5682 29.5575 55.5477C22.5331 53.9129 16.3172 50.3301 10.8095 45.1128C7.81856 42.2797 5.22554 39.0474 2.91685 35.5291C2.441 34.8037 2.43314 34.8092 1.80092 35.3137C1.44539 35.5971 1.10008 35.8974 0.734479 36.1648C0.353986 36.4432 0.359711 36.7142 0.636524 37.1331C2.63002 40.147 4.78523 43.0034 7.23303 45.5873C11.8555 50.4669 17.0893 54.2437 23.042 56.6927C32.6382 60.6409 42.1627 60.7138 51.5948 57.1631C55.3684 55.7428 58.9017 53.7973 62.1874 51.3443C63.5681 50.3136 64.9149 49.2286 66.2766 48.1677C66.4466 48.0352 66.6131 47.8973 66.781 47.762C66.8226 47.7877 66.8644 47.8135 66.9058 47.8395C66.8835 48.0684 66.8604 48.2973 66.8386 48.5266L66.8387 48.5265Z"
+                fill="white"
+              />
+              <path
+                d="M66.9061 47.8393C66.8836 48.0684 66.8605 48.2974 66.8387 48.5266C66.4181 52.8938 66.0022 57.262 65.5709 61.6273C65.5279 62.0615 65.6318 62.2525 66.037 62.2788C66.4292 62.3045 66.827 62.3456 67.2102 62.4465C67.6989 62.575 67.8916 62.4192 67.9956 61.8652C68.5878 58.7142 69.1469 55.551 69.8569 52.4367C70.6879 48.7905 71.6622 45.1904 72.5736 41.5701C72.6248 41.3669 72.6675 41.1608 72.7335 40.8709C72.4711 40.8474 72.2656 40.8127 72.0618 40.8135C65.7879 40.8427 59.5228 40.3491 53.2615 39.6119C52.2985 39.4986 52.3925 39.3891 52.2274 40.4674C51.9812 42.0774 51.9804 42.0645 53.4526 42.3825C57.1774 43.1872 60.9 44.004 64.6237 44.8174C64.7225 44.839 64.8185 44.8819 64.9762 44.9357C64.868 45.0597 64.8084 45.1493 64.7312 45.2133C61.4572 47.9244 58.0875 50.4668 54.3324 52.3526C46.4753 56.2988 38.2387 57.5683 29.5575 55.5478C22.5331 53.913 16.3172 50.3302 10.8095 45.1129C7.81856 42.2798 5.22554 39.0475 2.91685 35.5292C2.441 34.8038 2.43314 34.8093 1.80092 35.3138C1.44539 35.5972 1.10008 35.8975 0.734479 36.1649C0.353986 36.4433 0.359711 36.7143 0.636524 37.1332C2.63002 40.1471 4.78523 43.0035 7.23303 45.5874C11.8555 50.467 17.0893 54.2438 23.042 56.6928C32.6382 60.641 42.1627 60.7139 51.5948 57.1632C55.3684 55.7429 58.9017 53.7974 62.1874 51.3444C63.5681 50.3137 64.9149 49.2287 66.2766 48.1678C66.4466 48.0353 66.6131 47.8974 66.781 47.7621C66.8228 47.788 66.8646 47.8138 66.906 47.8394L66.9061 47.8393Z"
+                fill="white"
+              />
+            </g>
+            <defs>
+              <clipPath id="clip0_502_275">
+                <rect
+                  width="58.5851"
+                  height="62.7146"
+                  fill="white"
+                  transform="matrix(0.778716 -0.627377 0.558147 0.829742 0 36.755)"
+                />
+              </clipPath>
+            </defs>
+          </svg>
+        </button>
+
+        <button
+          className={style.tipsSwipe}
+          onClick={() => setShowTips(!showTips)}
+        >
+          <div className={style.bouncingArrow} />
+          <p>SVEIP FOR LØRDAGSTIPS!</p>
+          <div className={style.bouncingArrow} />
+        </button>
       </section>
 
       <section className={style.agenda} id="sondag">
@@ -237,6 +308,8 @@ export default function Cph22() {
       <section className={style.toTop}>
         <a href="#top">Ta meg til toppen</a>
       </section>
+
+      {showTips && <Tips setShowTips={setShowTips} />}
     </div>
   );
 }
@@ -286,3 +359,22 @@ const JippiBlob = () => {
     </>
   );
 };
+
+function useCurrentProgramDay() {
+  return useMemo(() => {
+    const currentDate = new Date();
+    const formattedCurrentDate = currentDate
+      .getDate()
+      .toString()
+      .concat(
+        (currentDate.getMonth() + 1).toString(),
+        currentDate.getFullYear().toString(),
+      );
+
+    let currentProgramDay = 'fredag';
+    if (formattedCurrentDate === '392022') currentProgramDay = 'lordag';
+    else if (formattedCurrentDate === '492022') currentProgramDay = 'sondag';
+
+    return currentProgramDay;
+  }, []);
+}
---
+++ src/cph22/tips/index.tsx
@@ -0,0 +1,104 @@
+import React from 'react';
+import style from './tips.module.css';
+
+export default function Tips({
+  setShowTips,
+}: {
+  setShowTips: (showTips: boolean) => void;
+}) {
+  return (
+    <div className={style.tips}>
+      <button className={style.back} onClick={() => setShowTips(false)}>
+        👈 Tilbake
+      </button>
+      <h1>Lørdagstips!</h1>
+      <p>
+        Vi har samlet noen av våre beste tips til hva vi kan bruke dagen på 👇
+      </p>
+
+      <section className={style.categories}>
+        <article className={style.category}>
+          <div>
+            <h2>Shopping 🛍️</h2>
+            <ul>
+              <li>Strøget</li>
+              <li>Studiestræde</li>
+              <li>Jægerborgsgade</li>
+            </ul>
+            <a
+              className={style.where}
+              href="https://www.google.com/maps/dir/Str%C3%B8get,+K%C3%B8benhavn,+Denmark/Studiestr%C3%A6de,+K%C3%B8benhavn,+Denmark/J%C3%A6gersborggade,+2200+K%C3%B8benhavn,+Denmark//@55.6858222,12.5411449,14z"
+              target="_blank"
+            >
+              Hvor finner jeg det på kartet? ↗️
+            </a>
+          </div>
+
+          <div className={style.categoryImage}>
+            <img src={require('./shopping.png')} />
+          </div>
+        </article>
+
+        <article className={style.reverseCategory}>
+          <div>
+            <h2>Kultur 🏛️</h2>
+
+            <ul>
+              <li>Designcenteret</li>
+              <li>Glyptoteket</li>
+              <li>Et av byens mange museum</li>
+              <li>Bibiloteket (Den Sorte Diamant)</li>
+            </ul>
+
+            <a
+              className={style.where}
+              href="https://www.google.com/maps/dir/DDC+–+Danish+Design+Center,+Bryghuspladsen+8,+Entrance+C,+2nd+floor+Blox,+1473+København,+Danmark/Glyptoteket,+Dantes+Plads,+København,+Danmark/Den+Sorte+Diamant,+Søren+Kierkegaards+Plads,+Københavns+kommune,+Danmark//@55.6732989,12.5695535,14.56z"
+              target="_blank"
+            >
+              Hvor finner jeg det på kartet? ↗️
+            </a>
+          </div>
+
+          <div className={style.categoryImage}>
+            <img src={require('./kultur.png')} />
+          </div>
+        </article>
+        <article className={style.category}>
+          <div>
+            <h2>Pusterom 🧘</h2>
+
+            <ul>
+              <li>Botanisk Have</li>
+              <li>Kongens Have</li>
+              <li>Christiania</li>
+            </ul>
+
+            <a
+              className={style.where}
+              href="https://www.google.com/maps/dir/Botanisk+have,+Gothersgade+128,+1123+K%C3%B8benhavn+K,+Danmark/Kongens+Have,+%C3%98ster+Voldgade+4A,+1350+K%C3%B8benhavn,+Danmark/Christiania,+K%C3%B8benhavns+kommune,+Danmark//@55.6809588,12.5757019,13.72z"
+              target="_blank"
+            >
+              Hvor finner jeg det på kartet? ↗️
+            </a>
+          </div>
+
+          <div className={style.categoryImage}>
+            <img src={require('./pusterom.png')} />
+          </div>
+        </article>
+      </section>
+      <article className={style.metro}>
+        <h2>Psst!</h2>
+        <p>
+          København har fått en ny metro, som går i ring rundt byen og har
+          holdeplass rett utenfor hotellet. Smart! 🚇
+        </p>
+      </article>
+      <button className={style.back} onClick={() => setShowTips(false)}>
+        👈 Tilbake
+      </button>
+    </div>
+  );
+}
+
+// https://www.google.com/maps/dir/33.93729,-106.85761/33.91629,-106.866761/33.98729,-106.85861//@34.0593359,-106.7131944,11z
