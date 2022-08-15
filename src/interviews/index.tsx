303d3895e9ca4815f5c96acc42cb52124b3f572e
Morten Nordseth <43166974+mortennordseth@users.noreply.github.com>
2022-08-15 11:13:43 +0200

    Intervjusider - Statens vegvesen #237 (#244)
    * Laget oppsett for intervju med varianter
    
    * fix font size
    
    * added interview with Caroline and Marthe
    
    * skråstilt tekst
    
    * Update caroline-svv.md
    
    Got some text updates from Caroline.
    
    * Update caroline-svv.md
    
    Fixed typo
    
    * new public folder for interview images
    
    * images are now fetched from cv-partner
    
    * Update marthe-svv.md
    
    Skriveleifer
    
    * Update andreas-svv.md
    
    Oppdatert tekst fra Andreas
    
    * mulighet for flere lokasjoner, forbedret tidsrom, la inn pil som svg
    
    * delte opp ordet trafikksikkerhetsinspeksjoner
    
    Co-authored-by: hildeor <hildeor@users.noreply.github.com>

---
+++ src/interviews/index.tsx
@@ -0,0 +1,114 @@
+import React, { useMemo } from 'react';
+import { getStaticProps } from 'pages/intervju/[interview]';
+import { Interview } from './utils/interviewHandlers';
+import MarkdownIt from 'markdown-it';
+import { NextPage, InferGetStaticPropsType } from 'next';
+import Head from 'next/head';
+import Layout from 'src/layout';
+import style from './index.module.css';
+import Link from 'next/link';
+import SayHi from 'src/index/say-hi';
+
+const createHtmlFromMetadata = (interview: Interview) => {
+  let locationsHtml = '';
+  interview.location.forEach((location: string) => {
+    locationsHtml += `<span>${location}</span>`;
+  });
+  let html =
+    '<div class="variant">' +
+    '<div class="imageContainer">' +
+    '<figure>' +
+    '<div class="img__decorationBox"></div>' +
+    '<div class="img__decorationBox"></div>' +
+    `<img src="${interview.image}" alt="${interview.imageAltText}"/>` +
+    '</figure>' +
+    '</div>' +
+    '<div>' +
+    `<div class="variant__location"><p>Lokasjon</p>${locationsHtml}</div>` +
+    `<div class="variant__duration"><p>Tid hos kunden</p><span>${
+      interview.durationFrom
+    } ${
+      interview.durationTill
+        ? '- ' + interview.durationTill
+        : '<img src="/images/arrow.svg" alt="Pil mot høyre"'
+    }</span></div>` +
+    '</div>' +
+    '</div>';
+
+  return html;
+};
+
+const Interview: NextPage<InferGetStaticPropsType<typeof getStaticProps>> =
+  React.memo(({ interview }) => {
+    const innerHtml = useMemo(() => {
+      const md = new MarkdownIt({
+        linkify: true,
+        html: true,
+        typographer: true,
+      });
+      let html = md.render(interview.content);
+
+      const htmlFromMetadata = createHtmlFromMetadata(interview);
+
+      return {
+        __html: html.replace('variant_info_placeholder', htmlFromMetadata),
+      };
+    }, [interview]);
+
+    return (
+      <Layout title={`${interview.meta_title}`}>
+        <Head>
+          <meta
+            property="og:url"
+            content={`https://www.variant.no/intervju/${interview.name}`}
+            key="og:url"
+          />
+          {interview.meta_title && (
+            <meta
+              property="og:title"
+              content={interview.meta_title}
+              key="og:title"
+            />
+          )}
+          {interview.meta_description && (
+            <>
+              <meta
+                name="description"
+                content={interview.meta_description}
+                key="description"
+              />
+              <meta
+                property="og:description"
+                content={interview.meta_description}
+                key="og:description"
+              />
+            </>
+          )}
+        </Head>
+
+        <SayHi
+          className={style.sayhi}
+          href="/jobs"
+          rel="noopener"
+          text="Bli en variant!"
+        />
+
+        <nav className={style.nav}>
+          <Link href={`/prosjekter/${interview.projectSlug}`}>
+            {interview.project}
+          </Link>
+          <span className={style.nav__arrow}>&gt;</span>
+          <span className={style.nav__subject}>{interview.variant}</span>
+        </nav>
+
+        <section className={style.interview}>
+          <article
+            className={style.rendered__markdown__wrapper}
+            dangerouslySetInnerHTML={innerHtml}
+          ></article>
+        </section>
+      </Layout>
+    );
+  });
+
+export default Interview;
