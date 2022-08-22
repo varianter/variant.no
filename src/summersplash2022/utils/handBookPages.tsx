517eb13e8d7ea7aadbf293b7ccd8e801cd8ba4d7
Inge Hegge <ingehegg@hotmail.com>
2022-08-22 16:13:31 +0200

    Fix: removed line breaks

--- src/summersplash2022/utils/handBookPages.tsx
+++ src/summersplash2022/utils/handBookPages.tsx
@@ -10,11 +10,9 @@ const HandBooKPages = ({ selectedButton }: WhichButtonPressed) => {
           Så, hvorfor Variant? Hvorfor er vi egentlig til? Vårt formål er å
           utvikle samfunnet vi lever i. Flinke personer som tenker nye tanker,
           og så lager de riktige løsningene.
-          <strong>
-            <br></br>
-            <br></br>
-            Våre tre grunnverdier er:
-          </strong>
+          <p className={style.handbookParagraphHeader}>
+            <strong>Våre tre grunnverdier er:</strong>
+          </p>
           <p>
             <strong>Raushet</strong> - Dette vises i hvordan vi møter hverandre,
             våre kunder og folk i nærmiljøet.
@@ -48,15 +46,15 @@ const HandBooKPages = ({ selectedButton }: WhichButtonPressed) => {
           nytt eller gjøre jobben din er opp til deg. Vi forventer bare at du
           selv vurderer kostnadene opp mot vinningen og sparrer med andre når
           det trengs.
-          <br></br>
-          <br></br>
+        </p>
+        <p className={style.positionTextSubLeft}>
           Ha det morsomt - Et arbeidsliv består av maaaange timer. Vi vil gjøre
           noe som gir mening og samtidig ha det morsomt. Det spekuleres i om
           Variant eksisterer fordi det er hyggeligere å skape noe sammen med
           mennesker man trives med. Det får vi nok ikke svaret på, men inntil
           videre så kan du le ofte og gjøre gærne ting!
-          <br></br>
-          <br></br>
+        </p>
+        <p className={style.positionTextSubLeft}>
           Vær stolt - Vi vil at du skal være stolt av det du gjør. Er du ikke
           den stolte typen? Da vil vi gjerne at du har mot til å endre på det.
           Du skal kunne være stolt av å være her, nettopp fordi du har gjort så
@@ -75,8 +73,9 @@ const HandBooKPages = ({ selectedButton }: WhichButtonPressed) => {
           hele dagen sammen, for faglig, administrativt og sosialt påfyll.
           Dersom du takker ja til et tilbud om sommerjobb hos oss er du
           selvfølgelig invitert til å bli med.
-          <br></br>
-          <br></br>
+        </p>
+
+        <p className={style.positionTextSubLeft}>
           Her vil du alltid finne oversikt over gjennomførte og planlagte
           variantdager.
         </p>
@@ -109,12 +108,14 @@ const HandBooKPages = ({ selectedButton }: WhichButtonPressed) => {
           på dette, for å bli en del av deres kultur. Vi oppnår mest når vi får
           jobbe tett med andre, så når du velger din arbeidsrytme regner vi med
           at det er med i vurderingen.
-          <br></br>
-          <br></br>
+        </p>
+
+        <p className={style.positionTextSubLeft}>
           Likevel er det slik at mange av våre kunder har kjernetid, eller at
           oppdraget du er på avtaler en kjernetid. Dette må vi forholde oss til.
-          <br></br>
-          <br></br>
+        </p>
+
+        <p className={style.positionTextSubLeft}>
           Vi vet at livet har ulike faser, i noen perioder ønsker eller trenger
           man å kunne bruke mindre tid på jobb. Hos oss er det mulig å senke
           stillingsgraden i perioder. Dette avgjør du best selv du. Med åpenhet
