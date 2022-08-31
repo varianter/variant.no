ce06b2b77f4ea1cf7abd03366b37e2058408c088
Mikael Brevik <mikaelbre@gmail.com>
2022-08-31 18:13:36 +0200

    fix: updates title for mangfold

--- src/components/page-title/index.tsx
+++ src/components/page-title/index.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { and } from 'src/utils/css';
 import style from './page-title.module.css';
 type PageTitleProps = {
   element?: 'h2' | 'h1';
   title: string;
+  bold?: boolean;
 };
-export default function PageTitle({ element = 'h1', title }: PageTitleProps) {
-  return React.createElement(element, { className: style.title }, [title]);
+export default function PageTitle({
+  element = 'h1',
+  title,
+  bold = false,
+}: PageTitleProps) {
+  return React.createElement(
+    element,
+    { className: and(style.title, bold ? style['title--bold'] : undefined) },
+    [title],
+  );
 }
