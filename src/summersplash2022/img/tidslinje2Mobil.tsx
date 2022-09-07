dcb0131c9b3cfeabc4502f1c273a000b99015eec
TrymVei <tve@variant.no>
2022-09-07 11:13:32 +0200

    fix: web-accessebility on Timeline over first year

--- src/summersplash2022/img/tidslinje2Mobil.tsx
+++ src/summersplash2022/img/tidslinje2Mobil.tsx
@@ -10,8 +10,14 @@ const TimeLine2Mobile = () => {
       height="1353"
       viewBox="0 0 720 1353"
       fill="none"
+      aria-labelledby="TimelineTitle TimelineDesc"
+      role="img"
     >
-      <title>
+      <title id="TimelineTitle">
+        Hvordan første året kommer til å se ut fra august til mai
+      </title>
+      <desc id="TimelineDesc">
+        {' '}
         August - Startskudd Alle nyutdannede samles til en oppstartsuke med
         faglig og sosialt innhold. Oktober - Designverktøy/Rammeverk Utviklerne
         jobber med HTML, CSS og JS, mens designerene går gjennom de vanligste
@@ -25,7 +31,7 @@ const TimeLine2Mobile = () => {
         skyløsninger og utfordringer knyttet til dette. Mai - Konsulentøkonomi
         De nyutdannede introduseres til konsulentøkonomi og økonomi i Variant.
         Dyktig(ere) konsulent
-      </title>
+      </desc>
       <path
         fillRule="evenodd"
         clipRule="evenodd"
