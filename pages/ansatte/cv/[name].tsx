ee609dbcac34e5ba3aa2c27f77d41fc63ba6cd0a
Tormod Haugene <th@variant.no>
2022-10-12 08:57:19 +0200

    Remove unused variable temporary

--- pages/ansatte/cv/[name].tsx
+++ pages/ansatte/cv/[name].tsx
@@ -164,14 +164,14 @@ const convertToCv = (cvJson: EmployeeCvJson): EmployeeCv => {
         title: cvJson.title.no,
         email: cvJson.email,
         imageUrl: cvJson.image.thumb.url,
-        summary: parseSummary(cvJson),
+        summary: parseSummary(),
         qualifications: parseTechnologies(cvJson),
         projects: parseProjects(cvJson),
         publications: parsePublications(cvJson),
     };
 }
 
-const parseSummary = (cvJson: EmployeeCvJson): string => {
+const parseSummary = (): string => {
     return "Hva er dette, og hvordan fungerer den? " + "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 }
 
