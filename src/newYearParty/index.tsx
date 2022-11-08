320fbb53dc5005b1d84bebe4170f293ff48f6240
TrymVei <tve@variant.no>
2022-11-08 10:27:57 +0100

    feat: added text

--- src/newYearParty/index.tsx
+++ src/newYearParty/index.tsx
@@ -15,6 +15,16 @@ const NewYearParty = () => {
           <p>Mesh Youngstorget</p>
           <p>27. Januar</p>
         </div>
+        <p className={style.newyearParty__invitation_ingress}>
+          Hei alle varianter, venner, familie, kollegaer og alle andre
+          interesserte! I Variant er vi er bygd på åpenhet, raushet og
+          læreglede. Dette er aldri noe vi bare skal si, men noe vi skal leve
+          opp til. Så i den anledning ønsker vi i Variant Oslo å invitere dere
+          alle på et storstilt nyttårskalas med underholdning, mat, drikke,
+          spill, musikk og moro i ekte Variant-ånd. Arrangementet er gratis, men
+          alle gjester må løse billett på Hoopla. Velkommen skal du være, vi
+          gleder oss til å treffe deg!
+        </p>
         <div className={style.newyearParty__invitation__table}>
           <div className={style.newyearParty__invitation__table_row}>
             <p>18.00</p>
