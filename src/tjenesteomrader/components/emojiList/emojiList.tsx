af093ef9c756e2040d0179786344dbbfe58c955b
TrymVei <tve@variant.no>
2022-11-23 09:51:40 +0100

    fix: added package to check if mobile. Change styling if mobile on emojilist

--- src/tjenesteomrader/components/emojiList/emojiList.tsx
+++ src/tjenesteomrader/components/emojiList/emojiList.tsx
@@ -1,27 +1,36 @@
+import { useMediaQuery } from 'react-responsive';
 import style from './emojiList.module.css';
 const EmojiList = (props: {
   emoji: string;
   listItem: string;
   reversed: boolean;
 }) => {
+  const isMobile = useMediaQuery({ query: `(max-width: 760px)` });
+
   return (
     <div
       className={style.emojiList}
       style={
-        props.reversed
+        props.reversed && !isMobile
           ? { justifyContent: 'flex-end' }
           : { justifyContent: 'flex-start' }
       }
     >
       <p
-        style={props.reversed ? { order: 1 } : { order: -1 }}
+        style={props.reversed && !isMobile ? { order: 1 } : { order: -1 }}
         className={style.emojiList__emoji}
       >
         {props.emoji}
       </p>
       <p
         className={style.emojiList__text}
-        style={props.reversed ? { textAlign: 'right' } : { textAlign: 'left' }}
+        style={
+          props.reversed && !isMobile
+            ? { textAlign: 'right' }
+            : isMobile
+            ? { textAlign: 'center' }
+            : { textAlign: 'start' }
+        }
       >
         {props.listItem}
       </p>
