7ddb0f65f54512ec1feb232ae20b4da123e9c714
TrymVei <tve@variant.no>
2022-11-24 09:57:30 +0100

    feat: arrows on datadriven
    fix: styling on datadriven three blobs

--- src/tjenesteomrader/areas/datadrevet/index.tsx
+++ src/tjenesteomrader/areas/datadrevet/index.tsx
@@ -2,11 +2,24 @@ import BlobText from 'src/tjenesteomrader/components/blobText/blobText';
 import EmojiList from 'src/tjenesteomrader/components/emojiList/emojiList';
 import TwoRows from 'src/tjenesteomrader/components/twoRows/twoRows';
 import HeaderBackground from 'src/tjenesteomrader/images/headerBackground/headerBackground';
+import { useMediaQuery } from 'react-responsive';
+
+// Arrows
+import down1 from 'src/tjenesteomrader/images/arrows/down1.svg';
+import down2 from 'src/tjenesteomrader/images/arrows/down2.svg';
+import down3 from 'src/tjenesteomrader/images/arrows/down3.svg';
+import down4 from 'src/tjenesteomrader/images/arrows/down4.svg';
+import right1 from 'src/tjenesteomrader/images/arrows/right1.svg';
+import right2 from 'src/tjenesteomrader/images/arrows/right2.svg';
+import left1 from 'src/tjenesteomrader/images/arrows/left1.svg';
+import left2 from 'src/tjenesteomrader/images/arrows/left2.svg';
 
 import style from 'src/tjenesteomrader/shared/index.module.css';
 import BottomText from './bottomText/bottomText';
 
 const Datadrevet = () => {
+  const isMobile = useMediaQuery({ query: `(max-width: 900px)` });
+
   return (
     <div>
       <title>Datadrevet</title>
@@ -22,19 +35,22 @@ const Datadrevet = () => {
               color={'#028377'}
               text={['Kunnskap til å ta bedre beslutninger']}
               blobNr={0}
+              threeBlobs={true}
             />
             <BlobText
               color={'#028377'}
               text={['Utvikling av helt nye produkter og tjenester']}
               blobNr={1}
+              threeBlobs={true}
             />
             <BlobText
               color={'#028377'}
               text={['Ny funksjonalitet i eksisterende produkter']}
               blobNr={2}
+              threeBlobs={true}
             />
           </div>
-
+          <img className={style.downArrow} src={down1} alt="donw 1 arrow" />
           <TwoRows>
             <div>
               <EmojiList
@@ -64,6 +80,19 @@ const Datadrevet = () => {
               blobNr={1}
             />
           </TwoRows>
+          {isMobile ? (
+            <img
+              className={style.sideArrowRight}
+              src={down2}
+              alt="left arrow 1"
+            />
+          ) : (
+            <img
+              className={style.sideArrowRight}
+              src={left1}
+              alt="down arrow 1"
+            />
+          )}
           <TwoRows>
             <BlobText
               color={'#028377'}
@@ -88,6 +117,19 @@ const Datadrevet = () => {
               />
             </div>
           </TwoRows>
+          {isMobile ? (
+            <img
+              className={style.sideArrowRight}
+              src={down3}
+              alt="left arrow 1"
+            />
+          ) : (
+            <img
+              className={style.sideArrowRight}
+              src={right1}
+              alt="down arrow 1"
+            />
+          )}
           <TwoRows>
             <div>
               <EmojiList
@@ -103,6 +145,19 @@ const Datadrevet = () => {
             </div>
             <BlobText color={'#028377'} text={['Kartlegging']} blobNr={3} />
           </TwoRows>
+          {isMobile ? (
+            <img
+              className={style.sideArrowRight}
+              src={down4}
+              alt="left arrow 1"
+            />
+          ) : (
+            <img
+              className={style.sideArrowRight}
+              src={left2}
+              alt="down arrow 1"
+            />
+          )}
           <TwoRows>
             <BlobText
               color={'#028377'}
--- src/tjenesteomrader/components/blobText/blobText.tsx
+++ src/tjenesteomrader/components/blobText/blobText.tsx
@@ -1,19 +1,35 @@
 import Blob from '../../images/blobs/blobs';
 import style from './blobText.module.css';
+import { useMediaQuery } from 'react-responsive';
 
 const BlobText = (props: {
   color: string;
   text: Array<string>;
   blobNr: number;
+  threeBlobs?: boolean;
 }) => {
+  const isMobile = useMediaQuery({ query: `(max-width: 900px)` });
+
   return (
     <div className={style.blobText}>
       <div className={style.blobText__blob}>
         <Blob BlobPathNr={props.blobNr} color={props.color} />
       </div>
-      <div className={style.blobText__textbox}>
+      <div
+        style={
+          props.threeBlobs ? { backgroundPosition: 'right: 0 left 0' } : {}
+        }
+        className={style.blobText__textbox}
+      >
         {props.text.map((paragraph) => {
-          return <p className={style.blobText__text}>{paragraph}</p>;
+          return (
+            <p
+              style={props.threeBlobs && !isMobile ? { maxWidth: '20rem' } : {}}
+              className={style.blobText__text}
+            >
+              {paragraph}
+            </p>
+          );
         })}
       </div>
     </div>
