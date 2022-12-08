87ade3dded30d2f57349d169d777f7d875f26c3f
TrymVei <tve@variant.no>
2022-12-08 09:02:42 +0100

    fix: updated props on components

--- src/tjenesteomrader/components/emojiList/emojiList.tsx
+++ src/tjenesteomrader/components/emojiList/emojiList.tsx
@@ -1,24 +1,27 @@
 import { and } from 'src/utils/css';
 import style from './emojiList.module.css';
-const EmojiList = (props: {
+
+type EmojiListProps = {
   listItems: Array<[string, string]>;
   inverted: boolean;
-}) => {
+};
+
+const EmojiList = ({ listItems, inverted }: EmojiListProps) => {
   return (
     <div>
-      {props.listItems.map((listItem, i) => {
+      {listItems.map((listItem, i) => {
         return (
           <div
             key={i}
             className={and(
               style.emojiList,
-              props.inverted ? style.emojiList__inverted : '',
+              inverted ? style.emojiList__inverted : '',
             )}
           >
             <p
               className={and(
                 style.emojiList__emoji,
-                props.inverted ? style.emojiList__emoji__inverted : ' ',
+                inverted ? style.emojiList__emoji__inverted : ' ',
               )}
             >
               {listItem[0]}
@@ -26,7 +29,7 @@ const EmojiList = (props: {
             <p
               className={and(
                 style.emojiList__text,
-                props.inverted ? style.emojiList__text__inverted : '',
+                inverted ? style.emojiList__text__inverted : '',
               )}
             >
               {listItem[1]}
