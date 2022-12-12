4581f531a33a247ba16f86c2a98d11a9e35c2832
TrymVei <tve@variant.no>
2022-12-12 16:41:51 +0100

    feat: added header to kultur

--- src/tjenesteomrader/areas/kultur/index.tsx
+++ src/tjenesteomrader/areas/kultur/index.tsx
@@ -1,9 +1,14 @@
 import { allColors } from '@variant/profile/lib/colors';
+import HeaderBackground from 'src/tjenesteomrader/images/headerBackground/headerBackground';
 import BottomText from './bottomText/bottomText';
 
 const Kultur = () => {
   return (
     <div>
+      <HeaderBackground
+        headerColor={allColors.secondary3__shade4}
+        headerText={'Kultur'}
+      />
       <BottomText footerColor={allColors.secondary3__shade4} />
     </div>
   );
