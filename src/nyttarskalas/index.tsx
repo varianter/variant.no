659bf22c1d49424fc5d8bbf6b348c06f5e222bdf
Tore Stensaker Tefre <toretefre@users.noreply.github.com>
2023-01-17 11:23:05 +0000

    add kalas mail trondheim (#351)
    * add kalas mail trondheim
    
    * add url for electronic letters

--- src/nyttarskalas/index.tsx
+++ src/nyttarskalas/index.tsx
@@ -28,8 +28,12 @@ const NewYearParty = () => {
           </p>
           <p className={style.newyearParty__invitation_ingress_paragraph}>
             Arrangementet er gratis, men alle gjester må løse billett på Hoopla.
-            Promokode for å hente ut billetter får du av en Variant eller i
-            invitasjonen du mottok. Vi gleder oss til å treffe deg!
+            Promokode for å hente ut billetter får du av en Variant, eller du
+            kan sende oss en e-post på{' '}
+            <a href="mailto:kalas-trondheim@variant.no">
+              kalas-trondheim@variant.no
+            </a>{' '}
+            Vi gleder oss til å treffe deg!
           </p>
         </div>
         <div className={style.newyearParty__invitation__table}>
