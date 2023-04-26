b820374e43bbe50fb12d2a0ed39f9c415b2d2a0c
TrymVei <tve@variant.no>
2023-04-26 12:29:42 +0200

    fix: accordionstyle

--- src/startskudd/components/buttons/accordianButton.tsx
+++ src/startskudd/components/buttons/accordianButton.tsx
@@ -1,27 +1,24 @@
-import React, { useState } from 'react'
-import style from "./buttons.module.css"
-
+import React, { useState } from 'react';
+import style from './buttons.module.css';
 
 type AccordianButtonProps = {
-    title: string;
-    text: string;
-  }
+  title: string;
+  text: string;
+};
 
-const AccordianButton = ({title, text}: AccordianButtonProps) => {
-  const [open, setOpen] = useState(false)
-  
+const AccordianButton = ({ title, text }: AccordianButtonProps) => {
+  const [open, setOpen] = useState(false);
+
   const handleOpen = () => {
-      setOpen(!open)
-    }
-  
-  return (
-    <button className={style.accordianButton} 
-         onClick={handleOpen}
-         >
-     <p>{title}</p>
-     {open && <p>{text}</p>}
-    </button>
-  )
-}
+    setOpen(!open);
+  };
 
-export default AccordianButton
+  return (
+    <button className={style.accordianButton} onClick={handleOpen}>
+      <p>{title}</p>
+      {open && <p className={style.accordianButton__text}>{text}</p>}
+    </button>
+  );
+};
+
+export default AccordianButton;
