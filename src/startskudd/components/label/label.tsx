9a35f948f4a3f2cd80bd5a6b1d3a1f7e1b35e7c5
TrymVei <tve@variant.no>
2023-04-26 08:30:33 +0200

    feat: label component

---
+++ src/startskudd/components/label/label.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+import React from 'react'
+import style from "./label.module.css"
+
+type LabelProps = {
+    children: string;
+  }
+
+export const Label = ({children}: LabelProps) => {
+  return (
+    <div className={style.label}>
+      <Link href={`#${children}`}>
+        <div className={style.label__button}>{children}</div>
+      </Link>      
+    </div>
+  )
+}
