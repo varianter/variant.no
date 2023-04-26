b14de94a6858a2e851411b784a3b0c628ad0e38f
TrymVei <tve@variant.no>
2023-04-26 14:59:29 +0200

    feat: desktop styling

--- src/startskudd/index.tsx
+++ src/startskudd/index.tsx
@@ -13,94 +13,95 @@ const Startskudd = () => {
     <div className={style.startskudd}>
       <WhiteText>
         <div className={style.startskudd__content}>
-          <div className={style.startskudd_right}>
-            <Label>Velkommen</Label>
-          </div>
-          <p id="Velkommen" className={style.startskudd__content_ingress}>
-            ❤️ Velkommen til
-          </p>
-          <h1 className={style.startskudd__h1}>Startskudd!!!</h1>
-          <p>
-            Vi gleder oss så masse at du er med på laget! Nedenfor finner du
-            svar på nesten alt du kunne lure på , fra praktisk info til program
-            og kontaktpersoner.
-          </p>
+          <section id="Velkommen">
+            <div className={style.startskudd_right}>
+              <Label>Velkommen</Label>
+            </div>
+            <p className={style.startskudd__content_ingress}>
+              ❤️ Velkommen til
+            </p>
+            <h1 className={style.startskudd__h1}>Startskudd!!!</h1>
+            <p>
+              Vi gleder oss så masse at du er med på laget! Nedenfor finner du
+              svar på nesten alt du kunne lure på , fra praktisk info til
+              program og kontaktpersoner.
+            </p>
+          </section>
 
-          <Flexbox>
-            <h2 id="Meny" className={style.startskudd__h2}>
-              🍽 Meny
-            </h2>
-            <Label>Meny</Label>
-          </Flexbox>
-          <Meny>
-            <MenyButton text="Praktisk info" emoji="📌" />
-            <MenyButton text="Kontakt" emoji="💬" />
-            <MenyButton text="Mandag" emoji="🐳" />
-            <MenyButton text="Tirsdag" emoji="🐥" />
-            <MenyButton text="Onsdag" emoji="🐽" />
-            <MenyButton text="Torsdag" emoji="🦧" />
-            <MenyButton text="Fredag" emoji="🦦" />
-          </Meny>
+          <section id="Meny">
+            <Flexbox>
+              <h2 className={style.startskudd__h2}>🍽 Meny</h2>
+              <Label>Meny</Label>
+            </Flexbox>
+            <Meny>
+              <MenyButton text="Praktisk info" emoji="📌" />
+              <MenyButton text="Kontakt" emoji="💬" />
+              <MenyButton text="Mandag" emoji="🐳" />
+              <MenyButton text="Tirsdag" emoji="🐥" />
+              <MenyButton text="Onsdag" emoji="🐽" />
+              <MenyButton text="Torsdag" emoji="🦧" />
+              <MenyButton text="Fredag" emoji="🦦" />
+            </Meny>
+          </section>
 
-          <Flexbox>
-            <h2 id="Praktisk info" className={style.startskudd__h2}>
-              📌 Praktisk info
-            </h2>
-            <Label>Praktisk info</Label>
-          </Flexbox>
+          <section id="Praktisk info">
+            <Flexbox>
+              <h2 className={style.startskudd__h2}>📌 Praktisk info</h2>
+              <Label>Praktisk info</Label>
+            </Flexbox>
+            <AccordianButton title="Lunsj" text="Lunsj" />
+            <AccordianButton title="start/sluttid" text="start/sluttid" />
+            <AccordianButton title="Lønn" text="Lønn" />
+            <AccordianButton title="Kunde" text="Kunde" />
+          </section>
 
-          <AccordianButton title="Lunsj" text="Lunsj" />
-          <AccordianButton title="start/sluttid" text="start/sluttid" />
-          <AccordianButton title="Lønn" text="Lønn" />
-          <AccordianButton title="Kunde" text="Kunde" />
+          <section id="Kontakt">
+            <Flexbox>
+              <h2 className={style.startskudd__h2}>💬 Kontakt</h2>
+              <Label>Kontakt</Label>
+            </Flexbox>
+            <Contact
+              firstName="Max"
+              surname="Mustermann"
+              email="mm@variant.no"
+              phoneNumber="+47 XXX XXX XX"
+            />
+            <Contact
+              firstName="Max"
+              surname="Mustermann"
+              email="mm@variant.no"
+              phoneNumber="+47 XXX XXX XX"
+            />
+            <Contact
+              firstName="Max"
+              surname="Mustermann"
+              email="mm@variant.no"
+              phoneNumber="+47 XXX XXX XX"
+            />
+          </section>
 
-          <Flexbox>
-            <h2 id="Kontakt" className={style.startskudd__h2}>
-              💬 Kontakt
-            </h2>
-            <Label>Kontakt</Label>
-          </Flexbox>
-          <Contact
-            firstName="Max"
-            surname="Mustermann"
-            email="mm@variant.no"
-            phoneNumber="+47 XXX XXX XX"
-          />
-          <Contact
-            firstName="Max"
-            surname="Mustermann"
-            email="mm@variant.no"
-            phoneNumber="+47 XXX XXX XX"
-          />
-          <Contact
-            firstName="Max"
-            surname="Mustermann"
-            email="mm@variant.no"
-            phoneNumber="+47 XXX XXX XX"
-          />
-
-          <Flexbox>
-            <h2 id="Mandag" className={style.startskudd__h2}>
-              🐳 Mandag 08.08.
-            </h2>
-            <Label>Mandag</Label>
-          </Flexbox>
-          <TimeSchedule time={'07:00 - 08:00'} activity={'Frokost'} />
-          <TimeSchedule
-            time={'08:00 - 09:00'}
-            activity={
-              'Keynote v/ Even: Tverrfaglig arbeid/kommunikasjon/presentasjon'
-            }
-          />
-          <TimeSchedule time={'09:00 - 11:30'} activity={'Casearbeid'} />
-          <TimeSchedule time={'11:30 - 12:30'} activity={'Lunsj'} />
-          <TimeSchedule time={'12:30 - 16:00'} activity={'Casearbeid'} />
-          <TimeSchedule time={'16:00 - 17:00'} activity={'Pause'} />
-          <TimeSchedule time={'17:00 - 18:00'} activity={'Vidsyn v/ Tonje'} />
-          <TimeSchedule
-            time={'18:00 - 21:00'}
-            activity={'Felles middag & sosial happening'}
-          />
+          <section id="Mandag">
+            <Flexbox>
+              <h2 className={style.startskudd__h2}>🐳 Mandag 08.08.</h2>
+              <Label>Mandag</Label>
+            </Flexbox>
+            <TimeSchedule time={'07:00 - 08:00'} activity={'Frokost'} />
+            <TimeSchedule
+              time={'08:00 - 09:00'}
+              activity={
+                'Keynote v/ Even: Tverrfaglig arbeid/kommunikasjon/presentasjon'
+              }
+            />
+            <TimeSchedule time={'09:00 - 11:30'} activity={'Casearbeid'} />
+            <TimeSchedule time={'11:30 - 12:30'} activity={'Lunsj'} />
+            <TimeSchedule time={'12:30 - 16:00'} activity={'Casearbeid'} />
+            <TimeSchedule time={'16:00 - 17:00'} activity={'Pause'} />
+            <TimeSchedule time={'17:00 - 18:00'} activity={'Vidsyn v/ Tonje'} />
+            <TimeSchedule
+              time={'18:00 - 21:00'}
+              activity={'Felles middag & sosial happening'}
+            />
+          </section>
         </div>
       </WhiteText>
     </div>
