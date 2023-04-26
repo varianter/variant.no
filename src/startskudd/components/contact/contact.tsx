3c0196ef8069f1cc2bc4256653ddf2cdb7923c4b
TrymVei <tve@variant.no>
2023-04-26 12:30:46 +0200

    feat: contact component

---
+++ src/startskudd/components/contact/contact.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import style from "./contact.module.css"
+
+type ContactProps = {
+    firstName: string;
+    surname: string;
+    email: string;
+    phoneNumber: string;
+  }
+
+export const Contact = ({firstName, surname, email,phoneNumber}: ContactProps) => {
+  return (
+    <div className={style.contact}>
+      <div className={style.contact_left}>
+        <p>{firstName}</p>
+        <p>{surname}</p>
+      </div>
+      <div className={style.contact_right}>
+        <p>{email}</p>
+        <p>{phoneNumber}</p>
+      </div>
+    </div>
+  )
+}
