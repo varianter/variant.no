7ee1d4da63f17b3a3826aaee4a80dedb7aebc61d
TrymVei <tve@variant.no>
2023-04-27 08:19:47 +0200

    feat: added rest of the days

--- src/startskudd/index.tsx
+++ src/startskudd/index.tsx
@@ -106,6 +106,98 @@ const Startskudd = () => {
               activity={'Felles middag & sosial happening'}
             />
           </section>
+
+          <section id="Tirsdag">
+            <Flexbox>
+              <h2 className={style.startskudd__h2}>🐥 Tirsdag 09.08.</h2>
+              <Label>Tirsdag</Label>
+            </Flexbox>
+            <TimeSchedule time={'07:00 - 08:00'} activity={'Frokost'} />
+            <TimeSchedule
+              time={'08:00 - 09:00'}
+              activity={
+                'Keynote v/ Even: Tverrfaglig arbeid/kommunikasjon/presentasjon'
+              }
+            />
+            <TimeSchedule time={'09:00 - 11:30'} activity={'Casearbeid'} />
+            <TimeSchedule time={'11:30 - 12:30'} activity={'Lunsj'} />
+            <TimeSchedule time={'12:30 - 16:00'} activity={'Casearbeid'} />
+            <TimeSchedule time={'16:00 - 17:00'} activity={'Pause'} />
+            <TimeSchedule time={'17:00 - 18:00'} activity={'Vidsyn v/ Tonje'} />
+            <TimeSchedule
+              time={'18:00 - 21:00'}
+              activity={'Felles middag & sosial happening'}
+            />
+          </section>
+
+          <section id="Onsdag">
+            <Flexbox>
+              <h2 className={style.startskudd__h2}>🐽 Onsdag 10.08.</h2>
+              <Label>Onsdag</Label>
+            </Flexbox>
+            <TimeSchedule time={'07:00 - 08:00'} activity={'Frokost'} />
+            <TimeSchedule
+              time={'08:00 - 09:00'}
+              activity={
+                'Keynote v/ Even: Tverrfaglig arbeid/kommunikasjon/presentasjon'
+              }
+            />
+            <TimeSchedule time={'09:00 - 11:30'} activity={'Casearbeid'} />
+            <TimeSchedule time={'11:30 - 12:30'} activity={'Lunsj'} />
+            <TimeSchedule time={'12:30 - 16:00'} activity={'Casearbeid'} />
+            <TimeSchedule time={'16:00 - 17:00'} activity={'Pause'} />
+            <TimeSchedule time={'17:00 - 18:00'} activity={'Vidsyn v/ Tonje'} />
+            <TimeSchedule
+              time={'18:00 - 21:00'}
+              activity={'Felles middag & sosial happening'}
+            />
+          </section>
+
+          <section id="Torsdag">
+            <Flexbox>
+              <h2 className={style.startskudd__h2}>🦧 Torsdag 11.08.</h2>
+              <Label>Torsdag</Label>
+            </Flexbox>
+            <TimeSchedule time={'07:00 - 08:00'} activity={'Frokost'} />
+            <TimeSchedule
+              time={'08:00 - 09:00'}
+              activity={
+                'Keynote v/ Even: Tverrfaglig arbeid/kommunikasjon/presentasjon'
+              }
+            />
+            <TimeSchedule time={'09:00 - 11:30'} activity={'Casearbeid'} />
+            <TimeSchedule time={'11:30 - 12:30'} activity={'Lunsj'} />
+            <TimeSchedule time={'12:30 - 16:00'} activity={'Casearbeid'} />
+            <TimeSchedule time={'16:00 - 17:00'} activity={'Pause'} />
+            <TimeSchedule time={'17:00 - 18:00'} activity={'Vidsyn v/ Tonje'} />
+            <TimeSchedule
+              time={'18:00 - 21:00'}
+              activity={'Felles middag & sosial happening'}
+            />
+          </section>
+
+          <section id="Fredag">
+            <Flexbox>
+              <h2 className={style.startskudd__h2}>🦦 Fredag 12.08.</h2>
+              <Label>Fredag</Label>
+            </Flexbox>
+            <TimeSchedule time={'07:00 - 08:00'} activity={'Frokost'} />
+            <TimeSchedule
+              time={'08:00 - 09:00'}
+              activity={
+                'Keynote v/ Even: Tverrfaglig arbeid/kommunikasjon/presentasjon'
+              }
+            />
+            <TimeSchedule time={'09:00 - 11:30'} activity={'Casearbeid'} />
+            <TimeSchedule time={'11:30 - 12:30'} activity={'Lunsj'} />
+            <TimeSchedule time={'12:30 - 16:00'} activity={'Casearbeid'} />
+            <TimeSchedule time={'16:00 - 17:00'} activity={'Pause'} />
+            <TimeSchedule time={'17:00 - 18:00'} activity={'Vidsyn v/ Tonje'} />
+            <TimeSchedule
+              time={'18:00 - 21:00'}
+              activity={'Felles middag & sosial happening'}
+            />
+          </section>
         </div>
       </WhiteText>
     </div>
