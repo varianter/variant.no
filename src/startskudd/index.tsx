f99ae39425cef9fcfd9cb48f2b5f0cee570c8412
TrymVei <tve@variant.no>
2023-04-27 13:47:55 +0200

    fix: Styling for large screens

--- src/startskudd/index.tsx
+++ src/startskudd/index.tsx
@@ -17,188 +17,205 @@ const Startskudd = () => {
           <LinkButton href="/">👈 til variant.no</LinkButton>
           <LinkButton href={''}>Scroll 👉</LinkButton>
         </nav>
-        <div className={style.startskudd__content}>
-          <section id="Velkommen" className={style.startskudd__section1}>
-            <div className={style.startskudd_right}>
-              <Label>Velkommen</Label>
-            </div>
-            <p className={style.startskudd__content_ingress}>
-              ❤️ Velkommen til
-            </p>
-            <h1 className={style.startskudd__h1}>Startskudd!!!</h1>
-            <p>
-              Vi gleder oss så masse at du er med på laget! Nedenfor finner du
-              svar på nesten alt du kunne lure på , fra praktisk info til
-              program og kontaktpersoner.
-            </p>
-          </section>
+        <div className={style.startskudd__wrapper}>
+          <div className={style.startskudd__content}>
+            <section id="Velkommen" className={style.startskudd__section1}>
+              <div className={style.startskudd_right}>
+                <Label>Velkommen</Label>
+              </div>
+              <p className={style.startskudd__content_ingress}>
+                ❤️ Velkommen til
+              </p>
+              <h1 className={style.startskudd__h1}>Startskudd!!!</h1>
+              <p>
+                Vi gleder oss så masse at du er med på laget! Nedenfor finner du
+                svar på nesten alt du kunne lure på , fra praktisk info til
+                program og kontaktpersoner.
+              </p>
+            </section>
 
-          <section id="Meny">
-            <Flexbox>
-              <h2 className={style.startskudd__h2}>🍽 Meny</h2>
-              <Label>Meny</Label>
-            </Flexbox>
-            <Meny>
-              <MenyButton text="Praktisk info" emoji="📌" />
-              <MenyButton text="Kontakt" emoji="💬" />
-              <MenyButton text="Mandag" emoji="🐳" />
-              <MenyButton text="Tirsdag" emoji="🐥" />
-              <MenyButton text="Onsdag" emoji="🐽" />
-              <MenyButton text="Torsdag" emoji="🦧" />
-              <MenyButton text="Fredag" emoji="🦦" />
-            </Meny>
-          </section>
+            <section id="Meny">
+              <Flexbox>
+                <h2 className={style.startskudd__h2}>🍽 Meny</h2>
+                <Label>Meny</Label>
+              </Flexbox>
+              <Meny>
+                <MenyButton text="Praktisk info" emoji="📌" />
+                <MenyButton text="Kontakt" emoji="💬" />
+                <MenyButton text="Mandag" emoji="🐳" />
+                <MenyButton text="Tirsdag" emoji="🐥" />
+                <MenyButton text="Onsdag" emoji="🐽" />
+                <MenyButton text="Torsdag" emoji="🦧" />
+                <MenyButton text="Fredag" emoji="🦦" />
+              </Meny>
+            </section>
 
-          <section id="Praktisk info">
-            <Flexbox>
-              <h2 className={style.startskudd__h2}>📌 Praktisk info</h2>
-              <Label>Praktisk info</Label>
-            </Flexbox>
-            <AccordianButton title="Lunsj" text="Lunsj" />
-            <AccordianButton title="start/sluttid" text="start/sluttid" />
-            <AccordianButton title="Lønn" text="Lønn" />
-            <AccordianButton title="Kunde" text="Kunde" />
-          </section>
+            <section id="Praktisk info">
+              <Flexbox>
+                <h2 className={style.startskudd__h2}>📌 Praktisk info</h2>
+                <Label>Praktisk info</Label>
+              </Flexbox>
+              <AccordianButton title="Lunsj" text="Lunsj" />
+              <AccordianButton title="start/sluttid" text="start/sluttid" />
+              <AccordianButton title="Lønn" text="Lønn" />
+              <AccordianButton title="Kunde" text="Kunde" />
+            </section>
 
-          <section id="Kontakt">
-            <Flexbox>
-              <h2 className={style.startskudd__h2}>💬 Kontakt</h2>
-              <Label>Kontakt</Label>
-            </Flexbox>
-            <Contact
-              firstName="Max"
-              surname="Mustermann"
-              email="mm@variant.no"
-              phoneNumber="+47 XXX XXX XX"
-            />
-            <Contact
-              firstName="Max"
-              surname="Mustermann"
-              email="mm@variant.no"
-              phoneNumber="+47 XXX XXX XX"
-            />
-            <Contact
-              firstName="Max"
-              surname="Mustermann"
-              email="mm@variant.no"
-              phoneNumber="+47 XXX XXX XX"
-            />
-          </section>
+            <section id="Kontakt" className={style.test}>
+              <Flexbox>
+                <h2 className={style.startskudd__h2}>💬 Kontakt</h2>
+                <Label>Kontakt</Label>
+              </Flexbox>
+              <Contact
+                firstName="Max"
+                surname="Mustermann"
+                email="mm@variant.no"
+                phoneNumber="+47 XXX XXX XX"
+              />
+              <Contact
+                firstName="Max"
+                surname="Mustermann"
+                email="mm@variant.no"
+                phoneNumber="+47 XXX XXX XX"
+              />
+              <Contact
+                firstName="Max"
+                surname="Mustermann"
+                email="mm@variant.no"
+                phoneNumber="+47 XXX XXX XX"
+              />
+            </section>
 
-          <section id="Mandag">
-            <Flexbox>
-              <h2 className={style.startskudd__h2}>🐳 Mandag 08.08.</h2>
-              <Label>Mandag</Label>
-            </Flexbox>
-            <TimeSchedule time={'07:00 - 08:00'} activity={'Frokost'} />
-            <TimeSchedule
-              time={'08:00 - 09:00'}
-              activity={
-                'Keynote v/ Even: Tverrfaglig arbeid/kommunikasjon/presentasjon'
-              }
-            />
-            <TimeSchedule time={'09:00 - 11:30'} activity={'Casearbeid'} />
-            <TimeSchedule time={'11:30 - 12:30'} activity={'Lunsj'} />
-            <TimeSchedule time={'12:30 - 16:00'} activity={'Casearbeid'} />
-            <TimeSchedule time={'16:00 - 17:00'} activity={'Pause'} />
-            <TimeSchedule time={'17:00 - 18:00'} activity={'Vidsyn v/ Tonje'} />
-            <TimeSchedule
-              time={'18:00 - 21:00'}
-              activity={'Felles middag & sosial happening'}
-            />
-          </section>
+            <section id="Mandag">
+              <Flexbox>
+                <h2 className={style.startskudd__h2}>🐳 Mandag 08.08.</h2>
+                <Label>Mandag</Label>
+              </Flexbox>
+              <TimeSchedule time={'07:00 - 08:00'} activity={'Frokost'} />
+              <TimeSchedule
+                time={'08:00 - 09:00'}
+                activity={
+                  'Keynote v/ Even: Tverrfaglig arbeid/kommunikasjon/presentasjon'
+                }
+              />
+              <TimeSchedule time={'09:00 - 11:30'} activity={'Casearbeid'} />
+              <TimeSchedule time={'11:30 - 12:30'} activity={'Lunsj'} />
+              <TimeSchedule time={'12:30 - 16:00'} activity={'Casearbeid'} />
+              <TimeSchedule time={'16:00 - 17:00'} activity={'Pause'} />
+              <TimeSchedule
+                time={'17:00 - 18:00'}
+                activity={'Vidsyn v/ Tonje'}
+              />
+              <TimeSchedule
+                time={'18:00 - 21:00'}
+                activity={'Felles middag & sosial happening'}
+              />
+            </section>
 
-          <section id="Tirsdag">
-            <Flexbox>
-              <h2 className={style.startskudd__h2}>🐥 Tirsdag 09.08.</h2>
-              <Label>Tirsdag</Label>
-            </Flexbox>
-            <TimeSchedule time={'07:00 - 08:00'} activity={'Frokost'} />
-            <TimeSchedule
-              time={'08:00 - 09:00'}
-              activity={
-                'Keynote v/ Even: Tverrfaglig arbeid/kommunikasjon/presentasjon'
-              }
-            />
-            <TimeSchedule time={'09:00 - 11:30'} activity={'Casearbeid'} />
-            <TimeSchedule time={'11:30 - 12:30'} activity={'Lunsj'} />
-            <TimeSchedule time={'12:30 - 16:00'} activity={'Casearbeid'} />
-            <TimeSchedule time={'16:00 - 17:00'} activity={'Pause'} />
-            <TimeSchedule time={'17:00 - 18:00'} activity={'Vidsyn v/ Tonje'} />
-            <TimeSchedule
-              time={'18:00 - 21:00'}
-              activity={'Felles middag & sosial happening'}
-            />
-          </section>
+            <section id="Tirsdag">
+              <Flexbox>
+                <h2 className={style.startskudd__h2}>🐥 Tirsdag 09.08.</h2>
+                <Label>Tirsdag</Label>
+              </Flexbox>
+              <TimeSchedule time={'07:00 - 08:00'} activity={'Frokost'} />
+              <TimeSchedule
+                time={'08:00 - 09:00'}
+                activity={
+                  'Keynote v/ Even: Tverrfaglig arbeid/kommunikasjon/presentasjon'
+                }
+              />
+              <TimeSchedule time={'09:00 - 11:30'} activity={'Casearbeid'} />
+              <TimeSchedule time={'11:30 - 12:30'} activity={'Lunsj'} />
+              <TimeSchedule time={'12:30 - 16:00'} activity={'Casearbeid'} />
+              <TimeSchedule time={'16:00 - 17:00'} activity={'Pause'} />
+              <TimeSchedule
+                time={'17:00 - 18:00'}
+                activity={'Vidsyn v/ Tonje'}
+              />
+              <TimeSchedule
+                time={'18:00 - 21:00'}
+                activity={'Felles middag & sosial happening'}
+              />
+            </section>
 
-          <section id="Onsdag">
-            <Flexbox>
-              <h2 className={style.startskudd__h2}>🐽 Onsdag 10.08.</h2>
-              <Label>Onsdag</Label>
-            </Flexbox>
-            <TimeSchedule time={'07:00 - 08:00'} activity={'Frokost'} />
-            <TimeSchedule
-              time={'08:00 - 09:00'}
-              activity={
-                'Keynote v/ Even: Tverrfaglig arbeid/kommunikasjon/presentasjon'
-              }
-            />
-            <TimeSchedule time={'09:00 - 11:30'} activity={'Casearbeid'} />
-            <TimeSchedule time={'11:30 - 12:30'} activity={'Lunsj'} />
-            <TimeSchedule time={'12:30 - 16:00'} activity={'Casearbeid'} />
-            <TimeSchedule time={'16:00 - 17:00'} activity={'Pause'} />
-            <TimeSchedule time={'17:00 - 18:00'} activity={'Vidsyn v/ Tonje'} />
-            <TimeSchedule
-              time={'18:00 - 21:00'}
-              activity={'Felles middag & sosial happening'}
-            />
-          </section>
+            <section id="Onsdag">
+              <Flexbox>
+                <h2 className={style.startskudd__h2}>🐽 Onsdag 10.08.</h2>
+                <Label>Onsdag</Label>
+              </Flexbox>
+              <TimeSchedule time={'07:00 - 08:00'} activity={'Frokost'} />
+              <TimeSchedule
+                time={'08:00 - 09:00'}
+                activity={
+                  'Keynote v/ Even: Tverrfaglig arbeid/kommunikasjon/presentasjon'
+                }
+              />
+              <TimeSchedule time={'09:00 - 11:30'} activity={'Casearbeid'} />
+              <TimeSchedule time={'11:30 - 12:30'} activity={'Lunsj'} />
+              <TimeSchedule time={'12:30 - 16:00'} activity={'Casearbeid'} />
+              <TimeSchedule time={'16:00 - 17:00'} activity={'Pause'} />
+              <TimeSchedule
+                time={'17:00 - 18:00'}
+                activity={'Vidsyn v/ Tonje'}
+              />
+              <TimeSchedule
+                time={'18:00 - 21:00'}
+                activity={'Felles middag & sosial happening'}
+              />
+            </section>
 
-          <section id="Torsdag">
-            <Flexbox>
-              <h2 className={style.startskudd__h2}>🦧 Torsdag 11.08.</h2>
-              <Label>Torsdag</Label>
-            </Flexbox>
-            <TimeSchedule time={'07:00 - 08:00'} activity={'Frokost'} />
-            <TimeSchedule
-              time={'08:00 - 09:00'}
-              activity={
-                'Keynote v/ Even: Tverrfaglig arbeid/kommunikasjon/presentasjon'
-              }
-            />
-            <TimeSchedule time={'09:00 - 11:30'} activity={'Casearbeid'} />
-            <TimeSchedule time={'11:30 - 12:30'} activity={'Lunsj'} />
-            <TimeSchedule time={'12:30 - 16:00'} activity={'Casearbeid'} />
-            <TimeSchedule time={'16:00 - 17:00'} activity={'Pause'} />
-            <TimeSchedule time={'17:00 - 18:00'} activity={'Vidsyn v/ Tonje'} />
-            <TimeSchedule
-              time={'18:00 - 21:00'}
-              activity={'Felles middag & sosial happening'}
-            />
-          </section>
+            <section id="Torsdag">
+              <Flexbox>
+                <h2 className={style.startskudd__h2}>🦧 Torsdag 11.08.</h2>
+                <Label>Torsdag</Label>
+              </Flexbox>
+              <TimeSchedule time={'07:00 - 08:00'} activity={'Frokost'} />
+              <TimeSchedule
+                time={'08:00 - 09:00'}
+                activity={
+                  'Keynote v/ Even: Tverrfaglig arbeid/kommunikasjon/presentasjon'
+                }
+              />
+              <TimeSchedule time={'09:00 - 11:30'} activity={'Casearbeid'} />
+              <TimeSchedule time={'11:30 - 12:30'} activity={'Lunsj'} />
+              <TimeSchedule time={'12:30 - 16:00'} activity={'Casearbeid'} />
+              <TimeSchedule time={'16:00 - 17:00'} activity={'Pause'} />
+              <TimeSchedule
+                time={'17:00 - 18:00'}
+                activity={'Vidsyn v/ Tonje'}
+              />
+              <TimeSchedule
+                time={'18:00 - 21:00'}
+                activity={'Felles middag & sosial happening'}
+              />
+            </section>
 
-          <section id="Fredag">
-            <Flexbox>
-              <h2 className={style.startskudd__h2}>🦦 Fredag 12.08.</h2>
-              <Label>Fredag</Label>
-            </Flexbox>
-            <TimeSchedule time={'07:00 - 08:00'} activity={'Frokost'} />
-            <TimeSchedule
-              time={'08:00 - 09:00'}
-              activity={
-                'Keynote v/ Even: Tverrfaglig arbeid/kommunikasjon/presentasjon'
-              }
-            />
-            <TimeSchedule time={'09:00 - 11:30'} activity={'Casearbeid'} />
-            <TimeSchedule time={'11:30 - 12:30'} activity={'Lunsj'} />
-            <TimeSchedule time={'12:30 - 16:00'} activity={'Casearbeid'} />
-            <TimeSchedule time={'16:00 - 17:00'} activity={'Pause'} />
-            <TimeSchedule time={'17:00 - 18:00'} activity={'Vidsyn v/ Tonje'} />
-            <TimeSchedule
-              time={'18:00 - 21:00'}
-              activity={'Felles middag & sosial happening'}
-            />
-          </section>
+            <section id="Fredag">
+              <Flexbox>
+                <h2 className={style.startskudd__h2}>🦦 Fredag 12.08.</h2>
+                <Label>Fredag</Label>
+              </Flexbox>
+              <TimeSchedule time={'07:00 - 08:00'} activity={'Frokost'} />
+              <TimeSchedule
+                time={'08:00 - 09:00'}
+                activity={
+                  'Keynote v/ Even: Tverrfaglig arbeid/kommunikasjon/presentasjon'
+                }
+              />
+              <TimeSchedule time={'09:00 - 11:30'} activity={'Casearbeid'} />
+              <TimeSchedule time={'11:30 - 12:30'} activity={'Lunsj'} />
+              <TimeSchedule time={'12:30 - 16:00'} activity={'Casearbeid'} />
+              <TimeSchedule time={'16:00 - 17:00'} activity={'Pause'} />
+              <TimeSchedule
+                time={'17:00 - 18:00'}
+                activity={'Vidsyn v/ Tonje'}
+              />
+              <TimeSchedule
+                time={'18:00 - 21:00'}
+                activity={'Felles middag & sosial happening'}
+              />
+            </section>
+          </div>
         </div>
       </WhiteText>
     </div>
