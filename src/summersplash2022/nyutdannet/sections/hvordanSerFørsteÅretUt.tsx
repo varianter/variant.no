4675b433316ce0e54f7ff7b2a0456d9fcda58171
Dora Oline Eriksrud <doraoline@gmail.com>
2023-07-20 16:43:27 +0200

    Update css for "how first year look like"

--- src/summersplash2022/nyutdannet/sections/hvordanSerFørsteÅretUt.tsx
+++ src/summersplash2022/nyutdannet/sections/hvordanSerFørsteÅretUt.tsx
@@ -1,26 +1,29 @@
 import style from '../nyutdannet.module.css';
-import TimeLine2 from '../img/tidslinje';
 import Section5Blob from '../img/section5Blob';
-import TimeLine2Mobile from 'src/summersplash2022/img/tidslinje2Mobil';
+import TimelineNarrow from 'src/summersplash2022/img/tidslinjeNarrow';
+import TimelineWide from 'src/summersplash2022/img/tidslinjeWide';
 import { useEffect, useState } from 'react';
 const FirstYear = () => {
   const mountain = require('src/summersplash2022/nyutdannet/img/MountainImg.png');
 
-  const [isDesktop, setIsDesktop] = useState(true);
+  const [showWideTimeline, setShowWideTimeline] = useState(true);
+
+  const checkIfNarrowOrWideTimeline = () => {
+    if (window.matchMedia('(max-width: 1310px) and (min-width: 801px)').matches) {
+      setShowWideTimeline(true);
+    } else {
+      setShowWideTimeline(false);
+    }
+  }
 
   const handleResize = () => {
-    if (window.matchMedia('(max-width: 1400px)').matches) {
-      setIsDesktop(false);
-    } else {
-      setIsDesktop(true);
-    }
+    console.log("Handle resize")
+    checkIfNarrowOrWideTimeline();
   };
 
   useEffect(() => {
     if (window) {
-      if (window.matchMedia('(max-width: 1400px)').matches) {
-        setIsDesktop(false);
-      }
+      checkIfNarrowOrWideTimeline();
       window.addEventListener('resize', handleResize);
     }
   }, []);
@@ -29,27 +32,39 @@ const FirstYear = () => {
     <>
       <section className={style.section5}>
         <h2 className={style.heading}>Hvordan ser første året ut?</h2>
-        <p>
-          For å bidra til personlig og faglig utvikling av nyutdannede
-          gjennomfører vi oppstartsprogrammet{' '}
-          <a href="https://handbook.variant.no/#Utviklingsprogram" style={{ color: 'white' }}>
-            variant:skudd
-          </a>
-          . Programmet gjennomføres i løpet av det første året den nyutdannede
-          jobber i Variant.{' '}
-        </p>
-        <div className={style.graphicContainer}>
-          {isDesktop ? <TimeLine2 /> : <TimeLine2Mobile />}
-          <div className={style.section5PictureDiv}>
-            <div className={style.section5Blob}>
-              <Section5Blob />
-            </div>
-            <img
-              className={style.section5Picture}
-              src={mountain}
-              alt="Mountaint trip in Oppdal"
-            />
+        <div className={style.section5Flex}>
+          <div className={style.section5Text}>
+            <p>
+              For å bidra til personlig og faglig utvikling av nyutdannede
+              gjennomfører vi oppstartsprogrammet{' '}
+              <a href="https://handbook.variant.no/#Utviklingsprogram" style={{ color: 'white' }}>
+                variant:skudd
+              </a>
+              . Programmet gjennomføres i løpet av det første året den nyutdannede
+              jobber i Variant.{' '}
+            </p>
+            <div className={style.section5PictureDiv}>
+              <div className={style.section5PictureWrap}>
+
+              <div className={style.section5BlobWrapper}>
+                <Section5Blob />
+              </div>
+              <img
+                className={style.section5Picture}
+                src={mountain}
+                alt="Mountaint trip in Oppdal"
+              />
+              </div>
           </div>
+        </div>
+        <div className={style.graphicContainer}>
+          { showWideTimeline && (
+            <TimelineWide />
+          )}
+          {!showWideTimeline && (
+            <TimelineNarrow />
+          )}
+        </div>
         </div>
       </section>
     </>
