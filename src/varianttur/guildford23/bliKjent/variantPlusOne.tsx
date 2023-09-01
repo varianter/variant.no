9b96c7702a9e4091c0cf7fd26706a70aee999c25
Ida Klemmetsrud Isaksen <iki@variant.no>
2023-09-01 09:28:00 +0200

    alternating background color on small screens

--- src/varianttur/guildford23/bliKjent/variantPlusOne.tsx
+++ src/varianttur/guildford23/bliKjent/variantPlusOne.tsx
@@ -4,7 +4,7 @@ import { VariantPlusOneModel } from './getToKnow';
 
 const VariantPlusOne = ({variant, variantIndex}: {variant: VariantPlusOneModel, variantIndex: number}) => {
   return (
-    <div className={style.variantContainer}>
+    <div className={variantIndex % 2 !== 0 ? style.variantContainer : style.reverseVariantContainer}>
       {variant.people.map((person, index) => <div key={index}><Person person={person} variantIndex={variantIndex} /></div>)}
     </div>
   );
