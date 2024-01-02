a96bf8c714e280c8b3ea44429fe195d1d9d814f6
Marius Krakeli <marius.krakeli@gmail.com>
2024-01-02 11:00:35 +0100

    Revert "Specify that Trondheim does not accept open applications (#430)"
    This reverts commit 8eccbbdb2f41c974e009c312ca474c04399ecf79.

--- src/jobs/open-application-listing-item.tsx
+++ src/jobs/open-application-listing-item.tsx
@@ -3,13 +3,12 @@ import { ButtonNextLink } from 'src/components/button';
 import { and } from 'src/utils/css';
 import style from './index.module.css';
 
-const emailSubject = "Åpen søknad - Oslo/Bergen";
+const emailSubject = "Åpen søknad - Oslo/Trondheim/Bergen";
 const emailBody = 
 `
 Så hyggelig at du ønsker å jobbe hos oss!
 
 Tips: Husk å oppdatere emnefeltet med hvilken by du ønsker å skrive til, så vet vi hvem som skal svare deg.
-PS: Kontoret i Trondheim tar for tiden ikke i mot åpne søknader.
 `;
 const mailtoLink = `mailto:soknad@variant.no?subject=${encodeURIComponent(emailSubject)}&body=${encodeURIComponent(emailBody)}`;
 
@@ -17,7 +16,7 @@ export default function OpenApplicationListingItem() {
   return (
     <section className={style.job__listing__container}>
       <div>
-        <h3 className={and(style.job__title, 'fancy')}>Åpen Søknad - Oslo, Bergen</h3>
+        <h3 className={and(style.job__title, 'fancy')}>Åpen Søknad</h3>
         <span>Finner du ikke en stilling som passer deg?</span>
       </div>
       <ButtonNextLink href={mailtoLink} aria-label="Send en åpen søknad">
