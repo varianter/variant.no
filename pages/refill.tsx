7214fccdb8741481346315c0c85f8ba29b588ffc
Mikael Brevik <mikaelbre@gmail.com>
2024-05-28 15:31:49 +0200

    feat: adds complete program

--- pages/refill.tsx
+++ pages/refill.tsx
@@ -94,9 +94,13 @@ export default function Refill() {
           </p>
 
           <div className={style.calendarContainer}>
-            <a href="/refill/event.ics" download="true">
+            <a href="/refill/event.ics" download>
               <CalendarIcon />
               Marker i kalender
+            </a>
+            <a href="/refill/program.pdf" download>
+              <DownloadIcont />
+              Last ned programmet
             </a>
           </div>
         </div>
@@ -151,6 +155,23 @@ function Speaker({ name, src }: ProgramItemProps['speaker']) {
       )}
       <h4>{name}</h4>
     </footer>
+  );
+}
+
+function DownloadIcont() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20px"
+      height="20px"
+      role="none"
+      viewBox="0 0 24 24"
+    >
+      <path
+        fill="currentColor"
+        d="m12 16l-5-5l1.4-1.45l2.6 2.6V4h2v8.15l2.6-2.6L17 11zm-6 4q-.825 0-1.412-.587T4 18v-3h2v3h12v-3h2v3q0 .825-.587 1.413T18 20z"
+      />
+    </svg>
   );
 }
 
