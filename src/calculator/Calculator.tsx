d29724098a6601be459f235bea471b397146b788
Mathias Oterhals Myklebust <mom@variant.no>
2024-08-13 15:17:09 +0200

    feat: dummy calculator

---
+++ src/calculator/Calculator.tsx
@@ -0,0 +1,11 @@
+import styles from './calculator.module.css';
+import Text from 'src/components/text/Text';
+
+export const Calculator = () => {
+  return (
+    <div className={styles.content}>
+      <Text>🤖 Beep boop, I&apos;m a calculator</Text>
+      <Text type={"small"}>1 + 1 = {1 + 1}</Text>
+    </div>
+  );
+};
