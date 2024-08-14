e8b1ffd931675b8bb486dcf09deaf04d977a41e7
Mathias Oterhals Myklebust <mom@variant.no>
2024-08-14 08:27:06 +0200

    chore: replace typography HTML tags with Text component

--- src/components/missingData/MissingData.tsx
+++ src/components/missingData/MissingData.tsx
@@ -1,7 +1,8 @@
 import LinkButton from '../linkButton/LinkButton';
 import { LinkType } from '../../../studio/lib/payloads/navigation';
+import Text from "src/components/text/Text";
 import styles from './missingData.module.css';
-import textStyles from "src/components/text/text.module.css";
+
 
 const studioLink = {
   _key: "go-to-sanity-studio",
@@ -16,12 +17,10 @@ const studioLink = {
 export const MissingData = ({ description }: { description: string }) => {
   return (
     <div className={styles.wrapper}>
-      <div className={`${textStyles.small} ${styles.container}`}>
-        <h3>Missing Content</h3>
-        <p>Navigate to Sanity Studio to add the following:</p>
-        <strong>
-          <pre>{description}</pre>
-        </strong>
+      <div className={`${styles.container}`}>
+        <Text type={"h3"}>Missing Content</Text>
+        <Text type={"small"}>Navigate to Sanity Studio to add the following:</Text>
+        <p className={styles.description}>{description}</p>
         <LinkButton link={studioLink} />
       </div>
     </div>
