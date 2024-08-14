baf6fb66e3e565d0585c929822a12b1dde066af4
Maria Bonde <mariabonde99@gmail.com>
2024-08-14 12:18:25 +0200

    hemsedal23 hero done

--- src/hemsedal24/components/Hero.tsx
+++ src/hemsedal24/components/Hero.tsx
@@ -1,28 +1,32 @@
 import styles from "./Hero.module.css";
-
+import {isMobile} from 'react-device-detect';
 import arrow from "../images/curved-arrow.svg";
 import s from "../images/s.svg"
 import Image from 'next/image';
 import blob from "../images/blob.svg";
-import HemsedalImage from "./hemsedalimgcurvedtext";
-import hemsedalhero1 from "../../../public/images/hemsedalhero1text.svg"
-import hemsedalHero2 from "../images/hemsedalhero2.png"
-import Container from "../../helsinki23/components/Container";
+import retreatmobile from "../images/retreatmobile.svg"
+import retreat from "../images/Retreatclub-fyri-solbaddar.svg"
+import hemsedalHero2 from "../images/hemsedalhero2.svg"
+import Container from "./Container";
+
 
 
 export default function Hero() {
   return <section id="hero">
     <Container className={styles.container}>
-    <HemsedalImage/>
-    {/* <img src={hemsedalhero1} alt="hemsedalbilde" className={styles.hemsedalhero1}/> */}
-    <img src={hemsedalHero2} alt="hemsedalbilde" className={styles.hemsedalhero2}/>
-        <div className={styles.blob}>
-          <img src={blob} alt="blobben" />
-        </div>
+
+    
+    <img src={isMobile ? retreatmobile : retreat} alt="hemsedalbilde" className={styles.hemsedalhero1}/>
+        <div className={styles.blobandtext}>
         <div className={styles.text}>
           <h1>Hemsedal</h1>
           <h2>30. august-1. september</h2>
         </div>
+        <div className={styles.blob}>
+          <img src={blob} alt="blobben" />
+        </div>
+        </div>
+        <img src={hemsedalHero2} alt="hemsedalbilde" className={styles.hemsedalhero2}/>
     </Container>
   </section>;
 }
