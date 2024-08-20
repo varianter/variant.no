770e5d12a10346dbd9ce65149441495e9dfadfd4
Mathias Oterhals Myklebust <mom@variant.no>
2024-08-20 09:49:35 +0200

    feat: salary and benefits schema
    Closes #483

---
+++ studio/schemas/documents/salaryAndBenefits.ts
@@ -0,0 +1,27 @@
+import { defineField, defineType } from 'sanity';
+import { pageSlug } from '../schemaTypes/slug';
+import seo from '../objects/seo';
+import { title } from '../fields/text';
+import { benefitId } from './benefit';
+
+export const salaryAndBenefitsId = "salaryAndBenefits";
+
+const salaryAndBenefits = defineType({
+  name: salaryAndBenefitsId,
+  type: "document",
+  title: "Salary and Benefits",
+  fields: [
+    title,
+    pageSlug,
+    seo,
+    defineField(({
+      name: "benefits",
+      title: "Benefits",
+      description: "Manage benefits for the salary and benefits page",
+      type: "array",
+      of: [{ type: benefitId }]
+    }))
+  ]
+});
+
+export default salaryAndBenefits;
