a660cb15ddc80baf1245409f37e8140fa1af1523
Mathias Oterhals Myklebust <mom@variant.no>
2024-08-21 12:58:20 +0200

    fix(SalaryAndBenefitsPreview): check unexpected page data type in development

--- src/salaryAndBenefits/SalaryAndBenefitsPreview.tsx
+++ src/salaryAndBenefits/SalaryAndBenefitsPreview.tsx
@@ -1,28 +1,37 @@
-"use client"
+"use client";
 import { Suspense } from "react";
 import SalaryAndBenefits from "./SalaryAndBenefits";
-import { QueryResponseInitial, useQuery } from '@sanity/react-loader';
-import {SalaryAndBenefits as SalaryAndBenefitsPayload} from "studio/lib/payloads/salaryAndBenefits";
-import { SALARY_AND_BENEFITS_PAGE_QUERY } from 'studio/lib/queries/pages';
-
+import { QueryResponseInitial, useQuery } from "@sanity/react-loader";
+import { SalaryAndBenefits as SalaryAndBenefitsPayload } from "studio/lib/payloads/salaryAndBenefits";
+import { SALARY_AND_BENEFITS_PAGE_QUERY } from "studio/lib/queries/pages";
 
 interface SalaryAndBenefitsPreviewProps {
   initialSalaryAndBenefits: QueryResponseInitial<SalaryAndBenefitsPayload>;
 }
 
-const SalaryAndBenefitsPreview = ({initialSalaryAndBenefits}: SalaryAndBenefitsPreviewProps) => {
-
-  const {data: newData } = useQuery<SalaryAndBenefitsPayload>(
+const SalaryAndBenefitsPreview = ({
+  initialSalaryAndBenefits,
+}: SalaryAndBenefitsPreviewProps) => {
+  const { data: newData } = useQuery<SalaryAndBenefitsPayload>(
     SALARY_AND_BENEFITS_PAGE_QUERY,
     { slug: initialSalaryAndBenefits.data.slug.current },
-    { initial: initialSalaryAndBenefits }
+    { initial: initialSalaryAndBenefits },
   );
+
+  if (process.env.NODE_ENV !== "production") {
+    if (typeof newData !== "object" || newData === null) {
+      console.error(
+        `newData has an unexpected type: ${typeof newData}. Expected an object.`,
+      );
+    }
+  }
+  const pageData = newData || initialSalaryAndBenefits.data;
 
   return (
     <Suspense>
-      <SalaryAndBenefits salaryAndBenefits={newData ?? initialSalaryAndBenefits.data} />
+      <SalaryAndBenefits salaryAndBenefits={pageData} />
     </Suspense>
-  )
-}
+  );
+};
 
 export default SalaryAndBenefitsPreview;
