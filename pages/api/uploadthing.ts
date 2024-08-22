ac884b717c2f31d0ff5205970b73549340dcee49
Maria Bonde <mariabonde99@gmail.com>
2024-08-22 14:29:48 +0200

    deleted from other branch

--- pages/api/uploadthing.ts
+++
@@ -1,8 +0,0 @@
-import { createRouteHandler } from "uploadthing/next-legacy";
-import { uploadRouter } from "../../src/server/uploadthing";
-
-const handler = createRouteHandler({
-  router: uploadRouter,
-});
-
-export default handler;
