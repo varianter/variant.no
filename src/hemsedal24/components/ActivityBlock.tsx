7a749bda3fcde220210bbc59eb0947b2b51c5dec
Maria Katrin Bonde <78419778+MariaBonde@users.noreply.github.com>
2024-08-23 16:30:55 +0200

    Lasterrorhemsedal24 (#515)
    * couldt do arrows:/
    
    * hopefully last commit now:)

--- src/hemsedal24/components/ActivityBlock.tsx
+++ src/hemsedal24/components/ActivityBlock.tsx
@@ -4,13 +4,13 @@ import Link from "next/link";
 import styles from "./ActivityBlock.module.css"
 import { ReactNode } from "react";
 
-export default function ActivityBlock({ title, text, link, reference, secondtext, picture}: { title: string, text: string, link: string, reference: string,secondtext:ReactNode,picture:string}) {
+export default function ActivityBlock({ title, text, link, reference, secondtext, picture}: { title: string, text: ReactNode, link: string, reference: string,secondtext:ReactNode,picture:string}) {
     return <section style={{display:"flex", justifyContent:"center", margin: "auto"}}>
         <div className={styles.block}>
             <div className={styles.textportion}>
                 <h2>{title}</h2>
                 <p>{text}</p>
-                <p>Du finner mer informasjon om {reference} <Link href={link}>HER</Link></p>
+                <p>Du finner mer informasjon om {reference} <Link href={link}>HER</Link>.</p>
                 <p>{secondtext}</p>
             </div>
             <div className={styles.picture}><img src={picture} alt='bilde' /></div>
