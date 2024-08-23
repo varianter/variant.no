087f2c24f60642edec13ea0264610eeaadd54ae9
Mathias Oterhals Myklebust <mom@variant.no>
2024-08-23 14:00:51 +0200

    chore(salaryCalculator): prettify

--- src/salaryAndBenefits/utils/calculateSalary.ts
+++ src/salaryAndBenefits/utils/calculateSalary.ts
@@ -1,29 +1,28 @@
 import { payscale } from "./salaryData";
-  
+
 interface PayScale {
-    [year: number]: {
-      [examinationYear: number]: number;
-    };
-  }
-  
-  const salaryPayscale: PayScale = payscale;
-  
-  export function calculateSalary(
-    currentYear: number,
-    examinationYear: number,
-    degree: string
-  ): number {
+  [year: number]: {
+    [examinationYear: number]: number;
+  };
+}
 
-    const degreeValue = degree === "bachelor" ? 1 : 0;
-    const adjustedYear = (examinationYear + degreeValue);
-    return salaryPayscale[currentYear][adjustedYear];
-  }
+const salaryPayscale: PayScale = payscale;
 
-  export function calculatePension(salary: number): number {
-    return Math.round(salary * 0.07);
-  }
+export function calculateSalary(
+  currentYear: number,
+  examinationYear: number,
+  degree: string,
+): number {
+  const degreeValue = degree === "bachelor" ? 1 : 0;
+  const adjustedYear = examinationYear + degreeValue;
+  return salaryPayscale[currentYear][adjustedYear];
+}
 
-  export function maxExperience(thisYear: number): number {
-  const years = Object.keys(salaryPayscale[thisYear]).map(Number)
-    return Math.min(...years);
-  }
+export function calculatePension(salary: number): number {
+  return Math.round(salary * 0.07);
+}
+
+export function maxExperience(thisYear: number): number {
+  const years = Object.keys(salaryPayscale[thisYear]).map(Number);
+  return Math.min(...years);
+}
