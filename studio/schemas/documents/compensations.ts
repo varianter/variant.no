f0ce0369d62db13fa3ae855cc6249d92c64d93ab
Mathias Oterhals Myklebust <mom@variant.no>
2024-08-29 15:53:54 +0200

    feat(compensations): offices array preview

--- studio/schemas/documents/compensations.ts
+++ studio/schemas/documents/compensations.ts
@@ -5,6 +5,9 @@ import offices from "../objects/offices";
 import { title } from "../fields/text";
 import { benefitId } from "./benefit";
 
+// smallest number of offices where ellipsis (...) will be used in preview
+const OFFICES_PREVIEW_ELLIPSIS_LIMIT = 5;
+
 export const compensationsId = "compensations";
 
 const compensations = defineType({
@@ -33,8 +36,29 @@ const compensations = defineType({
   ],
   preview: {
     select: {
-      title: "office.basicTitle",
       subtitle: "basicTitle",
+      // https://www.sanity.io/docs/previews-list-views#62febb15a63a
+      ...[...Array(OFFICES_PREVIEW_ELLIPSIS_LIMIT).keys()].reduce(
+        (o, i) => ({ ...o, [`office${i}`]: `offices.${i}.basicTitle` }),
+        {},
+      ),
+    },
+    prepare({ subtitle, ...officesMap }) {
+      const offices = Object.values<string>(officesMap).filter(
+        (o) => o !== undefined,
+      );
+      let title;
+      if (offices.length >= OFFICES_PREVIEW_ELLIPSIS_LIMIT) {
+        title =
+          offices.slice(0, OFFICES_PREVIEW_ELLIPSIS_LIMIT - 1).join(", ") +
+          ", ...";
+      } else {
+        title = offices.join(", ");
+      }
+      return {
+        title,
+        subtitle,
+      };
     },
   },
 });
