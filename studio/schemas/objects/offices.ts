a1b16dd8c51a27a7df063e7e65161ca6b69074af
Mathias Oterhals Myklebust <mom@variant.no>
2024-08-30 13:17:41 +0200

    feat(office): default descriptions for reusable office and offices fields

--- studio/schemas/objects/offices.ts
+++ studio/schemas/objects/offices.ts
@@ -4,6 +4,7 @@ import office from "./office";
 const offices = defineField({
   name: "offices",
   title: "Offices",
+  description: "List of company offices",
   type: "array",
   of: [office],
   validation: (Rule) => Rule.required().min(1).unique(),
