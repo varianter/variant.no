b363ccfe0beddd91c88847b15e303276494c9b18
Mathias Oterhals Myklebust <mom@variant.no>
2024-09-04 13:37:23 +0200

    feat(robots): disallow crawling of studio and api paths

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -4,6 +4,7 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
+      disallow: ["/studio", "/shared", "/api"],
     },
     sitemap: new URL("sitemap.xml", process.env.NEXT_PUBLIC_URL).toString(),
   };
