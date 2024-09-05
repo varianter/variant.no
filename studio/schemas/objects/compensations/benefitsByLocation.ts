e9917c86f5684dbe4579c255a3c638c81a452833
Ane <ane-ness@hotmail.no>
2024-09-05 11:39:07 +0200

    Add benefitgroup for a spesific location

---
+++ studio/schemas/objects/compensations/benefitsByLocation.ts
@@ -0,0 +1,55 @@
+import { defineField } from "sanity";
+import { location, locationID } from "../locations";
+import { companyLocationNameID } from "studio/schemas/documents/companyLocation";
+import { benefitId } from "studio/schemas/documents/benefit";
+import { checkForDuplicateLocations, DocumentWithLocation } from "studio/utils/checkForDuplicateLocations";
+
+export const benefitsByLocation = defineField({
+  name: "benefitsByLocation",
+  title: "Benefits by Location",
+  description:
+  "Enter the benefits offered at each office location. You can only have one location entry, but you can list multiple benefits for each office location.", 
+  type: "array",
+  of: [
+    {    
+      type: "object",
+      fields: [
+        {
+          ...location,
+          description:
+            "Select the office location for which you are entering benefits information. Each location must be unique.",
+          validation: (Rule) => Rule.required(),
+        },
+        defineField({
+          name: "benefitsGroup",
+          title: "Benefits Group",
+          description: "Enter the benefits offered at the location selected above.",
+          type: "array",
+          of: [{ type: benefitId }],
+        }),
+      ],
+    preview: {
+    select: {
+      location: `${locationID}.${companyLocationNameID}`,
+      benefitsGroup: "benefitsGroup",
+    },
+    prepare({ location, benefitsGroup }) {
+        const benefitsCount = benefitsGroup ? benefitsGroup.length : 0;
+      return {
+        title: location ? `Benefits group for ${location}` : "No location selected",
+        subtitle: `Number of benefits: ${benefitsCount}`,
+      };
+    },
+  },
+},
+],
+validation: (Rule) =>
+Rule.custom((benefitsByLocation) => {
+  return (
+    checkForDuplicateLocations(
+      benefitsByLocation as DocumentWithLocation[] | undefined,
+    ) ||
+    "Each location should be listed only once in the benefits list."
+  );
+}),
+});
