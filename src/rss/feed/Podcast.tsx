89e9b441d2ebc79c36ee088189c4eabd21e82794
Kristoffer Nordström <totte.nordstrom@gmail.com>
2024-09-07 09:57:33 +0200

    fix(podcast): use html text & only render image if there is one

--- src/rss/feed/Podcast.tsx
+++ src/rss/feed/Podcast.tsx
@@ -1,5 +1,6 @@
 import { BaseBlob } from '@variant/components/lib/blob';
 import dynamic from 'next/dynamic';
+import DOMPurify from 'isomorphic-dompurify';
 import { and } from 'src/utils/css';
 import { PodcastItem } from '../index';
 import style from './feed.module.css';
@@ -7,10 +8,12 @@ import style from './feed.module.css';
 const Player = dynamic(() => import('./Player'), { ssr: false });
 
 export default function Podcast({ item }: { item: PodcastItem }) {
+  const cleanDescription = DOMPurify.sanitize(item.description);
+
   return (
     <div className={and(style.card, style['card--podcast'])}>
       <div className={style.media}>
-        <BaseBlob
+        {item.imageCoverUrl && <BaseBlob
           height={200}
           width={200}
           randomness={2}
@@ -21,7 +24,7 @@ export default function Podcast({ item }: { item: PodcastItem }) {
             loading: 'lazy',
             decoding: 'async',
           }}
-        />
+        />}
       </div>
       <div className={style.text}>
         <div className={style.podcast__content}>
@@ -40,7 +43,7 @@ export default function Podcast({ item }: { item: PodcastItem }) {
             </div>
           </Player>
           <div className={style.published}>Publisert {item.publishDate}</div>
-          <p className={style.summary}>{item.description}</p>
+          <div dangerouslySetInnerHTML={{ __html: cleanDescription }} />
         </div>
       </div>
     </div>
