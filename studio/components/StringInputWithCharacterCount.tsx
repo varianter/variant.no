996773ea0bb903003058fe6ae0ab7b0359af4df2
Mathias Oterhals Myklebust <mom@variant.no>
2024-09-11 08:28:19 +0200

    feat(StringInputWithCharacterCount): handle plural form

--- studio/components/StringInputWithCharacterCount.tsx
+++ studio/components/StringInputWithCharacterCount.tsx
@@ -10,13 +10,16 @@ export const StringInputWithCharacterCount = ({
   ...defaultProps
 }: StringInputWithCharacterCountProps) => {
   const characterCount = defaultProps.value?.length ?? 0;
+  const isPlural =
+    (maxCount !== undefined && maxCount !== 1) ||
+    (maxCount === undefined && characterCount !== 1);
 
   return (
     <Stack space={3}>
       <Box>{defaultProps.renderDefault(defaultProps)}</Box>
       <Text size={1} muted>
         {characterCount}
-        {maxCount && `/${maxCount}`} characters
+        {maxCount && `/${maxCount}`} character{isPlural ? "s" : ""}
       </Text>
     </Stack>
   );
