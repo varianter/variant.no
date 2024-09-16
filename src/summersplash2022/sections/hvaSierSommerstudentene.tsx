a6b2f569c0b9291a6036370e16d0a8f6bb47d7f3
Marius Krakeli <marius.krakeli@gmail.com>
2024-09-16 11:20:02 +0200

    add: quotes from summer students

--- src/summersplash2022/sections/hvaSierSommerstudentene.tsx
+++ src/summersplash2022/sections/hvaSierSommerstudentene.tsx
@@ -4,13 +4,9 @@ import GreenBlob from '../img/greenBlob';
 
 const summerstudentsQuotes = [
   {
-    name: 'Emma',
-    quote: `“Jeg er utrolig glad for å få være en del av variantgjengen i
-      sommer! Det er gøy å se hvordan kjerneverdier som åpenhet og
-      læreglede virkelig praktiseres i arbeidshverdagen, ikke bare
-      på papiret. Det gjør at jeg føler meg veldig inkludert og
-      lærer masse, samtidig som vi har det mye moro!\”`,
-    picture: require('../img/emma.svg'),
+    name: 'Solveig',
+    quote: `“Sommeren hos Variant var som å bli tatt inn i varmen av en stor familie, som umiddelbart fikk meg til å føle meg hjemme. De faste ansatte delte raust av sin kunnskap, tid og støtte, noe som gjorde det lett å finne seg til rette. Sommerteamet vårt, bestående av to designere og to utviklere, jobbet i seks uker på et spennende prosjekt for en start-up. Det var kult å kunne bygge noe verdifullt på kort tid – og ikke minst at det gikk rett i produksjon etter sommeren! Og ja, tre uker med velfortjent fri midt i alt dette smakte også godt.”`,
+    picture: require('../img/solveig-blob.png'),
   },
   {
     name: 'Mathias',
@@ -24,14 +20,9 @@ const summerstudentsQuotes = [
     picture: require('../img/mathias.svg'),
   },
   {
-    name: 'Swarny',
-    quote: `“Sommeren hos Variant har vært helt fantastisk. Vi fikk jobbe
-      med et megakult og aktuelt prosjekt med god oppfølging, både
-      fra Variant og kunden. Selv om Variant er et konsulentselskap,
-      har de et godt sosialt miljø hvor alle er inkluderende, ivrige
-      og åpne. Jeg er superfornøyd med at
-      jeg søkte sommerjobb hos Variant!!”`,
-    picture: require('../img/swarny.svg'),
+    name: 'Max',
+    quote: `“Denne sommeren ble faktist en av de beste somrene på flere år, mye takk vare Variant! Det var så fint å komme til jobb og bli mottatt med varme og arbeide sammen med et fint team på et ordentlig prosjekt. Variant var skikkelig god på å tilby oss sosiale aktiviteter utover arbeidet, uten press om at man må bli med på alt. Opplevelsen fikk meg til å føle at det kanskje ikke er så verst å bli ferdig som student tross alt”`,
+    picture: require('../img/max-blob.png'),
   },
 ];
 
