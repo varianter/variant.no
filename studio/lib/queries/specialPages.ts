66b209e339d35c624c347b93887c670fad1055ec
Mathias Oterhals Myklebust <mom@variant.no>
2024-09-20 10:25:25 +0200

    hotfix(customerCases): customerCases → customerCasesPage

--- studio/lib/queries/specialPages.ts
+++ studio/lib/queries/specialPages.ts
@@ -1,7 +1,7 @@
 import { groq } from "next-sanity";
 
 export const CUSTOMER_CASES_PAGE_QUERY = groq`
-  *[_type == "customerCases" && slug.current == $slug][0]`;
+  *[_type == "customerCasesPage" && slug.current == $slug][0]`;
 
 export const COMPENSATIONS_PAGE_QUERY = groq`
   *[_type == "compensations" && slug.current == $slug][0]
