4b63acdf4c7285ca5fc6885898dabec2a73288e6
Mathias Oterhals Myklebust <mom@variant.no>
2024-09-25 14:55:41 +0200

    fix(languageSchemaField): title typo

--- internationalization/languageSchemaField.ts
+++ internationalization/languageSchemaField.ts
@@ -4,7 +4,7 @@ export const languageID = "language";
 
 const languageSchemaField = defineField({
   name: languageID,
-  title: "Langauge",
+  title: "Language",
   description:
     "Select the language for this document from the translation menu. This field reflects the chosen language and is set automatically based on your selection.",
   type: "string",
