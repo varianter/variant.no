c0d417b8af05e8f2228c2f6be82a4aa2cacf5096
anemne <63043552+anemne@users.noreply.github.com>
2024-09-26 10:13:30 +0200

    Add a language param to customer case query (#715)
    * add a lang param to customer case query
    
    * replace lang with english

--- src/customerCases/CustomerCases.tsx
+++ src/customerCases/CustomerCases.tsx
@@ -18,7 +18,11 @@ const CustomerCases = async ({ customerCasesPage }: CustomerCasesProps) => {
   const { perspective } = getDraftModeInfo();
 
   const [sharedCustomerCases] = await Promise.all([
-    loadSharedQuery<CustomerCase[]>(CUSTOMER_CASES_QUERY, {}, { perspective }),
+    loadSharedQuery<CustomerCase[]>(
+      CUSTOMER_CASES_QUERY,
+      { language: "en" },
+      { perspective },
+    ), //TODO: Replace hard coded language with selected language
   ]);
 
   return (
