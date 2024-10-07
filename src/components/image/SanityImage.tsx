c4f7d9883572c77adc1f385874ebe6daaf25003c
Mathias Oterhals Myklebust <mom@variant.no>
2024-10-07 10:35:19 +0200

    fix(SanityImage): full width for Storybook mock image

--- src/components/image/SanityImage.tsx
+++ src/components/image/SanityImage.tsx
@@ -68,7 +68,7 @@ export function SanityImage({ image }: { image: IImage }) {
       <Image
         alt={image?.alt || ""}
         src={image.src.src}
-        style={{ objectFit: "cover", height: "100%" }}
+        style={{ objectFit: "cover", height: "100%", width: "100%" }}
         width={300}
         height={300}
       />
