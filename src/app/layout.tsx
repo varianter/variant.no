edfd27c8c50dd8ad9f916f500238726880a04d5e
Mathias Oterhals Myklebust <24361490+mathiazom@users.noreply.github.com>
2024-10-14 11:48:09 +0000

    hotfix: missing language GROQ param (#775)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,44 +1,17 @@
-import { Metadata } from "next";
-import localFont from "next/font/local";
 import { draftMode } from "next/headers";
 
-import { generateMetadataFromSeo } from "src/utils/seo";
-import { LanguageObject } from "studio/lib/interfaces/supportedLanguages";
 import LiveVisualEditing from "studio/lib/loaders/AutomaticVisualEditing";
-import { DEFAULT_LANGUAGE_QUERY } from "studio/lib/queries/siteSettings";
-import { loadStudioQuery } from "studio/lib/store";
 
 import "src/styles/global.css";
-
-const fontBrittiSans = localFont({
-  src: "../../public/_assets/britti-sans-variable.woff2",
-  variable: "--font-britti-sans",
-});
-
-export async function generateMetadata(): Promise<Metadata> {
-  // fallback if no page-specific metadata is provided
-  return generateMetadataFromSeo(null);
-}
 
 export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  let siteLang;
-
-  try {
-    const { data } = await loadStudioQuery<LanguageObject>(
-      DEFAULT_LANGUAGE_QUERY,
-    );
-    siteLang = data.id;
-  } catch (error) {
-    console.error("Error loading site settings:", error);
-    siteLang = "en";
-  }
   return (
-    <html lang={siteLang}>
-      <body className={fontBrittiSans.variable}>
+    <html lang={"en"}>
+      <body>
         {children}
         {draftMode().isEnabled && <LiveVisualEditing />}
       </body>
