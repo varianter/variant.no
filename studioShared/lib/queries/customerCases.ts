fa5bf245f9a7af90dadc73d70b97191646170568
Mathias Oterhals Myklebust <mom@variant.no>
2024-10-16 08:37:46 +0200

    refactor(customerCases): format GROQ query and add language field fragment

--- studioShared/lib/queries/customerCases.ts
+++ studioShared/lib/queries/customerCases.ts
@@ -1,11 +1,14 @@
 import { groq } from "next-sanity";
 
+import { LANGUAGE_FIELD_FRAGMENT } from "studio/lib/queries/i18n";
 import { translatedFieldFragment } from "studio/lib/queries/utils/i18n";
 
-export const CUSTOMER_CASES_QUERY = groq`*[_type == "customerCase"]{
- "language": $language,
-  "basicTitle": ${translatedFieldFragment("basicTitle")},
-   "richText": ${translatedFieldFragment("richText")}}
+export const CUSTOMER_CASES_QUERY = groq`
+  *[_type == "customerCase"]{
+    ${LANGUAGE_FIELD_FRAGMENT},
+    "basicTitle": ${translatedFieldFragment("basicTitle")},
+    "richText": ${translatedFieldFragment("richText")}
+  }
 `;
 
 export const CUSTOMER_CASE_QUERY = groq`*[_type == "customerCase" && slug.current == $slug && language == $language][0]`;
