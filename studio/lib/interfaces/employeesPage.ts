e6eabee1278e8aa3bb6781e86b7fa5cba0720d65
Mathias Oterhals Myklebust <mom@variant.no>
2024-10-16 14:15:35 +0200

    feat: initial employees page

---
+++ studio/lib/interfaces/employeesPage.ts
@@ -0,0 +1,10 @@
+export interface EmployeesPage {
+  _createdAt: string;
+  _id: string;
+  _rev: string;
+  _type: string;
+  _updatedAt: string;
+  language: string;
+  basicTitle: string;
+  slug: string;
+}
