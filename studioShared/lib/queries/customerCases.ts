8a4a1a380f7456db4003951b6c244a0559ac4238
Mathias Oterhals Myklebust <mom@variant.no>
2024-10-17 11:00:25 +0200

    feat(customerCase): include block sections in customer case query

--- studioShared/lib/queries/customerCases.ts
+++ studioShared/lib/queries/customerCases.ts
@@ -28,6 +28,13 @@ export const CUSTOMER_CASE_QUERY = groq`
       "sector": ${translatedFieldFragment("sector")},
       "delivery": ${translatedFieldFragment("delivery")},
       consultants
+    },
+    "sections": sections[] {
+      _type,
+      _type == "richTextBlock" => {
+        ...,
+        "richText": ${translatedFieldFragment("richText")},
+      }
     }
   }
 `;
