e45cb07bb0706df05feafa237fffc4095914ce6a
Truls Henrik <39614013+trulshj@users.noreply.github.com>
2024-10-21 14:09:26 +0200

    chore: always show emails in employee page (#800)

--- src/employees/index.tsx
+++ src/employees/index.tsx
@@ -120,6 +120,7 @@ export default function Employees({
               <EmployeeTile
                 key={`${employee.name}-${index}`}
                 employee={employee}
+                alwaysEmail
               />
             );
           })}
