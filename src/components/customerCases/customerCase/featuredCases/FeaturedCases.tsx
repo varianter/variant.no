f12dce9ed39a2a49e0e03aa59e55e93fb1a05da4
Ane <ane-ness@hotmail.no>
2024-10-30 10:43:03 +0100

    fix render issue

--- src/components/customerCases/customerCase/featuredCases/FeaturedCases.tsx
+++ src/components/customerCases/customerCase/featuredCases/FeaturedCases.tsx
@@ -20,21 +20,24 @@ export default function FeaturedCases({
       <div className={styles.wrapper}>
         <Text type={"h3"}>Lignende prosjekter</Text>
         <div className={styles.content}>
-          {featuredCases.map((featuredCase) => (
-            <div key={featuredCase._id} className={styles.caseWrapper}>
-              <div className={styles.caseImageWrapper}>
-                <SanitySharedImage image={featuredCase.image} />
-              </div>
-              <div>
-                <Link
-                  href={`/${[...customerCasesPath, featuredCase.slug].join("/")}`}
-                >
-                  <Text type={"bodyBig"}>{featuredCase.basicTitle}</Text>
-                </Link>
-                <Text type={"bodySmall"}>{featuredCase.description}</Text>
-              </div>
-            </div>
-          ))}
+          {featuredCases.map(
+            (featuredCase) =>
+              featuredCase && (
+                <div key={featuredCase._id} className={styles.caseWrapper}>
+                  <div className={styles.caseImageWrapper}>
+                    <SanitySharedImage image={featuredCase.image} />
+                  </div>
+                  <div>
+                    <Link
+                      href={`/${[...customerCasesPath, featuredCase.slug].join("/")}`}
+                    >
+                      <Text type={"bodyBig"}>{featuredCase.basicTitle}</Text>
+                    </Link>
+                    <Text type={"bodySmall"}>{featuredCase.description}</Text>
+                  </div>
+                </div>
+              ),
+          )}
         </div>
       </div>
     )
