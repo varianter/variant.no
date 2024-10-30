0e1a46ac23055ca060d508864b1baee79aa8c790
Ane <ane-ness@hotmail.no>
2024-10-30 13:29:05 +0100

    fixes from review

--- src/components/navigation/header/Header.tsx
+++ src/components/navigation/header/Header.tsx
@@ -27,6 +27,11 @@ export interface IHeader {
   pathTranslations: InternationalizedString;
 }
 
+export interface BreadCrumbProps {
+  currentLanguage: string;
+  pathname: string;
+}
+
 const filterLinks = (data: ILink[], type: string) =>
   data?.filter((link) => link._type === type);
 
@@ -124,47 +129,56 @@ export const Header = ({
           </nav>
         </header>
       </FocusOn>
-      {pathname !== "/" &&
-        pathname !== "/" + currentLanguage &&
-        breadCrumb(currentLanguage, pathname)}
+      {pathname !== "/" && pathname !== "/" + currentLanguage && (
+        <BreadCrumb currentLanguage={currentLanguage} pathname={pathname} />
+      )}
     </>
   );
 };
 
-export const breadCrumb = (currentLanguage: string, pathname: string) => (
-  <ul className={styles.breadCrumbMenu}>
-    {/* TODO: Home should be updated with translation */}
-    {["Home", ...pathname.split("/").slice(2)].map((e, index, path) => {
-      const href =
-        "/" + currentLanguage + "/" + path.slice(1, index + 1).join("/");
-      const isLast = index === path.length - 1;
+export const BreadCrumb = ({ currentLanguage, pathname }: BreadCrumbProps) => {
+  return (
+    <ul className={styles.breadCrumbMenu}>
+      {/* TODO: "Hjem" should be updated with translation */}
+      {[
+        "Hjem",
+        ...(pathname.includes("/" + currentLanguage + "/")
+          ? pathname.split("/").slice(2)
+          : pathname.split("/").slice(1)),
+      ].map((e, index, path) => {
+        const href =
+          "/" + currentLanguage + "/" + path.slice(1, index + 1).join("/");
+        const isLast = index === path.length - 1;
 
-      return (
-        <li key={index} className={styles.breadCrumb}>
-          <Link className={styles.breadCrumb} href={href}>
-            <Text
-              className={isLast ? styles.breadCrumbText : styles.breadCrumbLink}
-              type={isLast ? "labelSemibold" : "desktopLink"}
-            >
-              {e.charAt(0).toUpperCase() + e.slice(1).toLowerCase()}
-            </Text>
-          </Link>
-          {!isLast && (
-            <span
-              className={
-                index < path.length - 2
-                  ? styles.dotSeparator
-                  : styles.lastDotSeparator
-              }
-            >
-              •
-            </span>
-          )}
-        </li>
-      );
-    })}
-  </ul>
-);
+        return (
+          <li key={index} className={styles.breadCrumb}>
+            <Link className={styles.breadCrumb} href={href}>
+              <Text
+                className={
+                  isLast ? styles.breadCrumbText : styles.breadCrumbLink
+                }
+                type={isLast ? "labelSemibold" : "desktopLink"}
+              >
+                {e.charAt(0).toUpperCase() + e.slice(1).toLowerCase()}
+              </Text>
+            </Link>
+            {!isLast && (
+              <span
+                className={
+                  index < path.length - 2
+                    ? styles.dotSeparator
+                    : styles.lastDotSeparator
+                }
+              >
+                •
+              </span>
+            )}
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
 
 export const renderPageLinks = (
   links: ILink[],
