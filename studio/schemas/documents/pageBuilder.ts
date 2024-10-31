045c2c104df7f35497b26849c26b6f70e630e487
Mathias Oterhals Myklebust <24361490+mathiazom@users.noreply.github.com>
2024-10-31 14:06:08 +0000

    feat(dynamicPage): internationalized page title (#845)
    to prepare for proper breadcrumb titles

--- studio/schemas/documents/pageBuilder.ts
+++ studio/schemas/documents/pageBuilder.ts
@@ -1,7 +1,7 @@
 import { defineField, defineType } from "sanity";
 
-import { StringInputWithCharacterCount } from "studio/components/stringInputWithCharacterCount/StringInputWithCharacterCount";
 import { isInternationalizedString } from "studio/lib/interfaces/global";
+import { titleID } from "studio/schemas/fields/text";
 import article from "studio/schemas/objects/sections/article";
 import callout from "studio/schemas/objects/sections/callout";
 import callToAction from "studio/schemas/objects/sections/callToAction";
@@ -12,7 +12,7 @@ import imageSection from "studio/schemas/objects/sections/image";
 import logoSalad from "studio/schemas/objects/sections/logoSalad";
 import testimonals from "studio/schemas/objects/sections/testimonials";
 import seo from "studio/schemas/objects/seo";
-import { pageSlug } from "studio/schemas/schemaTypes/slug";
+import { titleSlug } from "studio/schemas/schemaTypes/slug";
 import { firstTranslation } from "studio/utils/i18n";
 
 export const pageBuilderID = "pageBuilder";
@@ -22,20 +22,15 @@ const pageBuilder = defineType({
   type: "document",
   title: "Dynamic pages",
   fields: [
-    defineField({
-      name: "page",
-      title: "Page name",
-      description:
-        "Enter a distinctive name for the dynamic page to help content editors easily identify and manage it. This name is used internally and is not visible on your website.",
-      type: "string",
-      validation: (rule) => rule.required().max(30),
-      components: {
-        input: (props) =>
-          StringInputWithCharacterCount({ ...props, maxCount: 30 }),
-      },
-    }),
     {
-      ...pageSlug,
+      name: titleID.basic,
+      type: "internationalizedArrayString",
+      title: "Page Title",
+      description:
+        "Enter the primary title that will be displayed in Sanity and the breadcrumb trail at the top of the page.",
+    },
+    {
+      ...titleSlug,
       type: "internationalizedArrayString",
     },
     seo,
@@ -59,17 +54,22 @@ const pageBuilder = defineType({
   ],
   preview: {
     select: {
-      title: "page",
-      slug: pageSlug.name,
+      title: titleID.basic,
+      slug: titleSlug.name,
     },
     prepare({ title, slug }) {
+      if (!isInternationalizedString(title)) {
+        throw new TypeError(
+          `Expected 'title' to be InternationalizedString, was ${typeof title}`,
+        );
+      }
       if (!isInternationalizedString(slug)) {
         throw new TypeError(
           `Expected 'slug' to be InternationalizedString, was ${typeof slug}`,
         );
       }
       return {
-        title: title ?? undefined,
+        title: firstTranslation(title) ?? undefined,
         subtitle: firstTranslation(slug) ?? undefined,
       };
     },
