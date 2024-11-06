35d757371f57e7bbdfc3023035a1a9e727c4cfef
Truls Henrik <39614013+trulshj@users.noreply.github.com>
2024-11-06 12:23:10 +0100

    Update src/i18n/request.ts
    Co-authored-by: anemne <63043552+anemne@users.noreply.github.com>

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -5,7 +5,7 @@ import { routing } from "./routing";
 export default getRequestConfig(async ({ requestLocale }) => {
   let locale = await requestLocale;
 
-  if (!locale || !routing.locales.includes(locale as "en" | "no")) {
+  if (!locale || !routing.locales.includes(locale as "en" | "no" | "se" )) {
     locale = routing.defaultLocale;
   }
 
