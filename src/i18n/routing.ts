63b6286c831285985ebec8a806b0cb1f1d37a92d
Truls Henrik <39614013+trulshj@users.noreply.github.com>
2024-11-06 12:22:48 +0100

    Update src/i18n/routing.ts
    Co-authored-by: anemne <63043552+anemne@users.noreply.github.com>

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -2,7 +2,7 @@ import { createNavigation } from "next-intl/navigation";
 import { defineRouting } from "next-intl/routing";
 
 export type Locale = (typeof locales)[number];
-const locales = ["en", "no"] as const;
+const locales = ["en", "no", "se"] as const;
 
 export const routing = defineRouting({
   locales,
