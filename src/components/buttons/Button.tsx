09e420cd6c0b5a67781d0638746e7c56608c220c
Ane <ane-ness@hotmail.no>
2024-11-13 11:16:28 +0100

    update colorpalette and globals with new variables

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -19,61 +19,62 @@ interface IButton {
   ariaDisabled?: boolean;
 }
 
-const sizeClassMap: { [key in ButtonSize]: string } = {
-  large: styles.large,
-  medium: styles.medium,
-  small: styles.small,
-};
+// const sizeClassMap: { [key in ButtonSize]: string } = {
+//   large: styles.large,
+//   medium: styles.medium,
+//   small: styles.small,
+// };
 
-const typeClassMap: { [key in ButtonType]: string } = {
-  primary: styles.primary,
-  secondary: styles.secondary,
-};
+// const typeClassMap: { [key in ButtonType]: string } = {
+//   primary: styles.primary,
+//   secondary: styles.secondary,
+// };
 
-const backgroundClassMap: { [key in ButtonBackground]: string } = {
-  dark: styles.dark,
-  light: styles.light,
-};
+// const backgroundClassMap: { [key in ButtonBackground]: string } = {
+//   dark: styles.dark,
+//   light: styles.light,
+// };
 
-const iconColorClassMap: { [key in ButtonBackground]: string } = {
-  dark: styles.iconColorDark,
-  light: styles.iconColorLight,
-};
+// const iconColorClassMap: { [key in ButtonBackground]: string } = {
+//   dark: styles.iconColorDark,
+//   light: styles.iconColorLight,
+// };
 
-const iconSizeClassMap: { [key in ButtonSize]: string } = {
-  large: styles.iconLarge,
-  medium: styles.iconMedium,
-  small: styles.iconSmall,
-};
+// const iconSizeClassMap: { [key in ButtonSize]: string } = {
+//   large: styles.iconLarge,
+//   medium: styles.iconMedium,
+//   small: styles.iconSmall,
+// };
 
 const Button = ({
-  size = "large",
-  type = "primary",
+  // size = "large",
+  // type = "primary",
   onClick,
   children,
   disabled,
-  loading,
+  // loading,
   ariaDisabled,
-  icon,
-  background = "dark",
+  // icon,
+  // background = "dark",
 }: IButton) => {
-  const className = `${styles.button} ${typeClassMap[type]} ${sizeClassMap[size]} ${backgroundClassMap[background]} ${loading ? styles.loading : ""}`;
-  const iconColorClass = `${iconColorClassMap[background]} ${iconSizeClassMap[size]}`;
+  // const className = `${styles.button} ${typeClassMap[type]} ${sizeClassMap[size]} ${backgroundClassMap[background]} ${loading ? styles.loading : ""}`;
+  // const iconColorClass = `${iconColorClassMap[background]} ${iconSizeClassMap[size]}`;
 
   return (
     <button
-      className={className}
+      className={styles.button}
       onClick={onClick}
       disabled={disabled}
       aria-disabled={ariaDisabled}
     >
-      {icon === "iconLeft" && (
+      {children}
+      {/* {icon === "iconLeft" && (
         <span className={`${styles.iconLeft} ${iconColorClass}`}></span>
       )}
-      {children}
+     
       {icon === "iconRight" && (
         <span className={`${styles.iconRight} ${iconColorClass}`}></span>
-      )}
+      )} */}
     </button>
   );
 };
