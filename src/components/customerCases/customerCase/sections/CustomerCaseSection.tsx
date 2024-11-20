b13d6dbf08c49736697cab78fe7c0a8538c77b29
Ida Marie Andreassen <ima@variant.no>
2024-11-20 09:19:47 +0100

    Add stacked highlights to result and quote component

--- src/components/customerCases/customerCase/sections/CustomerCaseSection.tsx
+++ src/components/customerCases/customerCase/sections/CustomerCaseSection.tsx
@@ -2,7 +2,6 @@ import { CustomerCaseSection as CustomerCaseSectionObject } from "studioShared/l
 
 import ImageSection from "./image/ImageSection";
 import ListBlock from "./list/ListBlock";
-import QuoteBlock from "./quote/QuoteBlock";
 import ResultsBlock from "./results/ResultsBlock";
 import SplitSection from "./splitSection/SplitSection";
 import TextSection from "./text/TextSection";
@@ -17,8 +16,6 @@ export function CustomerCaseSection({
       return <SplitSection section={section} />;
     case "textBlock":
       return <TextSection section={section} />;
-    case "quoteBlock":
-      return <QuoteBlock section={section} />;
     case "imageBlock":
       return <ImageSection section={section} />;
     case "resultsBlock":
--- src/components/customerCases/customerCase/sections/results/ResultsBlock.tsx
+++ src/components/customerCases/customerCase/sections/results/ResultsBlock.tsx
@@ -1,4 +1,3 @@
-import StackedHighlights from "src/components/stackedHighlights/StackedHighlights";
 import Text from "src/components/text/Text";
 import { ResultsBlock as ResultsBlockObject } from "studioShared/lib/interfaces/resultsBlock";
 
@@ -12,25 +11,42 @@ export default function ResultsBlock({ section }: ResultsBlockProps) {
   return (
     section.resultsBlockTitle && (
       <div className={styles.wrapper}>
-        <div className={styles.resultblock}>
-          <Text type="h4" className={styles.blocktitle}>
-            {section.resultsBlockTitle}
-          </Text>
-          <div className={styles.resultrow}>
-            {section.resultsList?.map((result) => (
-              <div className={styles.results} key={result._key}>
-                <Text type="h2" className={styles.mainresult}>
-                  {result.result}
-                </Text>
-                <Text type="labelRegular">{result.description}</Text>
-              </div>
-            ))}
-          </div>
-        </div>
         <div>
-          <StackedHighlights></StackedHighlights>
+          <StackedHighlights section={section}></StackedHighlights>
         </div>
       </div>
     )
+  );
+}
+
+function StackedHighlights({ section }: ResultsBlockProps) {
+  return (
+    <div className={styles.highlightWrapper}>
+      <div className={styles.highlightBlock}>
+        <Text type="labelRegular" className={styles.highlightOutside}>
+          {section.resultsBlockTitle}
+        </Text>
+        {section.quote?.map((quote) => (
+          <div className={styles.highlightCard} key={quote._key}>
+            <div className={styles.innerContent}>
+              <Text type="h2">{quote.quoteText}</Text>
+              <p className={styles.subtitle}>{quote.quoteAuthor}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className={styles.highlightRow}>
+        {section.resultsList?.map((result) => (
+          <div className={styles.highlightCard} key={result._key}>
+            <div className={styles.innerContent}>
+              <Text type="h2" className={styles.result}>
+                {result.result}
+              </Text>
+              <p className={styles.subtitle}>{result.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
