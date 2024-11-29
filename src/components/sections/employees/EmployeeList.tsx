2424a24a63ac05290df042e5e1243bcbefd0ba34
Mikael Brevik <mikaelbre@gmail.com>
2024-11-29 20:32:15 +0100

    derp

--- src/components/sections/employees/EmployeeList.tsx
+++ src/components/sections/employees/EmployeeList.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import Link from "next/link";
 import Image from "next/image";
+import Link from "next/link";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { useTranslations } from "next-intl";
 import { use, useState, useTransition } from "react";
