5071030125333db9d1aa1ec0da748dd94a8cc9da
Mikael Brevik <mikaelbre@gmail.com>
2024-12-08 19:50:38 +0100

    fix: responsiveness of costumer case

--- src/components/sections/customerCasesEntry/CustomerCasesList.tsx
+++ src/components/sections/customerCasesEntry/CustomerCasesList.tsx
@@ -5,11 +5,12 @@ import { useTranslations } from "next-intl";
 import { useState } from "react";
 
 import { SanitySharedImage } from "src/components/image/SanityImage";
-import styles from "src/components/sections/customerCasesEntry/customerCasesEntry.module.css";
 import { Tag } from "src/components/tag";
 import Text from "src/components/text/Text";
 import { capitalizeFirstLetter } from "src/components/utils/formatCapitalizedFirstLetter";
 import { CustomerCaseEntry } from "studioShared/lib/interfaces/customerCases";
+
+import styles from "./customerCasesEntry.module.css";
 
 interface CustomerCasesProps {
   customerCases: CustomerCaseEntry[];
@@ -22,7 +23,6 @@ const CustomerCaseList = ({
   language,
   customerCasePageSlug,
 }: CustomerCasesProps) => {
-  const t = useTranslations("customer_case");
   const [selectedCustomerCase, setSelectedCustomerCase] =
     useState<CustomerCaseEntry>(customerCases[0] || null);
 
@@ -37,64 +37,96 @@ const CustomerCaseList = ({
     <div className={styles.container}>
       <div className={styles.wrapper}>
         <div className={styles.info}>
-          <div className={styles.TagRow}>
-            <Text className={styles.font} type="labelRegular">
-              {t("customer_case_entry.case")}
-            </Text>
-            {customerCases.map(
-              (customerCase: CustomerCaseEntry) =>
-                customerCase && (
-                  <div key={customerCase._id}>
-                    <Tag
-                      active={customerCase._id === selectedCustomerCase._id}
-                      type="button"
-                      background="dark"
-                      onClick={() => setSelectedCustomerCase(customerCase)}
-                      text={capitalizeFirstLetter(
-                        customerCase.projectInfo.customer,
-                      )}
-                    />
-                  </div>
-                ),
-            )}
+          <div className={styles.infoInnerMaxWidth}>
+            <TagRow
+              customerCases={customerCases}
+              selectedCustomerCase={selectedCustomerCase}
+              setSelectedCustomerCase={setSelectedCustomerCase}
+            />
+            <Link
+              className={styles.link}
+              href={`/${language}/${customerCasePageSlug}/${selectedCustomerCase.slug}`}
+            >
+              <CardInfo
+                selectedCustomerCase={selectedCustomerCase}
+                deliveryNames={deliveryNames}
+              />
+            </Link>
           </div>
-          <Link
-            className={styles.link}
-            href={`/${language}/${customerCasePageSlug}/${selectedCustomerCase.slug}`}
-          >
-            <div className={styles.cardInfo}>
-              <Text type="h2" className={styles.heading}>
-                {" "}
-                {selectedCustomerCase.basicTitle}
-              </Text>
-              <div className={styles.deliveries}>
-                <Text type="labelRegular">
-                  {t("customer_case_entry.field")}
-                </Text>
-                <div className={styles.deliveriesList}>
-                  {deliveryNames.map((deliveryName, index) => (
-                    <Text key={index} type="h5" className={styles.dotSeparator}>
-                      {deliveryName}
-                    </Text>
-                  ))}
-                </div>
-              </div>
-            </div>
-          </Link>
         </div>
         <Link
           tabIndex={-1}
           className={styles.imageWrapper}
           href={`/${language}/${customerCasePageSlug}/${selectedCustomerCase.slug}`}
         >
-          {/* <div className={styles.imageWrapper}> */}
           {selectedCustomerCase.image && (
             <SanitySharedImage image={selectedCustomerCase.image} />
           )}
-          {/* </div> */}
         </Link>
       </div>
     </div>
   );
 };
 export default CustomerCaseList;
+
+function CardInfo({
+  selectedCustomerCase,
+  deliveryNames,
+}: {
+  selectedCustomerCase: CustomerCaseEntry;
+  deliveryNames: string[];
+}) {
+  const t = useTranslations("customer_case");
+
+  return (
+    <div className={styles.cardInfo}>
+      <Text type="h2" className={styles.heading}>
+        {selectedCustomerCase.basicTitle}
+      </Text>
+      <div className={styles.deliveries}>
+        <Text type="labelRegular">{t("customer_case_entry.field")}</Text>
+        <div className={styles.deliveriesList}>
+          {deliveryNames.map((deliveryName, index) => (
+            <Text key={index} type="h5" className={styles.dotSeparator}>
+              {deliveryName}
+            </Text>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function TagRow({
+  customerCases,
+  selectedCustomerCase,
+  setSelectedCustomerCase,
+}: {
+  customerCases: CustomerCaseEntry[];
+  selectedCustomerCase: CustomerCaseEntry;
+  setSelectedCustomerCase: (customerCase: CustomerCaseEntry) => void;
+}) {
+  const t = useTranslations("customer_case");
+
+  return (
+    <div className={styles.TagRow}>
+      <Text className={styles.font} type="labelRegular">
+        {t("customer_case_entry.case")}
+      </Text>
+      {customerCases.map(
+        (customerCase: CustomerCaseEntry) =>
+          customerCase && (
+            <div key={customerCase._id}>
+              <Tag
+                active={customerCase._id === selectedCustomerCase._id}
+                type="button"
+                background="dark"
+                onClick={() => setSelectedCustomerCase(customerCase)}
+                text={capitalizeFirstLetter(customerCase.projectInfo.customer)}
+              />
+            </div>
+          ),
+      )}
+    </div>
+  );
+}
