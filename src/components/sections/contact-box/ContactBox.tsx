b4fcb2f5a26e19199e4a9603c7f252a8913e8802
Mikael Brevik <mikaelbre@gmail.com>
2024-12-09 20:29:37 +0100

    refactor: clean up api fetching in contact box

--- src/components/sections/contact-box/ContactBox.tsx
+++ src/components/sections/contact-box/ContactBox.tsx
@@ -2,14 +2,13 @@ import { Suspense } from "react";
 
 import { EmployeeCardSkeleton } from "src/components/employeeCard/EmployeeCard";
 import Text from "src/components/text/Text";
-import { ChewbaccaEmployee } from "src/types/employees";
-import { fetchEmployeesByEmails } from "src/utils/employees";
 import { ContactBoxSection } from "studio/lib/interfaces/pages";
 import { EMPLOYEE_PAGE_SLUG_QUERY } from "studio/lib/queries/siteSettings";
 import { loadStudioQuery } from "studio/lib/store";
 
+import { fetchEmployeesWithTags, getEmployeesPageSlug } from "./api";
 import styles from "./contact-box.module.css";
-import ContactSelector, { EmployeeAndTag } from "./ContactSelector";
+import ContactSelector from "./ContactSelector";
 
 export interface ContactBoxProps {
   section: ContactBoxSection;
@@ -20,21 +19,13 @@ export default async function ContactBox({
   section,
   language,
 }: ContactBoxProps) {
-  const employeesPageRes = await loadStudioQuery<{ slug: string }>(
-    EMPLOYEE_PAGE_SLUG_QUERY,
-    {
-      language,
-    },
-  );
-  const employeesPageSlug = employeesPageRes.data.slug;
+  const employeesPageSlug = await getEmployeesPageSlug(language);
 
-  const contactPoints = fetchEmployeesByEmails(
-    section.contactPoints.map((contactPoint) => contactPoint.email),
-  ).then((result) =>
-    result.ok
-      ? result.value.map((e) => employeeAndTag(e, section.contactPoints))
-      : [],
-  );
+  if (!employeesPageSlug) {
+    return null;
+  }
+
+  const contactPoints = fetchEmployeesWithTags(section.contactPoints);
 
   const backgroundClass =
     section.background === "light" ? styles["contactBox__inner--light"] : "";
@@ -67,21 +58,4 @@ export default async function ContactBox({
       </div>
     </section>
   );
-}
-
-function employeeAndTag(
-  employee: ChewbaccaEmployee,
-  contactPoints: ContactBoxSection["contactPoints"],
-): EmployeeAndTag {
-  const tag =
-    contactPoints.find((contactPoint) => contactPoint.email === employee.email)
-      ?.tag ?? "";
-  return {
-    employee,
-    tag,
-    tagSlug: slugify(tag),
-  };
-}
-function slugify(tag: string) {
-  return tag.toLowerCase().replace(/ /g, "-");
 }
