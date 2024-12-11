56c8b8a2289a2c6f32f8ea293715f27871a4e907
Mikael Brevik <mikaelbre@gmail.com>
2024-12-11 13:25:27 +0100

    fix: language setting issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,27 @@
+import localFont from "next/font/local";
 import { draftMode } from "next/headers";
 
 import LiveVisualEditing from "studio/lib/loaders/AutomaticVisualEditing";
 
 import "src/styles/global.css";
 
+const fontBrittiSans = localFont({
+  src: "../../public/_assets/britti-sans-variable.woff2",
+  variable: "--font-britti-sans",
+});
+
 export default async function RootLayout({
   children,
+  params,
 }: Readonly<{
   children: React.ReactNode;
+  params: {
+    locale: string;
+  };
 }>) {
   return (
-    <html lang={"en"}>
-      <body>
+    <html lang={params.locale}>
+      <body className={fontBrittiSans.variable}>
         {children}
         {draftMode().isEnabled && <LiveVisualEditing />}
       </body>
