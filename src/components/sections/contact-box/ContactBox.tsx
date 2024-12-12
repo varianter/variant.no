0b2cad18757ccc2c9af6d4a8464cc114928168e2
Mikael Brevik <mikaelbre@gmail.com>
2024-12-12 15:32:30 +0100

    refactor: reuse contact box in case pages

--- src/components/sections/contact-box/ContactBox.tsx
+++ src/components/sections/contact-box/ContactBox.tsx
@@ -1,12 +1,7 @@
-import { Suspense } from "react";
-
-import { EmployeeCardSkeleton } from "src/components/employeeCard/EmployeeCard";
-import Text from "src/components/text/Text";
 import { ContactBoxSection } from "studio/lib/interfaces/pages";
 
 import { fetchEmployeesWithTags, getEmployeesPageSlug } from "./api";
-import styles from "./contact-box.module.css";
-import ContactSelector from "./ContactSelector";
+import ContactBoxContent from "./ContactBoxContent";
 
 export interface ContactBoxProps {
   section: ContactBoxSection;
@@ -25,38 +20,12 @@ export default async function ContactBox({
 
   const contactPoints = fetchEmployeesWithTags(section.contactPoints);
 
-  const backgroundClass = styles[`contactBox__inner--${section.background}`];
-  const employeeCardBackground =
-    section.background === "transparent" ? "light" : section.background;
-
   return (
-    <section className={styles.contactBox}>
-      <div className={`${styles.contactBox__inner} ${backgroundClass}`}>
-        <div className={styles.textContent}>
-          <Text type="h3" as="h2">
-            {section.basicTitle}
-          </Text>
-
-          {section.optionalSubtitle && (
-            <Text type="bodyBig">{section.optionalSubtitle}</Text>
-          )}
-        </div>
-
-        <div className={styles.contactSelectorWrapper}>
-          <Suspense
-            fallback={
-              <EmployeeCardSkeleton background={employeeCardBackground} />
-            }
-          >
-            <ContactSelector
-              employeesPageSlug={employeesPageSlug}
-              contactPoints={contactPoints}
-              language={language}
-              background={employeeCardBackground}
-            />
-          </Suspense>
-        </div>
-      </div>
-    </section>
+    <ContactBoxContent
+      data={section}
+      language={language}
+      employeesPageSlug={employeesPageSlug}
+      contactPoints={contactPoints}
+    />
   );
 }
