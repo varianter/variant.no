0b754678b402856d1b8ff3055b820924e6cc22cb
anemne <63043552+anemne@users.noreply.github.com>
2025-01-28 09:36:01 +0100

    Update src/components/sections/articleSection/ArticleSection.tsx

--- src/components/sections/articleSection/ArticleSection.tsx
+++ src/components/sections/articleSection/ArticleSection.tsx
@@ -44,7 +44,7 @@ const renderArticleParagraph = ({
 }: IParagraphArticleSection) => (
   <div className={styles.container}>
     <div className={styles.paragraphWrapper}>
-      <Text type="h4"> {title} </Text>
+      <Text type="h4" as="h2"> {title} </Text>
       <Text type="bodySmall">{textContent}</Text>
     </div>
   </div>
