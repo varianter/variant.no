1eaa2a1a5e6200ca6ccd3bd880c78f09b02465c4
Elise Kristiansen <elise.kri@hotmail.com>
2025-01-30 15:08:28 +0100

    Lint fixes

--- src/components/sections/customerCasesEntry/CustomerCasesList.tsx
+++ src/components/sections/customerCasesEntry/CustomerCasesList.tsx
@@ -5,12 +5,12 @@ import { useTranslations } from "next-intl";
 import { useState } from "react";
 
 import { SanityImage } from "src/components/image/SanityImage";
+import LinkButton from "src/components/linkButton/LinkButton";
 import { Tag } from "src/components/tag";
 import Text from "src/components/text/Text";
 import { CustomerCaseEntry } from "studioShared/lib/interfaces/customerCases";
 
 import styles from "./customerCasesEntry.module.css";
-import LinkButton from "src/components/linkButton/LinkButton";
 
 interface CustomerCasesProps {
   customerCases: CustomerCaseEntry[];
