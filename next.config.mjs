acd7d1d551f0f6b7e73421a4dff5fd771e9cec40
Elise Kristiansen <elise.kri@hotmail.com>
2025-02-05 08:31:30 +0100

    Added redirects for swedish domain

--- next.config.mjs
+++ next.config.mjs
@@ -24,6 +24,120 @@ const nextConfig = {
   experimental: {
     taint: true,
   },
+  async redirects() {
+    return [
+      {
+        source: "/tjanster",
+        destination: "/se/tjanster",
+        has: [
+          {
+            type: "host",
+            value: "variant.se",
+          },
+        ],
+        permanent: true,
+      },
+      {
+        source: "/jobs",
+        destination: "/se/jobs",
+        has: [
+          {
+            type: "host",
+            value: "variant.se",
+          },
+        ],
+        permanent: true,
+      },
+      {
+        source: "/om-variant",
+        destination: "/",
+        has: [
+          {
+            type: "host",
+            value: "variant.se",
+          },
+        ],
+        permanent: true,
+      },
+      {
+        source: "/varianter",
+        destination: "/se/varianter",
+        has: [
+          {
+            type: "host",
+            value: "variant.se",
+          },
+        ],
+        permanent: true,
+      },
+      {
+        source: "/varianter/goteborg",
+        destination: "/se/varianter?location=g%C3%B6teborg",
+        has: [
+          {
+            type: "host",
+            value: "variant.se",
+          },
+        ],
+        permanent: true,
+      },
+      {
+        source: "/varianter/linkoping",
+        destination: "/se/varianter?location=link%C3%B6ping",
+        has: [
+          {
+            type: "host",
+            value: "variant.se",
+          },
+        ],
+        permanent: true,
+      },
+      {
+        source: "/varianter/stockholm",
+        destination: "/se/varianter?location=stockholm",
+        has: [
+          {
+            type: "host",
+            value: "variant.se",
+          },
+        ],
+        permanent: true,
+      },
+      {
+        source: "/tjanster/digital",
+        destination: "/se/tjanster",
+        has: [
+          {
+            type: "host",
+            value: "variant.se",
+          },
+        ],
+        permanent: true,
+      },
+      {
+        source: "/tjanster/strategi",
+        destination: "/se/tjanster",
+        has: [
+          {
+            type: "host",
+            value: "variant.se",
+          },
+        ],
+        permanent: true,
+      },
+      {
+        source: "/tjanster/kultur",
+        destination: "/se/tjanster",
+        has: [
+          {
+            type: "host",
+            value: "variant.se",
+          },
+        ],
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default withNextIntl(withBundleAnalyzer(nextConfig));
