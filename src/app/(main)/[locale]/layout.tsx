f9b73c47efc303ac0e13e2b32ca6d589112b4721
Kristoffer Nordström <totte.nordstrom@gmail.com>
2025-07-06 10:52:50 +0200

    feat(sec): add nonce to csp script-src

--- src/app/(main)/[locale]/layout.tsx
+++ src/app/(main)/[locale]/layout.tsx
@@ -1,4 +1,5 @@
 import localFont from "next/font/local";
+import { headers } from "next/headers";
 import { notFound } from "next/navigation";
 import Script from "next/script";
 import { NextIntlClientProvider } from "next-intl";
@@ -63,6 +64,8 @@ export default async function Layout({
     notFound();
   }
 
+  const nonce = (await headers()).get("x-nonce");
+
   const messages = await getMessages();
 
   const { perspective } = getDraftModeInfo();
@@ -120,7 +123,11 @@ export default async function Layout({
             />
           </NextIntlClientProvider>
 
-          <Script id="matomoAnalytics" strategy="afterInteractive">
+          <Script
+            id="matomoAnalytics"
+            strategy="afterInteractive"
+            nonce={nonce ?? undefined}
+          >
             {`
                   var _paq = _paq || [];
                   _paq.push(["disableCookies"]);
---
+++ src/middlewares/nonceMiddleware.ts
@@ -0,0 +1,35 @@
+import { NextResponse } from "next/server";
+
+const generateNonce = () => {
+  return Buffer.from(crypto.randomUUID()).toString("base64");
+};
+
+const contentSecurityPolicy = (nonce: string) => {
+  // TODO: remove style-src 'unsafe-inline' when fixed https://github.com/vercel/next.js/discussions/54907
+  const csp = `
+    default-src 'self';
+    connect-src 'self' https://variant.innocraft.cloud/ https://g.nav.no/api/v1/;
+    script-src 'self' 'nonce-${nonce}' https://variant.innocraft.cloud/ ${
+      process.env.NODE_ENV !== "production" ? "'unsafe-eval'" : ""
+    };
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' data: https://cdn.sanity.io/;
+    media-src 'self';
+    frame-src 'self';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    object-src 'none';
+  `;
+  return csp.replace(/\s{2,}/g, " ").trim();
+};
+
+export function nonceMiddleware(response: NextResponse): NextResponse {
+  const nonce = generateNonce();
+  const csp = contentSecurityPolicy(nonce);
+
+  response.headers.set("x-nonce", nonce);
+  response.headers.set("Content-Security-Policy", csp);
+
+  return response;
+}
