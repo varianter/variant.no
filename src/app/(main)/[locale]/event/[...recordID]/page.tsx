8a3df9d27234e2c507c5a8b39a6c2aebc7126076
TrymVei <tve@variant.no>
2025-08-12 13:50:37 +0200

    feat: adds rest of the info to the event page

--- src/app/(main)/[locale]/event/[...recordID]/page.tsx
+++ src/app/(main)/[locale]/event/[...recordID]/page.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 
+import Badge from "src/components/badge/Badge";
 import EventRegistration from "src/components/hubspot/eventRegistration/eventRegistration";
+import { SanityImage } from "src/components/image/SanityImage";
+import { RichText } from "src/components/richText/RichText";
+import Text from "src/components/text/Text";
 import { IEventPosting } from "studio/lib/interfaces/eventPosting";
 import { EventRegistrationSection } from "studio/lib/interfaces/pages";
 import { EVENT_BY_RECORD_ID_QUERY } from "studio/lib/queries/specialPages";
@@ -20,8 +24,6 @@ interface EventPageProps {
 export default async function EventPage({ params }: EventPageProps) {
   const recordID = parseInt(params.recordID);
 
-  console.log(recordID);
-
   const { data } = await loadStudioQuery<{
     event: IEventPosting;
   }>(EVENT_BY_RECORD_ID_QUERY, {
@@ -29,8 +31,17 @@ export default async function EventPage({ params }: EventPageProps) {
     language: params.locale,
   });
 
-  const { eventTitle, eventDescription, date, locations, tags, _key } =
-    data.event;
+  const {
+    eventTitle,
+    date,
+    locations,
+    tags,
+    _key,
+    eventImage,
+    subtitle,
+    consultants,
+    richText,
+  } = data.event;
 
   const section: EventRegistrationSection = {
     _type: "eventRegistration",
@@ -40,20 +51,47 @@ export default async function EventPage({ params }: EventPageProps) {
 
   return (
     <div className={styles.eventPage}>
-      <h1 className={styles.eventPage__title}>{eventTitle}</h1>
-      <p>{eventDescription}</p>
-      <p>{date}</p>
-      {locations &&
-        locations.map((location, index) => (
-          <p key={index}>{location.locationString}</p>
-        ))}
-      {tags &&
-        tags.map((tag, index) => (
-          <span key={index} className="tag">
-            {tag.tag}
-          </span>
-        ))}
+      <div className={styles.eventInfo}>
+        <Badge badgeColor="#3840FF" className={styles.badge}>
+          Event
+        </Badge>
+        <Text type="h1">{eventTitle}</Text>
+        <Text type="bodyXl">{subtitle}</Text>
+        <div className={styles.eventDetails}>
+          <Text type="labelLarge">{date}</Text>
+          {locations &&
+            locations.map((location, index) => (
+              <Text type="labelLarge" key={index}>
+                {location.locationString}
+              </Text>
+            ))}
+        </div>
+      </div>
+      {eventImage && (
+        <div className={styles.eventImage}>
+          <SanityImage image={eventImage} objectFit="cover" />
+        </div>
+      )}
 
+      <div className={styles.description}>
+        <div className={styles.tags}>
+          {tags &&
+            tags.map((tag, index) => (
+              <Badge key={index} badgeColor="#FFFFFF" borderColor="#000000">
+                <Text type="labelLarge">{tag.tag}</Text>
+              </Badge>
+            ))}
+          <Text type="labelLarge">[</Text>
+          {consultants &&
+            consultants.map((consultant, index) => (
+              <Text type="labelLarge" key={index}>
+                {consultant.employeeFirstName}
+              </Text>
+            ))}
+          <Text type="labelLarge">]</Text>
+        </div>
+        {richText && <RichText value={richText} />}
+      </div>
       <EventRegistration section={section} />
     </div>
   );
