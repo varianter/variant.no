a99dee911698b652b01d9ab109ac1cf1a45bb7bf
TrymVei <tve@variant.no>
2025-08-18 11:06:10 +0200

    feat: move style to separate file

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -4,6 +4,15 @@ import { IEventPosting } from "studio/lib/interfaces/eventPosting";
 import { EVENT_BY_KEY_QUERY } from "studio/lib/queries/specialPages";
 import { loadStudioQuery } from "studio/lib/store";
 
+import {
+  backgroundStyle,
+  blueBackgroundStyle,
+  eventInfoContainerStyle,
+  eventInfoStyle,
+  imgStyle,
+  titleStyle,
+} from "./style";
+
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const language = searchParams.get("language") || "no";
@@ -21,79 +30,23 @@ export async function GET(request: Request) {
     language: language,
   });
 
-  const eventInfoStyling = {
-    fontSize: "38px",
-    color: "#FFFFFF",
-  };
-
   const { eventTitle, date, locations } = data.event;
   return new ImageResponse(
     (
-      <div
-        style={{
-          height: "100%",
-          width: "100%",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "flex-start",
-          justifyContent: "center",
-          padding: "1rem",
-          backgroundColor: "#FFFFFF",
-          fontSize: 60,
-          fontWeight: 700,
-          color: "#FFFFFF",
-        }}
-      >
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            gap: "6rem",
-            width: "100%",
-            height: "100%",
-            backgroundColor: "#3840FF",
-            borderRadius: "48px 48px 200px 48px",
-          }}
-        >
-          <h1
-            style={{
-              margin: 0,
-              textAlign: "left",
-              padding: "0 116px",
-              fontSize: "86px",
-              fontFamily: "BrittiSansVariable",
-            }}
-          >
-            {eventTitle}
-          </h1>
-
-          <div
-            style={{
-              display: "flex",
-              alignItems: "center",
-              gap: "2rem",
-              padding: "0 116px",
-              width: "100%",
-              height: "100%",
-              position: "relative",
-            }}
-          >
-            <p style={eventInfoStyling}>{date}</p>
-            <p style={eventInfoStyling}>TID</p>
+      <div style={backgroundStyle}>
+        <div style={blueBackgroundStyle}>
+          <h1 style={titleStyle}>{eventTitle}</h1>
+          <div style={eventInfoContainerStyle}>
+            <p style={eventInfoStyle}>{date}</p>
+            <p style={eventInfoStyle}>TID</p>
             {locations.map((location, index) => (
-              <p key={index} style={eventInfoStyling}>
+              <p key={index} style={eventInfoStyle}>
                 {location.locationString}
               </p>
             ))}
           </div>
           <img
-            style={{
-              width: "175px",
-              height: "42px",
-              position: "absolute",
-              bottom: "80px",
-              left: "116px",
-            }}
+            style={imgStyle}
             src={`${new URL(request.url).origin}/_assets/variant-logo-white.svg`}
           />
         </div>
