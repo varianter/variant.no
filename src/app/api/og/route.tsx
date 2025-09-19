7bba2ce158d03a9ebf822c5e8af39bf766ba97ce
anemne <63043552+anemne@users.noreply.github.com>
2025-09-19 15:52:31 +0200

    Fix 1326 - Dateformatting and subtitle to SEO-image (#1331)
    * format date and added dot seperators
    
    * Add subtitle to SEO-image
    
    * replaced date and truncate with utils methods

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -3,6 +3,8 @@ import { join } from "node:path";
 
 import { ImageResponse } from "next/og";
 
+import formatDate from "src/components/utils/formatDate";
+import formatLongStrings from "src/components/utils/formatLongStrings";
 import { IEventPosting } from "studio/lib/interfaces/eventPosting";
 import { EVENT_BY_KEY_QUERY } from "studio/lib/queries/specialPages";
 import { loadStudioQuery } from "studio/lib/store";
@@ -10,9 +12,11 @@ import { loadStudioQuery } from "studio/lib/store";
 import {
   backgroundStyle,
   blueBackgroundStyle,
+  dot,
   eventInfoContainerStyle,
   eventInfoStyle,
   imgStyle,
+  subTitleStyle,
   titleStyle,
 } from "./style";
 
@@ -37,26 +41,27 @@ export async function GET(request: Request) {
     language: language,
   });
 
-  const { eventTitle, date, time, locations } = data.event;
+  const { eventTitle, subtitle, date, time, locations } = data.event;
+  const dotWhiteSrc = `${new URL(request.url).origin}/_assets/dot-white.svg`;
+  const variantLogoWhiteSrc = `${new URL(request.url).origin}/_assets/variant-logo-white.svg`;
   return new ImageResponse(
     (
       <div style={backgroundStyle}>
         <div style={blueBackgroundStyle}>
           <h1 style={titleStyle}>{eventTitle}</h1>
+          <p style={subTitleStyle}>{formatLongStrings(80, subtitle)}</p>
           <div style={eventInfoContainerStyle}>
-            <p style={eventInfoStyle}>{date}</p>
+            <p style={eventInfoStyle}>{formatDate(date)}</p>
+            <img style={dot} alt="text seperator dot" src={dotWhiteSrc} />
             <p style={eventInfoStyle}>{time}</p>
+            <img style={dot} alt="text seperator dot" src={dotWhiteSrc} />
             {locations.map((location, index) => (
               <p key={index} style={eventInfoStyle}>
                 {location.locationString}
               </p>
             ))}
           </div>
-          <img
-            style={imgStyle}
-            alt="Variant logo"
-            src={`${new URL(request.url).origin}/_assets/variant-logo-white.svg`}
-          />
+          <img style={imgStyle} alt="Variant logo" src={variantLogoWhiteSrc} />
         </div>
       </div>
     ),
