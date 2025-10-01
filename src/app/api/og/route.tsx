efb14642794d080a8743a63b68b3858e348d4887
TrymVei <tve@variant.no>
2025-10-01 12:13:11 +0200

    fix: force og image to be dynamic

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -9,6 +9,8 @@ import { IEventPosting } from "studio/lib/interfaces/eventPosting";
 import { EVENT_BY_KEY_QUERY } from "studio/lib/queries/specialPages";
 import { loadStudioQuery } from "studio/lib/store";
 
+export const dynamic = "force-dynamic";
+
 import {
   backgroundStyle,
   blueBackgroundStyle,
