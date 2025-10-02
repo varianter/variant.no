64bc51106e4ea7d5b09b15054c88708318092e65
Mikael Brevik <mikaelbre@gmail.com>
2025-10-02 10:02:55 +0200

    feat: randomly select sentence

--- src/components/sections/punchLineBox/PunchLineBox.tsx
+++ src/components/sections/punchLineBox/PunchLineBox.tsx
@@ -1,3 +1,6 @@
+"use client";
+import { useEffect, useState } from "react";
+
 import { PunchLineBoxSection } from "studio/lib/interfaces/pages";
 
 export interface PunchLineBoxProps {
@@ -6,5 +9,30 @@ export interface PunchLineBoxProps {
 }
 
 export default function PunchLineBox({ section }: PunchLineBoxProps) {
-  return <div>{section.mainPunchLine}</div>;
+  console.log(section);
+  const randomSentence = usePickPeriodicallyRandomSentence(section.sentences);
+  return <div>{randomSentence.mainPunchLine}</div>;
+}
+
+function usePickPeriodicallyRandomSentence(
+  sentences: PunchLineBoxSection["sentences"],
+  intervalInSeconds: number = 10,
+) {
+  const [randomSentence, setRandomSentence] = useState<
+    PunchLineBoxSection["sentences"][number]
+  >(pickRandomSentence(sentences));
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setRandomSentence(pickRandomSentence(sentences));
+    }, intervalInSeconds * 1000);
+
+    return () => clearInterval(intervalId);
+  }, [sentences, intervalInSeconds]);
+
+  return randomSentence;
+}
+
+function pickRandomSentence(sentences: PunchLineBoxSection["sentences"]) {
+  return sentences[Math.floor(Math.random() * sentences.length)];
 }
