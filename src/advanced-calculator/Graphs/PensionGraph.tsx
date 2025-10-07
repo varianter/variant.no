cd1805a9d4ed665f777640c98c4b2faa69c1deb4
TrymVei <tve@variant.no>
2025-10-07 13:31:46 +0200

    fix: comment out graphs using visx

--- src/advanced-calculator/Graphs/PensionGraph.tsx
+++ src/advanced-calculator/Graphs/PensionGraph.tsx
@@ -1,139 +1,141 @@
-import { Group } from "@visx/group";
-import { ParentSize } from "@visx/responsive";
-import { scaleBand, scaleLinear } from "@visx/scale";
-import { motion } from "framer-motion";
-import { useTranslations } from "next-intl";
-import { useEffect, useMemo, useState } from "react";
-
-import styles from "src/advanced-calculator/calculator.module.css";
-import Counter from "src/advanced-calculator/Counter";
-import { formatCurrencyFromNumber } from "src/advanced-calculator/helpers/utils";
-
-const verticalMargin = 80;
-
-const getNumber = (d: BarData) => d.num;
-const getText = (d: BarData) => d.text;
-
-type BarsProps = {
-  data: BarData[];
-  parentWidth: number;
-  parentHeight: number;
-};
-
-type BarData = {
-  text: string;
-  num: number;
-  color: string;
-  position: string;
-};
-
-function PensionGraph({ data, parentWidth, parentHeight }: BarsProps) {
-  const t = useTranslations("compensation.graphs");
-  const [visible, setVisible] = useState(false);
-
-  useEffect(() => {
-    setTimeout(() => setVisible(true), 300);
-  }, []);
-
-  const xMax = parentWidth * 2;
-  const yMax = parentHeight - verticalMargin;
-
-  // Sort data to ensure the highest bar is rendered last (on top)
-  const sortedData = useMemo(() => {
-    return [...data].sort((a, b) => b.num - a.num);
-  }, [data]);
-
-  // scales, memoize for performance
-  const xScale = useMemo(
-    () =>
-      scaleBand<string>({
-        range: [0, xMax],
-        round: true,
-        domain: sortedData.map(getText),
-      }),
-    [xMax, sortedData],
-  );
-
-  const yScale = useMemo(
-    () =>
-      scaleLinear<number>({
-        range: [yMax, 0],
-        round: true,
-        domain: [0, Math.max(...sortedData.map(getNumber))],
-      }),
-    [yMax, sortedData],
-  );
-
-  return parentWidth < 10 || !visible ? null : (
-    <svg
-      width={parentWidth}
-      height={parentHeight}
-      style={{ overflow: "visible" }}
-      role="img"
-      aria-labelledby="pension-chart-title"
-    >
-      <title id="pension-chart-title">{t("pensionChart")}</title>
-      <Group top={verticalMargin}>
-        {data.map((d, index) => {
-          const text = getText(d);
-          const barWidth = xScale.bandwidth() / 3;
-          const barHeight = yMax - (yScale(getNumber(d)) ?? 0);
-          const barX = (xScale(text) ?? 0) / 2;
-
-          const barY = yMax - barHeight;
-
-          return (
-            <g key={`bar-${index}`}>
-              <motion.rect
-                width={barWidth}
-                height={barHeight}
-                x={barX}
-                animate={{ height: barHeight, y: barY, x: 0 }}
-                fill={d.color}
-                rx={10}
-              />
-              <motion.g animate={{ x: barX, y: barY - 50 }}>
-                <motion.foreignObject
-                  width={barWidth}
-                  height="50"
-                  style={{
-                    overflow: "visible",
-                  }}
-                >
-                  <div
-                    className={styles.pensionGraphTextWrapper}
-                    data-position={d.position}
-                  >
-                    <span className={styles.pensionGraphPercentage}>
-                      {text}
-                    </span>
-                    <div className={styles.pensionGraphNumber}>
-                      <Counter
-                        num={d.num}
-                        formatter={formatCurrencyFromNumber}
-                      />
-                    </div>
-                  </div>
-                </motion.foreignObject>
-              </motion.g>
-            </g>
-          );
-        })}
-      </Group>
-    </svg>
-  );
-}
-
-const PensionGraphParent = ({ data }: { data: BarData[] }) => {
-  return (
-    <div className={styles.pensionGraph}>
-      <ParentSize>
-        {({ width, height }) => (
-          <PensionGraph parentHeight={height} parentWidth={width} data={data} />
-        )}
-      </ParentSize>
-    </div>
-  );
-};
-
-export default PensionGraphParent;
+// TODO: Remove comments when visx support react 19
+//
+// import { Group } from "@visx/group";
+// // import { ParentSize } from "@visx/responsive";
+// import { scaleBand, scaleLinear } from "@visx/scale";
+// import { motion } from "framer-motion";
+// import { useTranslations } from "next-intl";
+// import { useEffect, useMemo, useState } from "react";
+//
+// import styles from "src/advanced-calculator/calculator.module.css";
+// import Counter from "src/advanced-calculator/Counter";
+// import { formatCurrencyFromNumber } from "src/advanced-calculator/helpers/utils";
+//
+// const verticalMargin = 80;
+//
+// const getNumber = (d: BarData) => d.num;
+// const getText = (d: BarData) => d.text;
+//
+// type BarsProps = {
+//   data: BarData[];
+//   parentWidth: number;
+//   parentHeight: number;
+// };
+//
+// type BarData = {
+//   text: string;
+//   num: number;
+//   color: string;
+//   position: string;
+// };
+//
+// function PensionGraph({ data, parentWidth, parentHeight }: BarsProps) {
+//   const t = useTranslations("compensation.graphs");
+//   const [visible, setVisible] = useState(false);
+//
+//   useEffect(() => {
+//     setTimeout(() => setVisible(true), 300);
+//   }, []);
+//
+//   const xMax = parentWidth * 2;
+//   const yMax = parentHeight - verticalMargin;
+//
+//   // Sort data to ensure the highest bar is rendered last (on top)
+//   const sortedData = useMemo(() => {
+//     return [...data].sort((a, b) => b.num - a.num);
+//   }, [data]);
+//
+//   // scales, memoize for performance
+//   const xScale = useMemo(
+//     () =>
+//       scaleBand<string>({
+//         range: [0, xMax],
+//         round: true,
+//         domain: sortedData.map(getText),
+//       }),
+//     [xMax, sortedData],
+//   );
+//
+//   const yScale = useMemo(
+//     () =>
+//       scaleLinear<number>({
+//         range: [yMax, 0],
+//         round: true,
+//         domain: [0, Math.max(...sortedData.map(getNumber))],
+//       }),
+//     [yMax, sortedData],
+//   );
+//
+//   return parentWidth < 10 || !visible ? null : (
+//     <svg
+//       width={parentWidth}
+//       height={parentHeight}
+//       style={{ overflow: "visible" }}
+//       role="img"
+//       aria-labelledby="pension-chart-title"
+//     >
+//       <title id="pension-chart-title">{t("pensionChart")}</title>
+//       <Group top={verticalMargin}>
+//         {data.map((d, index) => {
+//           const text = getText(d);
+//           const barWidth = xScale.bandwidth() / 3;
+//           const barHeight = yMax - (yScale(getNumber(d)) ?? 0);
+//           const barX = (xScale(text) ?? 0) / 2;
+//
+//           const barY = yMax - barHeight;
+//
+//           return (
+//             <g key={`bar-${index}`}>
+//               <motion.rect
+//                 width={barWidth}
+//                 height={barHeight}
+//                 x={barX}
+//                 animate={{ height: barHeight, y: barY, x: 0 }}
+//                 fill={d.color}
+//                 rx={10}
+//               />
+//               <motion.g animate={{ x: barX, y: barY - 50 }}>
+//                 <motion.foreignObject
+//                   width={barWidth}
+//                   height="50"
+//                   style={{
+//                     overflow: "visible",
+//                   }}
+//                 >
+//                   <div
+//                     className={styles.pensionGraphTextWrapper}
+//                     data-position={d.position}
+//                   >
+//                     <span className={styles.pensionGraphPercentage}>
+//                       {text}
+//                     </span>
+//                     <div className={styles.pensionGraphNumber}>
+//                       <Counter
+//                         num={d.num}
+//                         formatter={formatCurrencyFromNumber}
+//                       />
+//                     </div>
+//                   </div>
+//                 </motion.foreignObject>
+//               </motion.g>
+//             </g>
+//           );
+//         })}
+//       </Group>
+//     </svg>
+//   );
+// }
+//
+// const PensionGraphParent = ({ data }: { data: BarData[] }) => {
+//   return (
+//     <div className={styles.pensionGraph}>
+//       {/* <ParentSize> */}
+//       {/*   {({ width, height }) => ( */}
+//       {/*     <PensionGraph parentHeight={height} parentWidth={width} data={data} /> */}
+//       {/*   )} */}
+//       {/* </ParentSize> */}
+//     </div>
+//   );
+// };
+//
+// export default PensionGraphParent;
--- src/advanced-calculator/Graphs/SalaryGraph.tsx
+++ src/advanced-calculator/Graphs/SalaryGraph.tsx
@@ -1,174 +1,176 @@
-"use client";
-
-import { curveLinear } from "@visx/curve";
-import { ParentSize } from "@visx/responsive";
-import { scaleLinear, scaleTime } from "@visx/scale";
-import { LinePath } from "@visx/shape";
-import { extent, max, min } from "d3-array";
-import { useTranslations } from "next-intl";
-import { useMemo } from "react";
-
-import styles from "src/advanced-calculator/calculator.module.css";
-import {
-  Payscale,
-  PayscaleEntry,
-} from "src/advanced-calculator/helpers/getPayscale";
-import {
-  formatCurrencyFromNumber,
-  getLastInArray,
-} from "src/advanced-calculator/helpers/utils";
-import Text from "src/components/text/Text";
-
-const getYear = (d: PayscaleEntry): Date => d?.date ?? new Date();
-const getPayValue = (d: PayscaleEntry): number => d?.pay;
-
-const SalaryGraph = ({
-  payscale,
-  parentHeight,
-  parentWidth,
-}: {
-  payscale: Payscale;
-  parentHeight: number;
-  parentWidth: number;
-}) => {
-  const t = useTranslations("compensation.graphs");
-  const height = parentHeight;
-  const width = parentWidth;
-  const padding = {
-    top: height / 10,
-    bottom: height / 10,
-    left: 30,
-    right: 30,
-  };
-
-  const yMin = padding.top;
-  const yMax = Math.max(0, height - (padding.top + padding.bottom));
-  const xMax = Math.max(0, width - (padding.left + padding.right));
-
-  const { historic, prognosis } = payscale;
-  const projectedPayscale = [getLastInArray(historic), ...prognosis];
-  const markers = useMemo(() => {
-    return [...historic, ...prognosis].filter(
-      (entry) => entry.date instanceof Date && typeof entry.pay === "number",
-    );
-  }, [historic, prognosis]);
-
-  const yearScale = useMemo(
-    () =>
-      scaleTime({
-        range: [width, 0],
-        domain: extent(markers, getYear).reverse() as [Date, Date],
-      }),
-    [width, markers],
-  );
-
-  const payScale = useMemo(
-    () =>
-      scaleLinear({
-        range: [yMin, yMax],
-        domain: [
-          max(markers, getPayValue) ?? 0,
-          min(markers, getPayValue) ?? 1000000,
-        ],
-        nice: true,
-      }),
-    [yMin, yMax, markers],
-  );
-
-  return (
-    <div>
-      <svg
-        className={styles.payscale}
-        width={width}
-        height={height}
-        style={{ overflow: "visible" }}
-        role="img"
-        aria-labelledby="salary-chart-title"
-      >
-        <title id="salary-chart-title">{t("salaryChart")}</title>
-        <rect
-          x={padding.left}
-          y={padding.top}
-          width={xMax}
-          height={yMax}
-          rx={20}
-          fill="var(--surface-violet-light)"
-        />
-
-        <LinePath
-          data={historic}
-          x={(d) => yearScale(getYear(d))}
-          y={(d) => payScale(getPayValue(d))}
-          strokeWidth={2}
-          shapeRendering="geometricPrecision"
-          stroke="currentColor"
-          curve={curveLinear}
-        />
-
-        <LinePath
-          data={projectedPayscale}
-          x={(d) => yearScale(getYear(d))}
-          y={(d) => payScale(getPayValue(d))}
-          strokeWidth={2}
-          shapeRendering="geometricPrecision"
-          strokeDasharray="4"
-          stroke="currentColor"
-          curve={curveLinear}
-        />
-
-        {markers.map(({ year, pay, date }) => (
-          <g
-            key={year}
-            transform={`translate(${yearScale(date)} ${payScale(pay)})`}
-          >
-            <circle
-              key={year}
-              r="5"
-              stroke="currentColor"
-              strokeWidth="2"
-              fill="var(--surface-violet-light)"
-            />
-            <foreignObject
-              x="-50"
-              y="10"
-              width="100"
-              height="50"
-              className={styles.markerText}
-            >
-              <div className={styles.markerTextContainer}>
-                <Text type="labelRegular" className={styles.markerYear}>
-                  {year}
-                </Text>
-                <Text type="h5" className={styles.markerPay}>
-                  {formatCurrencyFromNumber(pay)}
-                </Text>
-              </div>
-            </foreignObject>
-          </g>
-        ))}
-      </svg>
-    </div>
-  );
-};
-
-const SalaryGraphParent = ({ payscale }: { payscale: Payscale }) => {
-  const t = useTranslations("compensation.salary");
-
-  return (
-    <div style={{ aspectRatio: "5/3", width: "90%", margin: "auto" }}>
-      <ParentSize>
-        {({ width, height }) => (
-          <SalaryGraph
-            parentHeight={height}
-            parentWidth={width}
-            payscale={payscale}
-          />
-        )}
-      </ParentSize>
-      <p>{t("historicalGrowth")}</p>
-      <p>{t("basedOnExperience")}</p>
-    </div>
-  );
-};
-
-export default SalaryGraphParent;
+// TODO: Remove comments when visx support react 19
+//
+// "use client";
+//
+// import { curveLinear } from "@visx/curve";
+// import { ParentSize } from "@visx/responsive";
+// import { scaleLinear, scaleTime } from "@visx/scale";
+// import { LinePath } from "@visx/shape";
+// import { extent, max, min } from "d3-array";
+// import { useTranslations } from "next-intl";
+// import { useMemo } from "react";
+//
+// import styles from "src/advanced-calculator/calculator.module.css";
+// import {
+//   Payscale,
+//   PayscaleEntry,
+// } from "src/advanced-calculator/helpers/getPayscale";
+// import {
+//   formatCurrencyFromNumber,
+//   getLastInArray,
+// } from "src/advanced-calculator/helpers/utils";
+// import Text from "src/components/text/Text";
+//
+// const getYear = (d: PayscaleEntry): Date => d?.date ?? new Date();
+// const getPayValue = (d: PayscaleEntry): number => d?.pay;
+//
+// const SalaryGraph = ({
+//   payscale,
+//   parentHeight,
+//   parentWidth,
+// }: {
+//   payscale: Payscale;
+//   parentHeight: number;
+//   parentWidth: number;
+// }) => {
+//   const t = useTranslations("compensation.graphs");
+//   const height = parentHeight;
+//   const width = parentWidth;
+//   const padding = {
+//     top: height / 10,
+//     bottom: height / 10,
+//     left: 30,
+//     right: 30,
+//   };
+//
+//   const yMin = padding.top;
+//   const yMax = Math.max(0, height - (padding.top + padding.bottom));
+//   const xMax = Math.max(0, width - (padding.left + padding.right));
+//
+//   const { historic, prognosis } = payscale;
+//   const projectedPayscale = [getLastInArray(historic), ...prognosis];
+//   const markers = useMemo(() => {
+//     return [...historic, ...prognosis].filter(
+//       (entry) => entry.date instanceof Date && typeof entry.pay === "number",
+//     );
+//   }, [historic, prognosis]);
+//
+//   const yearScale = useMemo(
+//     () =>
+//       scaleTime({
+//         range: [width, 0],
+//         domain: extent(markers, getYear).reverse() as [Date, Date],
+//       }),
+//     [width, markers],
+//   );
+//
+//   const payScale = useMemo(
+//     () =>
+//       scaleLinear({
+//         range: [yMin, yMax],
+//         domain: [
+//           max(markers, getPayValue) ?? 0,
+//           min(markers, getPayValue) ?? 1000000,
+//         ],
+//         nice: true,
+//       }),
+//     [yMin, yMax, markers],
+//   );
+//
+//   return (
+//     <div>
+//       <svg
+//         className={styles.payscale}
+//         width={width}
+//         height={height}
+//         style={{ overflow: "visible" }}
+//         role="img"
+//         aria-labelledby="salary-chart-title"
+//       >
+//         <title id="salary-chart-title">{t("salaryChart")}</title>
+//         <rect
+//           x={padding.left}
+//           y={padding.top}
+//           width={xMax}
+//           height={yMax}
+//           rx={20}
+//           fill="var(--surface-violet-light)"
+//         />
+//
+//         <LinePath
+//           data={historic}
+//           x={(d) => yearScale(getYear(d))}
+//           y={(d) => payScale(getPayValue(d))}
+//           strokeWidth={2}
+//           shapeRendering="geometricPrecision"
+//           stroke="currentColor"
+//           curve={curveLinear}
+//         />
+//
+//         <LinePath
+//           data={projectedPayscale}
+//           x={(d) => yearScale(getYear(d))}
+//           y={(d) => payScale(getPayValue(d))}
+//           strokeWidth={2}
+//           shapeRendering="geometricPrecision"
+//           strokeDasharray="4"
+//           stroke="currentColor"
+//           curve={curveLinear}
+//         />
+//
+//         {markers.map(({ year, pay, date }) => (
+//           <g
+//             key={year}
+//             transform={`translate(${yearScale(date)} ${payScale(pay)})`}
+//           >
+//             <circle
+//               key={year}
+//               r="5"
+//               stroke="currentColor"
+//               strokeWidth="2"
+//               fill="var(--surface-violet-light)"
+//             />
+//             <foreignObject
+//               x="-50"
+//               y="10"
+//               width="100"
+//               height="50"
+//               className={styles.markerText}
+//             >
+//               <div className={styles.markerTextContainer}>
+//                 <Text type="labelRegular" className={styles.markerYear}>
+//                   {year}
+//                 </Text>
+//                 <Text type="h5" className={styles.markerPay}>
+//                   {formatCurrencyFromNumber(pay)}
+//                 </Text>
+//               </div>
+//             </foreignObject>
+//           </g>
+//         ))}
+//       </svg>
+//     </div>
+//   );
+// };
+//
+// const SalaryGraphParent = ({ payscale }: { payscale: Payscale }) => {
+//   const t = useTranslations("compensation.salary");
+//
+//   return (
+//     <div style={{ aspectRatio: "5/3", width: "90%", margin: "auto" }}>
+//       <ParentSize>
+//         {({ width, height }) => (
+//           <SalaryGraph
+//             parentHeight={height}
+//             parentWidth={width}
+//             payscale={payscale}
+//           />
+//         )}
+//       </ParentSize>
+//       <p>{t("historicalGrowth")}</p>
+//       <p>{t("basedOnExperience")}</p>
+//     </div>
+//   );
+// };
+//
+// export default SalaryGraphParent;
