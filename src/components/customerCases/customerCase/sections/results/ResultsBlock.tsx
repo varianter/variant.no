0b567db79b50beec5177b8f86b89d9ad7045fc36
Tuvald <tuvdju@gmail.com>
2025-10-09 13:52:50 +0200

    fix(ResultsBlock): adjusts component to fit new Text props

--- src/components/customerCases/customerCase/sections/results/ResultsBlock.tsx
+++ src/components/customerCases/customerCase/sections/results/ResultsBlock.tsx
@@ -32,14 +32,16 @@ function StackedHighlights({ section, blockColor }: ResultsBlockProps) {
     <div className={styles.highlightWrapper} style={style}>
       <div className={styles.content}>
         <div className={styles.highlightBlock}>
-          <Text type="labelRegular" className={styles.highlightOutside}>
-            {section.resultsBlockTitle}
-          </Text>
+          <div className={styles.highlightOutside}>
+            <Text type="labelRegular">{section.resultsBlockTitle}</Text>
+          </div>
           {section.quote?.map((quote) => (
             <div className={styles.highlightCard} key={quote._key}>
               <div className={styles.innerContent}>
                 <Text type="h3">{quote.quoteText}</Text>
-                <p className={styles.subtitle}>{quote.quoteAuthor}</p>
+                <Text type="bodySmall" color="tertiary">
+                  {quote.quoteAuthor}
+                </Text>
               </div>
             </div>
           ))}
@@ -48,10 +50,10 @@ function StackedHighlights({ section, blockColor }: ResultsBlockProps) {
           {section.resultsList?.map((result) => (
             <div className={styles.highlightCard} key={result._key}>
               <div className={styles.innerContent}>
-                <Text type="titleL" className={styles.result}>
-                  {result.result}
+                <Text type="titleL">{result.result}</Text>
+                <Text type="bodySmall" color="tertiary">
+                  {result.description}
                 </Text>
-                <p className={styles.subtitle}>{result.description}</p>
               </div>
             </div>
           ))}
