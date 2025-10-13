3220bfab16a2708ad6a2c01d467a3861aa49f671
Tuvald <tuvdju@gmail.com>
2025-10-13 08:54:30 +0200

    fix(Tag): makes text color switch correctly on active state

--- src/components/tag/index.tsx
+++ src/components/tag/index.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-import Text from "src/components/text/Text";
+import Text, { TextColor } from "src/components/text/Text";
 
 import styles from "./tag.module.css";
 
@@ -40,6 +40,39 @@ export function tagComponentStyle(active: boolean, background: string) {
   return className;
 }
 
+export function tagComponentTextColor(
+  active: boolean,
+  background: string,
+): TextColor {
+  // Active state
+  if (active) {
+    switch (background) {
+      case "dark":
+        // Match css: .tag--active.tag--bgDark: text-primary (dark)
+        return "dark";
+      case "violet":
+        // Match css: .tag--active.tag--bgViolet: text-primary (dark)
+        return "dark";
+      default:
+        // .tag--active: text-primary-light (light)
+        return "light";
+    }
+  }
+
+  // Inactive state
+  switch (background) {
+    case "dark":
+      // Match css: .tag--bgDark: text-primary-light (light)
+      return "light";
+    case "violet":
+      // Match css: .tag--bgViolet: text-primary-light (light)
+      return "light";
+    default:
+      // Match css: .tag: text-primary (dark)
+      return "dark";
+  }
+}
+
 export const Tag = ({
   text,
   background = "light",
@@ -47,6 +80,7 @@ export const Tag = ({
   ...props
 }: TagProps) => {
   const className = tagComponentStyle(active, background);
+  const textColor = tagComponentTextColor(active, background);
 
   if (props.type === "button") {
     return (
@@ -56,14 +90,18 @@ export const Tag = ({
         onClick={props.onClick}
         {...props}
       >
-        <Text type="labelRegular">{text}</Text>
+        <Text type="labelRegular" color={textColor}>
+          {text}
+        </Text>
       </button>
     );
   }
 
   return (
     <Link className={className} href={props.href}>
-      <Text type="labelRegular">{text}</Text>
+      <Text type="labelRegular" color={textColor}>
+        {text}
+      </Text>
     </Link>
   );
 };
