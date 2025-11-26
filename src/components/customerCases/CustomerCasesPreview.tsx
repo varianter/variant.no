656007da3da1e944b20fc72b1ddaa955cf9f5f4c
Robin Hovind <rh@variant.no>
2025-11-26 14:48:19 +0100

    fix: amend customercases preview component, use initial data from server and fetch live data from client (#1475)
    * fix: make pageBuilder more defensive when loading titles and slugs
    
    * fix(bug): for preview, query initial customercases data server side, fetch updates clientside
    
    fix: query customercases data server side, fetch updates clientside
    
    * fix: make customerCases have presedence, will default to dynamicpage in draft if not

--- src/components/customerCases/CustomerCasesPreview.tsx
+++ src/components/customerCases/CustomerCasesPreview.tsx
@@ -1,28 +1,46 @@
+"use client";
+
 import { QueryResponseInitial, useQuery } from "@sanity/react-loader";
 import { Suspense } from "react";
 
 import { CustomerCasePage } from "studio/lib/interfaces/specialPages";
 import { CUSTOMER_CASES_PAGE_QUERY } from "studio/lib/queries/specialPages";
+import { CustomerCaseBase } from "studioShared/lib/interfaces/customerCases";
+import { CUSTOMER_CASES_QUERY } from "studioShared/lib/queries/customerCases";
 
 import CustomerCases from "./CustomerCases";
 
 interface CustomerCasesPreviewProps {
   initialCustomerCases: QueryResponseInitial<CustomerCasePage>;
+  initialCustomerCasesList: QueryResponseInitial<CustomerCaseBase[]>;
+  domain: string;
 }
 
 const CustomerCasesPreview = ({
   initialCustomerCases,
+  initialCustomerCasesList,
+  domain,
 }: CustomerCasesPreviewProps) => {
-  const { data: customerCases } = useQuery<CustomerCasePage>(
+  const { data: customerCasesPage } = useQuery<CustomerCasePage>(
     CUSTOMER_CASES_PAGE_QUERY,
     { slug: initialCustomerCases.data.slug },
     { initial: initialCustomerCases },
   );
 
+  const { data: customerCasesList } = useQuery<CustomerCaseBase[]>(
+    CUSTOMER_CASES_QUERY,
+    { language: customerCasesPage?.language, domain },
+    { initial: initialCustomerCasesList },
+  );
+
   return (
-    customerCases && (
+    customerCasesPage &&
+    customerCasesList && (
       <Suspense>
-        <CustomerCases customerCasesPage={customerCases} />
+        <CustomerCases
+          customerCasesPage={customerCasesPage}
+          customerCases={customerCasesList}
+        />
       </Suspense>
     )
   );
