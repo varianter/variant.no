8f8f595995e167857a3512c504d317b1a7d02e5c
Robin Hovind <rh@variant.no>
2025-11-26 13:18:56 +0100

    fix: use NEXT_PUBLIC_VERCEL_URL if NEXT_PUBLIC_URL is not defined

--- studio/presentation/resolve.ts
+++ studio/presentation/resolve.ts
@@ -1,7 +1,9 @@
 // resolveProductionUrl.ts
 import type { SanityDocument, Slug } from "sanity";
 
-const remoteUrl = process.env.NEXT_PUBLIC_URL || "https://www.variant.no";
+const remoteUrl =
+  process.env.NEXT_PUBLIC_URL ||
+  "https://" + process.env.NEXT_PUBLIC_VERCEL_URL;
 const localUrl = `http://localhost:3000`;
 
 const secret = process.env.NEXT_PUBLIC_SANITY_STUDIO_PREVIEW_SECRET || "";
