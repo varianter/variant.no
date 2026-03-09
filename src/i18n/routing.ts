207a11d69b98161ae30483123ccd4c97849c966a
Mikael Brevik <mikaelbre@gmail.com>
2026-03-09 21:24:43 +0100

    fix: issue with unwanted /no redirect on swedish site

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,13 +1,11 @@
 import { createNavigation } from "next-intl/navigation";
 import { defineRouting } from "next-intl/routing";
 
-const locales = ["en", "no", "se"] as const;
+const defaultLocale = process.env.NEXT_PUBLIC_SWEDISH ? "se" : "no";
+const locales = process.env.NEXT_PUBLIC_SWEDISH ? ["se", "en"] : ["en", "no"];
 export type Locale = (typeof locales)[number];
 
-export const routing = defineRouting({
-  locales,
-  defaultLocale: "no",
-});
+export const routing = defineRouting({ locales, defaultLocale: defaultLocale });
 
 export const { Link, redirect, usePathname, useRouter } =
   createNavigation(routing);
